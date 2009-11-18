57db1992aa432716effe15b0603b17faeb1bac6c
John Lawrence Aspden <github@aspden.com>
2009-11-18 23:36:43 +0000

    added csharp

---
+++ hertfordstreet/csharp/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace fluffy
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello fluff");
+        }
+    }
+}
