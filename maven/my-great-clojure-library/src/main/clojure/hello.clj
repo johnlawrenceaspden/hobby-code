f63914b54d0a5a40496dfd6dc924d874ec511d3f
John Lawrence Aspden <github@aspden.com>
2009-12-01 00:01:59 +0000

    trying to call hello from repl

--- maven/my-great-clojure-library/src/main/clojure/hello.clj
+++ maven/my-great-clojure-library/src/main/clojure/hello.clj
@@ -4,4 +4,15 @@
   (println "hello, maven")
   (println "sigh."))
 
+
+
 (hello)
+
+;;Trying to find out how to load the hello namespace into the repl and swank server
+;;maven starts
+(println "None-swank namespaces")
+(println (for [s (for [n (all-ns)] (name (ns-name n))) :when (not (. s contains "swank"))] s))
+(println "Swank namespaces")
+(println (for [s (for [n (all-ns)] (name (ns-name n))) :when (. s contains "swank")] s))
+(println "Classpath")
+(println (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader))))
