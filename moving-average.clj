58f9294c7129ca719f9eb08276c42bdaf2eef328
john <john@john-desktop.(none)>
2010-03-03 19:06:20 +0000

    moving average attempts and profiling

---
+++ moving-average.clj
@@ -0,0 +1,134 @@
+;(set! *warn-on-reflection* true)
+;;Elegant but inefficient
+
+(def integers (iterate inc 0))
+
+(defn average [lst] (/ (reduce + lst) (count lst)))
+(defn moving-average [window lst] (map average (partition window 1 lst)))
+
+;(time (doall (take 10000 (moving-average 200 integers)))) ;; 4-5 seconds
+
+(defn average-2 [lst size] (/ (reduce + lst) size))
+(defn moving-average-2 [window lst] (map #(average-2 % window) (partition window 1 lst)))
+
+;(time (doall (take 10000 (moving-average2 200 integers)))) ;; no great improvement
+
+
+
+(defn partialsums [s as ms]
+     (if (empty? as) (list s)
+         (let [ns (- (+ s (first as)) (first ms))]
+           (cons s (partialsums ns (next as) (next ms))))))
+
+
+(defn cunning-moving-average [window lst]
+  (map #(/ % window) 
+       (partialsums
+        (reduce + (take window lst))
+        (drop window lst)
+        lst)))
+
+;;This works fine
+(cunning-moving-average 20 (take 100 (iterate inc 0)))
+;;But blows the stack because it's not lazy
+;; (take 100 (cunning-moving-average 20 (iterate inc 0)))
+;; (cunning-moving-average 20 (take 3000 (iterate inc 0)))
+
+
+(defn lazy-partialsums [s as ms]
+  (lazy-seq
+     (if (empty? as) (list s)
+         (let [ns (- (+ s (first as)) (first ms))]
+           (cons s (lazy-partialsums ns (next as) (next ms)))))))
+
+
+(defn very-cunning-moving-average [window lst]
+  (map #(/ % window) 
+       (lazy-partialsums
+        (reduce + (take window lst))
+        (drop window lst)
+        lst)))
+
+
+;;This works fine
+(very-cunning-moving-average 20 (take 100 (iterate inc 0)))
+;;And no longer blows stack
+(take 100 (very-cunning-moving-average 20 (iterate inc 0)))
+(very-cunning-moving-average 20 (take 3000 (iterate inc 0)))
+
+
+;; Apparently We can speed up the lazy sequence thing by calling seq on the sequences early
+;; but in practice that doesn't seem to make much difference
+(defn better-lazy-partialsums [s as ms]
+  (lazy-seq
+    (let [as (seq as)
+          ms (seq ms)]
+     (if as (let [ns (- (+ s (first as)) (first ms))]
+           (cons s (lazy-partialsums ns (next as) (next ms))))
+         (list s)))))
+
+(defn exceedingly-cunning-moving-average [window lst]
+  (map #(/ % window) 
+       (better-lazy-partialsums
+        (reduce + (take window lst))
+        (drop window lst)
+        lst)))
+
+
+(defn tail-recursive-moving-average [window lst]
+  (loop [sum (reduce + (take window lst))
+         addus (drop window lst)
+         subus lst
+         sofar (list sum)]
+    (if-let [addus (seq addus)]
+      (let [nsum (- (+ (first addus) sum) (first subus))]
+        (recur nsum (rest addus) (rest subus) (cons nsum sofar)))
+        (map #(/ % window) (reverse sofar)))))
+    
+
+(def slow-flist   '(moving-average moving-average-2 ))
+(def safe-flist   '(very-cunning-moving-average exceedingly-cunning-moving-average tail-recursive-moving-average))
+(def unsafe-flist '(cunning-moving-average))
+
+
+(= ; a test
+ (range 2 18)
+ (moving-average 5 (take 20 (iterate inc 0)))
+ (moving-average-2 5 (take 20 (iterate inc 0)))
+ (cunning-moving-average 5 (take 20 (iterate inc 0)))
+ (very-cunning-moving-average 5 (take 20 (iterate inc 0)))
+ (exceedingly-cunning-moving-average 5 (take 20 (iterate inc 0)))
+ (tail-recursive-moving-average 5 (take 20 (iterate inc 0)))
+)
+
+
+(defn timings [flist window seq]
+  (for [f flist]
+    (let [s (doall seq)
+          start (System/nanoTime)
+          r (doall (f window s))
+          end (System/nanoTime)]
+      (Math/round (/ (- end start) 1000000.0)))))
+
+
+
+(defn sym->shortsym [sym]  (apply str ((fn[x] (cons (first x) (map second (re-seq #"-(.)" x)))) (str sym))))
+
+
+(defn timing-chart [ windows lengths fns]
+    (let [ flist        (map resolve fns)
+           fabbs        (map sym->shortsym fns)]
+      (for [ l lengths 
+            w windows]
+        (let [intlist      (take l integers)]
+          [ w l (timings flist w intlist) ]))))
+
+(defn print-timing-chart [windows lengths fns]
+  (cl-format true "~{ ~{ ~6d ~8d ~{ ~5d ~} ~} \n~}" 
+             (cons (list 'window 'length (map sym->shortsym fns))
+                   (timing-chart windows lengths fns))))
+  
+
+(print-timing-chart '(3 10 30 ) '(100 300 1000) (concat slow-flist safe-flist unsafe-flist))
+
+(print-timing-chart '(3 10 30 100 300 1000) '(100 300 1000 3000 10000 30000) (concat safe-flist))
