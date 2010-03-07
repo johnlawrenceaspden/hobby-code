ea85adb69de13b60c3bee84c415bd8f12764a332
John Lawrence Aspden <github@aspden.com>
2010-03-07 16:43:56 +0000

    agora simplest hello world

---
+++ agora/src/main/clojure/hello.clj
@@ -0,0 +1,12 @@
+(ns hello)
+
+(use 'compojure)
+
+;; Router                                                                          
+(defroutes my-app
+  (GET "/" (html [:h1 "Hello World!"] "working!"))
+  (ANY "*" (page-not-found)))
+
+(defn hello []
+  (println "hello!")
+  (run-server {:port 8080} "/*" (servlet my-app)))
