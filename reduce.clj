54ec324031a1257894e15bc0e9c5edd281a4aebd
John Lawrence Aspden <github@aspden.com>
2010-08-11 23:57:02 +0100

    reduce extended after conversation with AS

--- reduce.clj
+++ reduce.clj
@@ -16,26 +16,35 @@
 
 ;; In C, or another naturally imperative language, we'd say:
 
-int list[]={0,1,2,3,4,5,6,7,8,9}; //or something.
-int len=10;
+;; int list[]={1,2,3,4,5,6,7,8,9,10}; //or something.
+;; int len=10;
 
-int a=0;
-int i;
+;; int a=0;
+;; int i;
 
-for (i=0; i<len; i++){
-     a=a+list[i];
-}
+;; for (i=0; i<len; i++){
+;;      a += list[i];
+;; }
 
 ;; It's been a while since I've used C,  and I can't remember the syntax!
 ;; But I'm pretty sure that this is idiomatic code, if it's code at all.
 
+;; In fact, in C, for is a syntactic abstraction, and the loop becomes
+
+;; i=0;
+;; while(i<len)
+;; {
+;;   a += list[i];
+;;   i++;
+;; }
+
 ;; This seems a perfectly natural and straightforward way to do things, and it pisses me off 
 ;; that you can't write this directly in languages with functional pretensions. Let's see how close we can get:
-
-(def lst '(0 1 2 3 4 5 6 7 8 9))
-(def len 10)
-
+;; Clojure means something else by for, so let's translate the while loop version:
 ;; Using atoms to provide mutable state, we can do something similar
+
+(def lst '(1 2 3 4 5 6 7 8 9 10))
+(def len 10)
 
 (def a (atom 0))
 (def i (atom 0))
@@ -48,51 +57,95 @@ for (i=0; i<len; i++){
 
 ;; The value ends up in the atom a, just as in the C version.
 
-;; In clojure, this looks more complicated, partly because we've got to build the loop by hand, 
-;; partly because mutation in clojure is intrinsically more complicated, and partly because C has a very
-;; good notation for the things that it's good at. But logically they're the same algorithm
+;; In clojure, this looks more complicated, 
+;; partly because we've got to build the loop by hand, from loop, when and recur
 
-;; However, I feel dirty just writing this code in clojure.
+;; partly because mutation in clojure is intrinsically more complicated, because
+;; clojure is extremely concerned with thread safety, and so we need to allocate 
+;; and dereference atoms rather than mutating local variables.
 
-;; The function is unnecessarily complicated in clojure because the idea of mutating the value of 
-;; a variable is foreign to clojure. You can do it, but it's hard work.
+;; and partly because C has very good notations for its fundamental operations. 
+
+;; But logically they're the same algorithm.
+
+
+;; However, I feel dirty just writing this code in clojure, even
+;; though that would have been a perfectly good piece of LISP in the sixties.
+
+;; The function is unnecessarily complicated in clojure because the idea 
+;; of mutating the value of a variable is foreign to clojure. 
+;; You can do it, but it's hard work.
  
-;; There's no need to mutate state just to stick the contents of a data structure in an accumulator.
-
-;; We can do almost the same thing clojure without using mutation:
+;; A more natural way to do it in clojure is the loop-as-function-call:
 
 (loop [a 0 i 0]
   (if (= i len) a
   (recur (+ a (nth lst i)) (inc i))))
 
-;; And here the final value is the value of the expression, which is nicer
+;; This is much more idiomatic code in clojure, and it doesn't mutate any values
+;; even though the variable-rebinding in the recur call produces a very similar
+;; effect.
 
-;; Also of course, clojure's lists know how long they are, or at least when they are empty or not:
+;; And here the final value is the value of the expression, which is nicer.
+
+;; Also of course, clojure's lists know how long they are, or at least when 
+;; they are empty or not, so we don't need an explicit loop counter either.
 
 ;; So how about
 (loop [a 0 l lst]
   (if (empty? l) a
       (recur (+ a (first l)) (rest l))))
 
-;; We can imagine that this is a common pattern
+;; l moves along the list, while a accumulates the values.
 
+;; We can imagine that this is a common pattern
 (loop [acc _ l _]
   (if (empty? l) a
       (recur (_ a (first l)) (rest l))))
 
-;; Where we need to fill in the _ with the initial value of the accumulator, the list to be accumulated over, 
+;; almost as common as
+;; a= _ 
+;; for(i=0; i<_; i++)
+;; { 
+;;   a _= _ [i] 
+;; } 
+;; is in C
+
+;; Where in both cases we need to fill in the _ with the initial value of 
+;; the accumulator, the list to be accumulated over, 
 ;; and the operation to be performed.
 
-;; And indeed it is. The pattern is called reduce.
+;; And indeed it is. The pattern is called reduce, and it works on anything
+;; that naturally has a number of items in it
 
-(reduce + 0 lst)
+(reduce + 1 lst)
 
-;; In this case where the function is commutative and associative, we might think of reduce as meaning:
+;; reduce is clojure's natural way of expressing accumulation over a list
+;; in the same way as the for-loop over += and ++ is C's
+
+;;Here are some examples
+
+(reduce * 1 lst) ;; use * instead of +, and start with 1
+(reduce conj '() lst)
+
+
+(reduce * #{1,2,3})
+(reduce * [1,2,3])
+
+(reduce (fn[[a s] b] [(+ a b), (str s b)]) [0,""] lst)
+;; In these cases where the function is commutative and associative, we might think of reduce as meaning:
 
 (reduce + 0 '(1 2 3 4 5 6 7 8 9 10)) -> (0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+(reduce * 1 '(1 2 3 4 5 6 7 8 9 10)) -> (1 * 1 * 2 * 3 * 4 * 5 * ........)
 
+(reduce conj '() '(1 2 3))
 
+((fn [m w] (assoc m w (inc (m w 0)))) {} "fred")
 
-
-
+(sort #(< (%1 1) (%2 1)) 
+      (reduce 
+       (fn [m w] (assoc m w (inc (m w 0)))) 
+       {} 
+       (clojure.contrib.string/split #"\W" 
+                                     (slurp "/home/john/hobby-code/reduce.clj"))))
 
