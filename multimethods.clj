2d47ab36f8bac31a77a43ea02e3c80b7bbaadf93
John Lawrence Aspden <github@aspden.com>
2010-08-24 08:30:52 +0100

    simple multimethod example

---
+++ multimethods.clj
@@ -0,0 +1,35 @@
+;;Clojure Multimethods
+
+(defn ticket [age]
+  (cond (< age 16) :child
+        (> age 64) :ancient
+        :else      :adult))
+
+(defrecord person [name age])
+
+(defmulti  print-name (fn [person] (ticket (:age person))))
+(defmethod print-name :child   [person] (str "Young "  (:name person)))
+(defmethod print-name :adult   [person] (str "Mr "     (:name person)))
+(defmethod print-name :ancient [person] (str "Old Mr " (:name person)))
+
+(map print-name (list (person. "Fred" 23)
+                      (person. "Jimmy" 12)
+                      (person. "Seth" 78)))
+
+;;Alternative
+
+(defn pname [person]
+     (str
+      (case (ticket(:age person))
+        :child "Young "
+        :adult "Mr "
+        :ancient "Old Mr ")
+      (:name person)))
+       
+(map pname (list (person. "Fred" 23)
+                 (person. "Jimmy" 12)
+                 (person. "Seth" 78)))
+
+
+
+
