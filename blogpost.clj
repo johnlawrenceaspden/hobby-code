6283d23aa02c32d251dc2d9c9f4fc62db3327670
John Lawrence Aspden <github@aspden.com>
2010-08-28 17:36:41 +0100

    blog poster

---
+++ blogpost.clj
@@ -0,0 +1,6 @@
+;; It's been mentioned that my approach of posting syntax-highlighted posts is less than optimally readable.
+
+;; I wonder if there's a way to split the files up into pieces which can be individually syntax-highlighted
+
+;; We can use this command on the command line to highlight a clojure file
+;; $ emacs --eval "(htmlize-file \"/home/john/file.clj\" ) (kill-emacs)"
