62e1234316b025b20904f9dd0437f8bf51fb1210
John Lawrence Aspden <github@aspden.com>
2010-09-15 02:23:27 +0100

    added laptop timings

--- runge-kutta.clj
+++ runge-kutta.clj
@@ -21,6 +21,8 @@
      1000.0))
 
 (cpuspeed);; 2.399 GHz on desktop, 1.6 on laptop, but there are two cores there.
+;; often when laptop is idle, the cpu will cut down to 0.8, which will screw things up fairly royally!
+(defn cpuspeed[] 1.6) ;; fix the damned thing for laptop
 
 (defmacro cyclesperit [expr its]
   `(let [start# (. System (nanoTime))
@@ -45,6 +47,10 @@
 3167
 2704
 3005
+;;laptop seems to be faster per cycle. cpu usage is (100,0) in htop.
+1927
+1762
+1765
 (cyclesperit (solveit 0.0 1.0 0.01) 100000)
 
 
@@ -65,6 +71,10 @@
 570
 580
 564
+;;laptop
+384
+384
+386
 (cyclesperit (solveit 0.0 1.0 0.01) 10000000)
 
 ;; profiler suggest integer casts done often
@@ -86,6 +96,10 @@
 60
 63
 61
+;; laptop
+50
+50
+49
 (cyclesperit (solveit 0.0 1.0 0.01) 100000000)
 
 ;; The Lord alone knows what is going on here, for another factor of two
@@ -98,51 +112,41 @@
         (let [t1 (+ t0 h)
               y1 (+ y0 (* h (- t0 y0)))]
           (recur t1 y1 h (dec its)))
-      [t0 y0 h its])))
+      [t0 y0 h its]))))
 
 ;;desktop
 26
 28
 27
+;;laptop
+
+19
+19
+20
 (cyclesperit (solveit 0.0  1.0 0.01) 100000000)
 
 ;; profiler says 2xadd, 1 gt, 1xminus, 1xdec 1xmulti
 ;; 4 floating point, 1 gt, 1 dec, 1 conditional branch;; 7 cycles/loop is optimal?
 ;; So maybe factor of 4 slower than hand-optimized?
+;; Or factor of 3 on laptop!
 
 (time (solveit 0.0 1.0 0.01 100000000))
+;;laptop
+"Elapsed time: 1215.044133 msecs"
+[1000000.0007792843 999999.000779284 0.01 0]
+;;desktop
 "Elapsed time: 1130.129868 msecs"
 [1000000.0007792843 999999.000779284 0.01 0]
 
 ;; 1.1 seconds to do 100 000 000 iterations on my desktop.
+;; 1.2 seconds on my laptop, which has about half the clock speed.
 
 
 
-
-;;desktop
-570
-580
-564
-
-(cyclesperit (solveit 0.0 1.0 0.01) 10000000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Lazy sequence thingy.
 ;; Assuming no way to optimize this because it has to involve function call?
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (defn iterator [[t0 y0 h]]
@@ -156,7 +160,12 @@
 3271
 3359
 3727
+;;laptop (again completely maxing out one core)
+3232
+3272
+3234
 (cyclesperit (nth iterations) 10000)
+
 
 ;; this doesn't seem to help
 (defn iterator [[t0 y0 h]]
