6af7e7fe57162ae7043a6b4e15cf325c05e273de
John Lawrence Aspden <github@aspden.com>
2010-10-06 15:59:05 +0100

    fourier essay

--- fourier.clj
+++ fourier.clj
@@ -61,27 +61,56 @@
 
 (product 10 v) ; [30 40 50]
 
-;; We can calculate the lengths of vectors by Pythagoras' Theorem length^2 = x^2 + y^2 + z^2
+;; We can calculate the lengths of vectors by Pythagoras' Theorem
+;; length^2 = x^2 + y^2 + z^2
 
 (defn length [v]
   (Math/sqrt (dot-product v v)))
 
-(length v) ; 7.0710678118654755
 (length u) ; 3.7416573867739413
+(length v) ; 7.0710678118654755
+
 
 ;; Notice that all the basis vectors are length 1
 (map length (list e1 e2 e3)) ; (1.0 1.0 1.0)
 
-;; Now, the cosine of the angle between two vectors is 
+;; And that the dot-product of any e? with any other e? is 0
+(partition 3 (for [x (list e1 e2 e3)
+                   y (list e1 e2 e3)]
+               (dot-product x y)))
+
+;; If we know the length of a vector, then we can make a unit vector in its direction by dividing it by its length
+
+(defn unit-vector [v]
+  (product (/ 1 (length v)) v))
+
+(unit-vector v) ;; [0.4242640687119285 0.565685424949238 0.7071067811865475]
+
+;; Of course this will have length 1 (as far as floating point arithmetic can
+;; tell us)
+(length (unit-vector v)) ;;0.9999999999999999 sigh...
+
+;; So one thing the dot-product gives us is a notion of the length of things,
+;; but it also gives us a notion of the angles between them, independently of
+;; actually drawing them and measuring the angle with a protractor.
+
+;; The dot product of u and v, as every schoolboy knows, is the length of
+;; u times the length of v times the cosine of the angle between them.
+
+;; And that means that we can work out the cosine of the angle between the two
+;; vectors:
 (defn cos-angle [ v1 v2 ]
   (/ (dot-product v1 v2) (length v1) (length v2)))
 
-(defn angle [v1 v2] (Math/acos (cos-angle v1 v2)))
+;; And that means that we can work out the angle between the vectors (that's
+;; what acos is for)
+(defn angle [v1 v2] (Math/acos (cos-angle v1 v2))) 
 
 (angle u v) ; 0.18623876586485016
 
-;; Thats in radians of course. So if your circle is 1 meters across, you need to go 19 cms around an arc to make that angle.
-;; that's about 1/30th of all the way round the circle, or full turn
+;; Thats in radians of course. So if your circle is 1 meters across, you need to
+;; go 18.62.. cms around an arc to make that angle.  that's about 1/30th of all
+;; the way round the circle, or 1/30th of a full turn
 
 (defn radians-to-turns[r]
   (/ r (* 2.0 Math/PI)))
@@ -91,8 +120,8 @@
 
 (turns-between u v) ; 0.02964082018272505, or about 3%, of a full turn.
 
-;; So far, this is school maths that everyone should dimly remember.
-;; Let's draw a picture, just forgetting about the z coordinate and drawing x and y:
+;; So far, this is school maths that everyone should dimly remember.  Let's draw
+;; a picture, just forgetting about the z coordinate and drawing x and y:
 (use 'simple-plotter)
 
 (defn projection-plot [v]
@@ -103,11 +132,27 @@
 (projection-plot u)
 (ink blue)
 (projection-plot v)
-;; Now let's add the basis vectors
+;; Let's see the unit vectors of u and v
+(ink green)
+(projection-plot (unit-vector u))
+(projection-plot (unit-vector v))
+;; Now let's add the basis vectors for comparison
 (ink yellow)
 (doall (map projection-plot (list e1 e2 e3)))
 
-;; One problem with this sort of picture is that we lose information. We've thrown away the z coordinate by drawing it.
+;; Notice that e1 and e2 look like unit vectors, but e3 has disappeared, because we're looking at it head on, and the unit vectors of u and v don't look long enough, because they're at an angle to our diagram, so the get foreshortened.
+
+;; There's also the idea of resolving one vector against another
+(defn resolve [u v]
+  (product (/ (dot-product u v) (length v)) (unit-vector v)))
+
+;; This gives us a vector in the direction of v that is as close to u as you can
+;; get.
+(ink magenta)
+(projection-plot (resolve u v))
+
+
+;; One problem with this sort of picture is that we lose information. We've thrown away the z coordinate by drawing it. In the case of e3, the z coordinate was all there was, so it doesn't look very long in the diagram.
 
 ;; What if we just plot the three values?
 
@@ -124,11 +169,27 @@
 (coords-plot v)
 (ink yellow)
 (doall (map coords-plot (list e1 e2 e3)))
+(ink green)
+(coords-plot (unit-vector u))
+(coords-plot (unit-vector v))
 
-;; We can see the vectors better this way, even though we can no longer see the space they're in!
+;; We can see the vectors better this way, because we can see all their coordinates at once, even though we can no longer see the space they're in!
 
+;; We can build the vectors up, bit by bit, out of basis vectors, by resolving
+;; them against the coordinate vectors.
+(ink magenta)
+(coords-plot (resolve u v))
+;; In this view, (resolve u v) is a scaled version of v that is as close as it can be to being u
+;; Similarly:
+(coords-plot (resolve v u))
 
-
+;; This gives us the idea of building up vectors bit by bit
+(let [u1 (resolve u e1)
+      u12 (sum u1 (resolve u e2))
+      u123 (sum u12 (resolve u e3))]
+  (coords-plot u1)
+  (coords-plot u12)
+  (coords-plot u123))
 
 
 
