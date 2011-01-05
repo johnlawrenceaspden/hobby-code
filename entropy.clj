f1f237c82965ae027e3f281707b21f49f3942ffb
John Lawrence Aspden <github@aspden.com>
2011-01-05 01:18:45 +0000

    hmm

---
+++ entropy.clj
@@ -0,0 +1,16 @@
+;; Suppose we've got a double ended piston with an ideal gas on both sides. The temperature is 1 degree.
+;; Volume on both sides is 1. Pressure on both sides is 1, area of piston is 1.
+;; 1m^3 of gas at 1 degree holds 1 Joule of Energy. Total energy of system is 2.
+
+;; pressure * volume / temperature is one on both sides. Always? If not, then volume changes to make it so.
+
+;; We burn something worth 1 Joule in the right hand side, raising the temperature on that side to 2 degrees. Pressure on that side doubles.
+;; Energy of whole system is now 3 Joules. Pressure on piston is now 1 Pascal left.
+
+;; The piston moves. How far?
+
+;; As it moves, it does work on the gas in the left hand side, using energy from the right. Work done is pressure times distance moved.
+
+;; As it moves 1mm, it does 1mm x 1 Pascal x Area of Piston (1 m2) = 0.001 Pm^3 = 0.001 Joule on the LHS
+
+;; But it takes 0.001 Joules from the RHS, So we now have VL=0.999, VR=1.001, TR=1.999 TL=1.001
