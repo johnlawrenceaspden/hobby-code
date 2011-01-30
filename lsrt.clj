5203a5a2e7cd5a4bab6c55855cb96e0d6a393ccd
John Lawrence Aspden <github@aspden.com>
2011-01-30 23:33:35 +0000

    lsrt

--- lsrt.clj
+++ lsrt.clj
@@ -1,10 +1,13 @@
-(defn as-file [x]
-  (cond (= (class x) java.io.File) x
-        (string? (name x)) (java.io.File. (name x))
-        :else  (throw "eek")))
+;; I often want to find out what the most recent things I've changed are, and it
+;; annoys me to have to leave my REPL to find out, so I added this to my startup
+;; file:
 
-(as-file ".")
-(as-file (java.io.File. "."))
+(defn as-file [x]
+  (try
+    (cond (= (class x) java.io.File) x
+          (string? (name x)) (java.io.File. (name x))
+          :else  (throw (Exception. "?")))
+    (catch Throwable t (throw (Exception. (str x " is not a file."))))))
 
 (defn file? [x]
   (= (class (as-file x)) java.io.File))
@@ -12,31 +15,41 @@
 (defn get-files [dir]
      (seq (.listFiles (as-file dir))))
 
-(get-files (java.io.File "."))
-
 (defn lsrt* [dir n]
-  (take n (map #(.getName %) (sort-by #(.lastModified %) (get-files dir)))))
+  (take n (map #(.getName %) (reverse (sort-by #(.lastModified %) (get-files dir))))))
+
+;; Up until this point it's all kind of sensible, and I can do
+(def curdir (java.io.File. "."))
 
 (lsrt* '. 10)
 (lsrt* "." 10)
-(lsrt* (java.io.File. ".") 10)
+(lsrt*  curdir 10)
+
+;; But I find that interactively, I am always forgetting quotes or typing things
+;; in the wrong way round, and for this function I'd prefer 'do what you think I
+;; mean':
 
 (defmacro lsrt
+  ([a]
+     (cond (number? a) `(lsrt* "." ~a)
+           (symbol? a) `(lsrt* '~a 10)
+           :else `(lsrt* (as-file ~a) 10)))
   ([a b]
-     (cond
-      (and (symbol? a) (number? b)) `(lsrt* (as-file '~a) ~b)
-      (and (number? a) (symbol? b)) `(lsrt* (as-file '~b) ~a)
-      :else  `(lsrt* (as-file ~a) ~b))))
+     (cond (and (number? a) (symbol? b)) `(lsrt* (as-file '~b) ~a)
+           (number? a) `(lsrt* (as-file ~b) ~a)
+           (symbol? a) `(lsrt* (as-file '~a) ~b)
+           :else  `(lsrt* (as-file ~a) ~b))))
 
 (lsrt . 10)
+(lsrt 10 .)
+(lsrt .)
+(lsrt (java.io.File. "."))
+(lsrt 10)
 (lsrt "." 10)
-(lsrt (java.io.File. ".") 10)
+(lsrt 10 ".")
+(lsrt (java.io.File. ".") 10) ; 
+(lsrt 10 (java.io.File. "."))
 
-(file? '.)
-(number? '10)
-(as-file '.)
+;unfortunately:
 
-(lsrt (java.io.File. ".") 10)
-(lsrt* "." 10)
-
-(as-file (java.io.File "."))
+(lsrt curdir) ; ()
