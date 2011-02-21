a65f7ddb44622c08024ed62fc61aa3bc18fb7f59
John Lawrence Aspden <github@aspden.com>
2011-02-21 21:59:55 +0000

    parameters

--- flask-hello.py
+++ flask-hello.py
@@ -11,5 +11,15 @@ def hello():
 def doom():
     return "Horror & Doom!"
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return str(post_id)
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
