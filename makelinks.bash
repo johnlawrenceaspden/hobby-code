8b24905094fbadfefc6dd5522887647ad9aba7af
John Lawrence Aspden <github@aspden.com>
2011-03-22 14:37:25 +0000

    added authorized_keys

--- makelinks.bash
+++ makelinks.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 for i in hgrc emacs bashrc inputrc gitconfig; 
     do 
+	echo replacing $i
 	mv ~/.$i ~/old.$i
 	ln -s ~/hobby-code/dot$i ~/.$i; 
 done
+
+echo replacing .ssh/authorized_keys
+mv ~/.ssh/authorized_keys ~/.ssh/old.authorized_keys
+ln -s ~/hobby-code/authorized_keys ~/.ssh/authorized_keys
