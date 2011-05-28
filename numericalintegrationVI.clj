55e0208c38d9fb5fbebd57f6faa3ea5b7a28cbc7
John Lawrence Aspden <github@aspden.com>
2011-05-29 00:26:27 +0100

    numerical integration

--- numericalintegrationVI.clj
+++ numericalintegrationVI.clj
@@ -1,6 +1,5 @@
 ;; Numerical Integration: Better Refinements?
 
-
 ;; Here are some very simple functions which we might want to test integration
 ;; methods on:
 (defn square  [x] (* x x))
@@ -108,7 +107,12 @@
     [error-guess, guess, [a,b]]))
 
 (def errorstructs (map (partial interval->errorstruct trapezium-rule inverse)
-                          [[0.01,25.025][25.025 50.05][50.05 100]]))
+                       [[0.01,25.025][25.025 50.05][50.05 100]]))
+
+errorstructs
+;; ([624.6256989814656 626.6241012183343     [0.01   25.025]]
+;;  [0.04166666666666663 0.7083333333333333  [25.025 50.05]]
+;;  [0.041486462512828726 0.7072645364881702 [50.05  100]])
 
 ;; And now we need a function to refine the interval with the largest error
 
@@ -170,16 +174,15 @@
 ;; In this case, that seems to work very well, but the run time is appalling.
 
 ;; The problem is that we have a longer and longer list of intervals at every
-;; step, and every step, we have to sort this list. That's an n^2 algorithm, which won't scale well.
+;; step, and every step, we have to sort this list. That's an n^2 algorithm,
+;; which won't scale well.
 
-;; What we should do here is use a priority queue. Clojure doesn't have an immutable version, although
-;; it's possible to fake one with a sorted map.
+;; What we should do here is use a priority queue. Clojure doesn't have an
+;; immutable version, although it's possible to fake one with a sorted map.
 
 ;; But rather than do that, I'm going to drop out of the functional paradigm
 ;; altogther, and use the heap implementation from Java in a mutable fashion,
 ;; looping and popping and adding.
-
-
 
 (defn improve-loop [rule f a b count]
   (let [pq (java.util.PriorityQueue. count (comparator (fn[a b](> (first a)(first b)))))]
@@ -201,6 +204,7 @@
     [(reduce + (map first pq))
      (reduce + (map second pq))]))
 
+;; We'll ask for a thousand refinements, and get back the error estimate, and the answer.
 (integrate booles-rule inverse 0.01 100 1000) ; [4.455637248046429E-15 9.21034037197618]
 
 
@@ -215,7 +219,7 @@
 (integrate booles-rule inverse 0.00000001 10000000 1000) ; [4.549938203979309E-11 34.53877639491147]
 (integrate booles-rule inverse 0.00000001 10000000 10000) ; [9.361001557239845E-16 34.53877639491065]
 
-;; For the non-stiff integrals that we started playing with, booles rule is great:
+;; For the non-stiff integrals that we started playing with, Boole's rule is great:
 ;; It's exact for quadratics, and several higher powers
 (integrate booles-rule square 0 2 10) ; [0 8/3]
 (integrate booles-rule (fn[x] (* x x x x)) 0 2 10) ; [0 32/5]
@@ -232,58 +236,114 @@
 (integrate booles-rule sine 0 Math/PI 10000) ; [6.32722651455846E-18 2.0]
 
 
-;; I'm still quite worried about the error estimate that we made. It's only a guess, and it can be a bad guess.
-;; Here are some functions that are deliberately designed to screw things up.
-(defn sine80squared[x] (square (Math/sin (* x 80))))
+;; But I'm still quite worried about the error estimate that we made. It's only
+;; a guess, and it can be a bad guess.  Here are some functions that are
+;; deliberately designed to screw things up.
 
-(/ (Math/PI) 2) ; 1.5707963267948966
-
-(integrate booles-rule sine80squared 0 Math/PI 1) ; [1.1091279850485843E-28 3.7074689566598855E-28]
-(integrate booles-rule sine80squared 0 Math/PI 10) ; [0.013089969389960716 0.7853981633974437]
-(integrate booles-rule sine80squared 0 Math/PI 100) ; [1.7991469747360833E-12 0.7853981633974478]
-(integrate booles-rule sine80squared 0 Math/PI 1000) ; [3.207733520089899E-13 0.7853981633974484]
-(integrate booles-rule sine80squared 0 Math/PI 10000) ; [3.1095566849572033E-15 0.7853981633974481]
-
-(integrate trapezium-rule sine80squared 0 Math/PI 1) ; [3.226319244612108E-28 4.262878793991289E-28]
-(integrate trapezium-rule sine80squared 0 Math/PI 10) ; [0.01573134053904405 0.19774924859401588]
-(integrate trapezium-rule sine80squared 0 Math/PI 100) ; [5.4528883422580115E-5 0.19634904414812832]
-(integrate trapezium-rule sine80squared 0 Math/PI 1000) ; [4.6327740574637914E-7 0.19634954102557595]
-(integrate trapezium-rule sine80squared 0 Math/PI 10000) ; [5.200068066397881E-9 0.19634954084967793]
-
+;; This function is extremely vibratey near the origin.
 (defn sineinverse[x] (Math/sin (/ x)))
-(integrate booles-rule sine20squared 0.001 10 1) ; [1.795529145642849E-6 2.9410957328822254]
-(integrate booles-rule sine20squared 0.001 10 10) ; [0.2719499960148505 3.5512650253630462]
-(integrate booles-rule sine20squared 0.001 10 100) ; [1.6314869508858218E-8 3.5646617645592134]
-(integrate booles-rule sine20squared 0.001 10 1000) ; [1.1886778077561055E-8 5.010636358663671]
-(integrate booles-rule sine20squared 0.001 10 10000) ; [2.7226909067499656E-14 5.010636358672848]
 
-(integrate trapezium-rule sine20squared 0.001 10 1) ; [0.1586959395930424 2.9915404007850515]
-(integrate trapezium-rule sine20squared 0.001 10 10) ; [0.006727699711582108 2.942286334606764]
-(integrate trapezium-rule sine20squared 0.001 10 100) ; [0.08352740221903857 3.410294136542448]
-(integrate trapezium-rule sine20squared 0.001 10 1000) ; [0.019942020277266624 4.967726331200218]
-(integrate trapezium-rule sine20squared 0.001 10 10000) ; [3.421319498353696E-4 5.010635936689832]
+;; The error estimates are clearly wrong here, but the answers seem to settle down to something that looks plausible.
 
+(integrate booles-rule sineinverse 0.001 10 1) ; [0.09752245288534744 3.189170812427795]
+(integrate booles-rule sineinverse 0.001 10 10) ; [0.014802407142066881 2.725700351059874]
+(integrate booles-rule sineinverse 0.001 10 100) ; [2.666579898821515E-4 2.7262259059929814]
+(integrate booles-rule sineinverse 0.001 10 1000) ; [7.84363268117651E-9 2.7262019887881457]
+(integrate booles-rule sineinverse 0.001 10 10000) ; [8.311387750656713E-15 2.726201989096135]
 
+;; I'm slightly reassured that if we use the trapezium rule, which should be
+;; slower converging but less sensitive to high derivatives, we seem to settle down to the same thing:
 
+(integrate trapezium-rule sineinverse 0.001 10 1) ; [0.3493754261290617 2.9603246206316127]
+(integrate trapezium-rule sineinverse 0.001 10 10) ; [0.12037643221528535 2.759621850911819]
+(integrate trapezium-rule sineinverse 0.001 10 100) ; [0.011584111090323689 2.728470051290524]
+(integrate trapezium-rule sineinverse 0.001 10 1000) ; [7.174438961790802E-4 2.7265014884997414]
+(integrate trapezium-rule sineinverse 0.001 10 10000) ; [1.0854830311799172E-5 2.7262023437746654]
+
+;; Since I don't actually know what the integral of sin(1/x) is, I've no idea
+;; whether this answer is correct. Since both rules seem to settle down to the
+;; same answer, I tend to believe that it is.
+
+;; Here's a weird function, which looks like it should be even worse that sin(1/x) on its own
 (defn strange[x] (- (Math/sin (/ x)) (/ (Math/cos (/ x)) x)))
 
-;; As it happens, this is the derivative of x sin(1/x), so the real answer over [0.001, 10]
-;; should be
+;; In fact it's the derivative of x sin(1/x), so we can calculate the real answer over [0.001, 10]
+;; which should be:
 (- (* 10 (Math/sin 1/10)) (* 0.001 (Math/sin 1000))) ; 0.9975072869277496
 
+;; Interestingly, the error estimates look sound for this one:
 (integrate booles-rule strange 0.001 10 1) ; [109.91706304856582 -108.12753277035351]
 (integrate booles-rule strange 0.001 10 10) ; [0.07641821305025362 -1.0276123964492345]
 (integrate booles-rule strange 0.001 10 100) ; [0.0798435700032961 1.0469088424961843]
 (integrate booles-rule strange 0.001 10 1000) ; [2.0359056110968434E-6 0.9975072871949854]
 (integrate booles-rule strange 0.001 10 10000) ; [1.9224976990340685E-12 0.997507286927752]
 
+;; Since we seem to have dealt well with some nasty functions, we might be
+;; getting confident in our rule. I know I was!
+
+;; But this innocuous looking function
+(defn sine80squared[x] (square (Math/sin (* x 80))))
+
+;; Is a complete nightmare. It looks as though the method is converging well:
+(integrate booles-rule sine80squared 0 Math/PI 1) ; [1.1091279850485843E-28 3.7074689566598855E-28]
+(integrate booles-rule sine80squared 0 Math/PI 10) ; [0.013089969389960716 0.7853981633974437]
+(integrate booles-rule sine80squared 0 Math/PI 100) ; [1.7991469747360833E-12 0.7853981633974478]
+(integrate booles-rule sine80squared 0 Math/PI 1000) ; [3.207733520089899E-13 0.7853981633974484]
+(integrate booles-rule sine80squared 0 Math/PI 10000) ; [3.1095566849572033E-15 0.7853981633974481]
+
+;; But if we use a different rule, it also seems to converge, but to a completely different answer
+(integrate trapezium-rule sine80squared 0 Math/PI 1) ; [3.226319244612108E-28 4.262878793991289E-28]
+(integrate trapezium-rule sine80squared 0 Math/PI 10) ; [0.01573134053904405 0.19774924859401588]
+(integrate trapezium-rule sine80squared 0 Math/PI 100) ; [5.4528883422580115E-5 0.19634904414812832]
+(integrate trapezium-rule sine80squared 0 Math/PI 1000) ; [4.6327740574637914E-7 0.19634954102557595]
+(integrate trapezium-rule sine80squared 0 Math/PI 10000) ; [5.200068066397881E-9 0.19634954084967793]
+
+;; In fact both answers are wrong. We can calculate the real integral of this
+;; function over the interval [0,pi] which should be:
+
+(/ (Math/PI) 2) ; 1.5707963267948966
+
+;; In fact if we use our much earlier 'divide every interval evenly' algorithm:
+(defn iterated-rule [rule f a b N]
+  (if (= N 0)
+    (rule f a b)
+    (let [midpoint (+ a (/ (- b a) 2))]
+      (+ (iterated-rule rule f a midpoint (dec N))
+         (iterated-rule rule f midpoint b (dec N))))))
+
+;; We very quickly get surprisingly good answers:
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 1) ; 1.13079223568638E-28
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 2) ; 4.262878793991289E-28
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 3) ; 3.5626606693542546E-28
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 4) ; 3.6535380881685736E-28
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 5) ; 1.5707963267948966
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 6) ; 1.570796326794897
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 7) ; 1.5707963267948972
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 8) ; 1.5707963267948966
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 9) ; 1.5707963267948957
+(iterated-rule trapezium-rule sine80squared 0 Math/PI 10) ; 1.5707963267948963
+
+;; With booles' rule the story is the same:
+(iterated-rule booles-rule sine80squared 0 Math/PI 1) ; 3.1974110932118413E-28
+(iterated-rule booles-rule sine80squared 0 Math/PI 2) ; 3.7074689566598855E-28
+(iterated-rule booles-rule sine80squared 0 Math/PI 3) ; 2.2340214425527414
+(iterated-rule booles-rule sine80squared 0 Math/PI 4) ; 1.5358897417550046
+(iterated-rule booles-rule sine80squared 0 Math/PI 5) ; 1.570796326794895
+(iterated-rule booles-rule sine80squared 0 Math/PI 6) ; 1.5707963267948961
+(iterated-rule booles-rule sine80squared 0 Math/PI 7) ; 1.5707963267948966
+(iterated-rule booles-rule sine80squared 0 Math/PI 8) ; 1.5707963267948968
+(iterated-rule booles-rule sine80squared 0 Math/PI 9) ; 1.5707963267948963
 
 
+;; So the nice rule that we'd come up with, which worked so well for the stiff problem
+;; of integrating 1/x near the origin, is completely broken on something that the obvious
+;; recursion integrates (suspiciously) well.
 
+;; The problem is that we used an error estimate that we can't trust to control
+;; the refinement process.
 
-
-
-
+;; If we guess that a certain interval contains hardly any error, then it will
+;; never get refined at all, so we'll never find out that our guess is wrong.
 
 
 
