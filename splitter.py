5d882f966c28bc06838d5663a6c277ef7dee5860
John Aspden (Contractor) <jla@jla-desktop.uk.level5networks.com>
2012-01-25 15:57:15 +0000

    example python script with emacs keys

---
+++ splitter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#C-c C-c evaluates file in interpreter
+#C-c C-z switches to that interpreter
+#C-c C-r evaluates the selected region
+    
+    
+print("Hello World!\n");
+
+
+
