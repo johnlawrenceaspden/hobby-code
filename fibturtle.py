8d98a4b2688cc62d5b46b2c52fc239d8fb8698d7
John Lawrence Aspden <github@aspden.com>
2012-11-23 18:56:23 +0000

    better fibturtle

--- fibturtle.py
+++ fibturtle.py
@@ -1,20 +1,11 @@
 import turtle
 
-"""
-def fib(i):
-    if i <= 1:
-        return 1
-    else:
-        return fib(i-1) + fib(i-2)
-
-n = 0
-while n < 30:
-    turtle.forward(fib(n))
-    turtle.left(90)
-    n += 1
-"""
-
 def fib(n):
+    if n<3:
+        turtle.color('green', 'green')
+    else:
+        turtle.color('brown', 'brown')
+        
     turtle.forward(30)
 
     if n<2:
@@ -27,7 +18,6 @@ def fib(n):
         turtle.left(15)
 
     turtle.forward(-30)
-
 
 turtle.reset()
 fib(10)
