9aea0a320b4d17ce82e0a58bba3e21ce348719a6
John Lawrence Aspden <github@aspden.com>
2012-12-11 21:06:09 +0000

    lein 2/clojure 1.4 project file

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
-(defproject trial "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+(defproject hobby-code "0.0.1"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :repl-options { 
+                 :port 4001 
+                 :init (println "hello from hobby-code/project.clj")
+                 }
+  :source-paths ["."]
+  :min-lein-version "2.0.0"
+)
+
