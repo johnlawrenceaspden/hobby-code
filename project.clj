278c7dabfa2aea79890a6ce584efb75d4ea42c45
John Lawrence Aspden <github@aspden.com>
2012-12-29 23:39:23 +0000

    seem to need data.xml occasionally

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject hobby-code "0.0.1"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.xml "0.0.6"]]
   :repl-options { 
                  :port 4001 
                  :init (println "hello from hobby-code/project.clj")
