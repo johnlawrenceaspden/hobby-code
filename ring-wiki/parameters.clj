dbc7eff987f29c24a05604701e46bc5be43c4c5e
John Lawrence Aspden <github@aspden.com>
2013-01-08 20:04:19 +0000

    getting parameters from a request

--- ring-wiki/parameters.clj
+++ ring-wiki/parameters.clj
@@ -67,10 +67,54 @@
       (wrap-spy "what the web server sees" false)
       ))
 
+;; With our spying middleware, we can investigate the subject of parameters
 
+;; point your browser at http://localhost:8080/?doom=sarnath
+
+;; In the map which is presented to app by jetty and ring, there is the key :query-string
+;; Which should have the value "doom=sarnath"
+
+;; We could use that directly
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<h1>Hello World!!!!!!!!!!!!!!!</h1>"  )})
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (if-let [s (request :query-string)]
+             (let [[a b c] (re-matches #"(.*)=(.*)" (request :query-string))]
+                (if (and a b c) (str "<h1>You Have Invoked " b " Upon the City of " c "</h1>")
+                    (str "<h1>I do not understand, oh dark master...</h1>" )))
+             (str "<h1>Hello World!!!!!!!!!!!!!!!</h1>" ))})
 
+;; But clearly there are issues with that approach.
+
+;; Instead, we can let ring take care of it for us by inserting another piece of middleware:
+
+(require 'ring.middleware.params)
+
+(def app
+  (-> #'handler
+      (wrap-spy "what the handler sees" true)
+      (ring.middleware.stacktrace/wrap-stacktrace)
+      (ring.middleware.params/wrap-params)
+      (wrap-spy "what the web server sees" false)
+      ))
+
+
+;; If you have another look at http://localhost:8080/?Doom=Sarnath
+
+;; You'll see that in between the web server and the handler function, wrap-params has inserted
+;; another key, :query-params, with the :query-string already split up into a key-value map
+
+;; So we don't need to parse the query string ourselves any more:
+
+(defn handler [request]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (if-let [m (request :query-params)]
+             (if (empty? m)
+               (str "<h1>Hello World!</h1>" )
+               (apply str (for [[k v] m] (str "<h1>You Have Invoked " k " Upon the City of " v "</h1>"))))
+               (str "<h1>Missing :query-params. Have you included ring.middleware.stacktrace/wrap-stacktrace, oh dark master?</h1>" ))})
+
+
+             
