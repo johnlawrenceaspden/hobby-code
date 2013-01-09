2d240710c0ffdfc524e92ea7b652b98cb04d000d
John Lawrence Aspden <github@aspden.com>
2013-01-09 14:47:53 +0000

    ring cookies tutorial

--- ring-wiki/cookies.clj
+++ ring-wiki/cookies.clj
@@ -55,4 +55,100 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Next we'll include We'll include 
+;; Next we'll include the cookies middleware
+
+(require 'ring.middleware.cookies)
+
+;; And re-plumb
+
+(def app
+  (-> #'handler
+      (ring.middleware.stacktrace/wrap-stacktrace)
+      (wrap-spy "what the handler sees" true)
+      (ring.middleware.cookies/wrap-cookies)
+      (wrap-spy "what the web server sees" false)))
+
+
+;; Now go and look at http://localhost:8080 again.
+
+;; In the map the handler sees, there is a key :cookies, whose value is {}
+
+;; Let's make our app  set a cookie:
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<h1>Hello World!</h1>" )
+   :cookies {"yo" {:value "hi"}} })
+
+
+;; What happens now is quite complicated. 
+
+;; Our key 
+{ :cookies {"yo" {:value "hi"}}}
+;; Gets converted by the middleware, and combined with our header, to make
+{ :headers {"Set-Cookie" '("yo=hi"), "Content-Type" "text/html"}}
+;; in the map given to the jetty adapter
+
+;; If you look at the page with
+;; $ curl -sv http://localhost:8080
+;; Then you'll see
+;; < Set-Cookie: yo=hi
+;; as part of the http transaction
+
+;; Now if we look at http://localhost:8080, the response will contain the Set-Cookie header.
+
+;; Most browsers will react to this by including the cookie next time they contact the site.
+;; You can examine cookies from the browser's point of view by 
+;; (In Chrome) looking at chrome://chrome/settings/cookies
+;; (In Firefox) following some interminable GUI procedure, but life is too short to describe it.
+
+
+;; Now we see 
+{:headers {"cookie" "yo=hi"}}
+;; in the incoming request from the webserver
+;; and a new key:
+{:cookies {"yo" {:value "hi"}}} 
+;; in the map the eventual handler sees (put there by the middleware of course!)
+
+
+
+;; We can use this to count how many times a particular browser has been greeted:
+(defn seen-before [request]
+  (try (Integer/parseInt (((request :cookies) "yo") :value))
+       (catch Exception e :never-before)))
+
+(defn handler [request]
+  (let [s (seen-before request)]
+    (cond
+     (= s :never-before) {:status 200
+                          :headers {"Content-Type" "text/html"}
+                          :body (str "<h1>Hello Stranger!</h1>" )
+                          :cookies {"yo" {:value "1"}}}
+     (= s 1) {:status 200
+                          :headers {"Content-Type" "text/html"}
+                          :body (str "<h1>Hello Again!</h1>" )
+                          :cookies {"yo" {:value "2"}}}
+     :else {:status 200
+                          :headers {"Content-Type" "text/html"}
+                          :body (str "<h1>Hi, this is visit "s"</h1>" )
+                          :cookies {"yo" {:value (str (inc s))}}})))
+
+
+
+
+
+;; And now, an exercise for the reader!
+
+;; If I look at my site in Firefox, it works as I expected.
+
+;; If I look at it with Chrome, it double counts
+
+;; If I use curl, like so:
+;; curl -sv http://localhost:8080 | grep -i hello
+
+;; Then all I ever see is "Hello Stranger"
+
+;; What is going on?
+
+
+
