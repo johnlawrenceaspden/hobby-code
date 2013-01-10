18baa4917ad0b34f3006b09da612101d503e867c
John Lawrence Aspden <github@aspden.com>
2013-01-10 20:59:43 +0000

    session tutorial

--- ring-wiki/sessions.clj
+++ ring-wiki/sessions.clj
@@ -24,7 +24,7 @@
     (println "-------------------------------"))))
 
 
-;; I have taken the liberty of removing some of the less interesting entries from the request and response maps, for clarity
+;; I have taken the liberty of removing some of the less fascinating entries from the request and response maps, for clarity
 (def kill-keys [:body :character-encoding :remote-addr :server-name :server-port :ssl-client-cert :scheme  :content-type  :content-length])
 (def kill-headers ["user-agent" "accept" "accept-encoding" "accept-language" "accept-charset"])
 
@@ -45,9 +45,11 @@
 ;; plumbing
 (def app
   (-> #'handler
-      (ring.middleware.stacktrace/wrap-stacktrace)
+      (ring.middleware.stacktrace/wrap-stacktrace) ;; belt
       (wrap-spy "what the handler sees" )
-      (wrap-spy "what the web server sees" )))
+      (wrap-spy "what the web server sees" )
+      (ring.middleware.stacktrace/wrap-stacktrace) ;; braces
+      ))
 
 ;; The actual application
 (defn handler [request]
@@ -59,11 +61,27 @@
 ;; Start the server if it hasn't already been started
 (defonce server (ring.adapter.jetty/run-jetty #'app {:port 8080 :join? false}))
 
+
+;; Have a look at http://localhost:8080, and while you're there,
+;; delete any cookies that your browser is storing for localhost:8080
+
+;; In Chrome, you can right-click on a page, Inspect Element, and then
+;; choose Resources/Cookies/localhost to give you a live view of your
+;; cookies, which is nice to watch for the following.
+
+;; In Firefox I can't find anything as nice. 
+
+;; With curl of course, you have total control.
+;; Real men, those who do not cower behind their mother's apron strings like whining infants
+;; may wish to experiment with commands such as:
+;; $ curl -sv http://localhost:8080 -b cookies.txt -c cookies.txt && cat cookies.txt
+
+
 ;; Now we'll add the session middleware:
 
 (require 'ring.middleware.session)
 
-;; plumbing
+;; and re-plumb
 (def app
   (-> #'handler
       (ring.middleware.stacktrace/wrap-stacktrace)
@@ -74,34 +92,56 @@
       ))
 
 
+;; Have another look at http://localhost:8080, (i.e. refresh the page)
+
+
 ;; If you examine the request as passed to the handler, then you'll
-;; see a :session key, whose value currently is {}
+;; see a :session key, whose value currently is {}. That's been
+;; inserted by the session middleware.
 
 ;; That seems to be the only difference so far.
 
-;; Now redefine the handler to return a :session key in the response map
+;; Now redefine the handler to return a :session key/value pair in the response map
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (str "<h1>Hello World!</h1>" )
-   :cookies {"ring-session" {:value ""} "yo" {:value "kill", :max-age 10}}
-   ; :cookies {"ring-session" {:value "kill", :max-age 10} "yo" {:value "kill", :max-age 10}}
-   ; :session "I am a session. Fear me."
-   })
+   :session "I am a session. Fear me."})
 
 
+;; And refresh the page again. Only once! And pay careful attention!
+
+;; The incoming maps are exactly the same, of course, but on the way out the middleware
+;; notices the :session key, and transforms it into a cookie-setting header
+
+;; Your browser should now have a stored cookie, named ring-session,
+;; with a cryptic but hopefully unique random value
+
+;; Again refresh, and notice that this time, the browser presents its cookie, 
+;; and the middleware decodes it and puts a :session key in the request.
+;; Notice that no new cookie is set the second time.
 
 
-;; This looks to be working in much the same way as
+;; One thing we have control of is the length of time before the session expires
+;; Ten seconds is a bit short, but it does allow for some interesting effects:
+(def app
+  (-> #'handler
+      (ring.middleware.stacktrace/wrap-stacktrace)
+      (wrap-spy "what the handler sees" )
+      (ring.middleware.session/wrap-session {:cookie-attrs {:max-age 10}})
+      (wrap-spy "what the web server sees" )
+      (ring.middleware.stacktrace/wrap-stacktrace)
+      ))
 
 
-
-
-;; The actual application
+;; Here's an app using sessions to store state in much the same way that we were using cookies earlier:
 (defn handler [request]
   (when (not= (request :uri) "/favicon.ico")
-    (let [count ((request :session {}) :count 0)]  ;; no, I didn't know you could do this either. neat, isn't it.
+    (let [count ((request :session {}) :count 0)]  ;; no, I didn't know you could do this either. neat, isn't it?
       {:status 200
        :headers {"Content-Type" "text/html"}
-       :body (str "<h1>Hello World(" count ")!</h1>" )
+       :body (cond (zero? count) (str "<h1>Hello Stranger!</h1>" )
+                   :else (str "<h1>Hello Again (" count ")!</h1>" ))
        :session {:count (inc count)}})))
+
+;; How many times can you ping the virtual goldfish?
