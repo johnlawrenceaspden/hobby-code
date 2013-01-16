6df08f303f5dcbde176abf2d122cd0ca53fd13c4
John Lawrence Aspden <github@aspden.com>
2013-01-16 14:10:24 +0000

    simplified moral maze

---
+++ ring-wiki/datainmemory.clj
@@ -0,0 +1,145 @@
+;;  necessary dependencies
+;; [[org.clojure/clojure "1.4.0"]
+;;  [ring/ring "1.1.6"]]
+;; -------------
+
+;; I've gone a bit beyond the ring wiki tutorials now. 
+
+;; In case it's not obvious, I have no idea what I'm doing!  I'm
+;; trying to work out what sort of structure a real webapp built on
+;; ring should have, so I'm trying various approaches.
+
+;; There are all sorts of helpful libraries and frameworks I could be
+;; using, but I'm avoiding them deliberately because I think it's
+;; important to understand what I'm doing. And if you can't write it,
+;; you don't understand it.
+
+;; If anyone's got any good links to 'what a web-app should like like
+;; structurally' articles, then please leave them in the comments. I
+;; can't find anything like that, which is weird for such a widespread
+;; problem, and that's one of the reasons why I'm trying to write one.
+
+;; In the previous articles, I made an app which kept all its state in
+;; sessions. Those sessions could be put in a cookie backed store, so
+;; that all a user's data is stored encrypted on a user's computer,
+;; and that might be a nice solution for certain problems.
+
+;; But I also want to try a different approach, where I keep all the
+;; data on the server.  Eventually I want to move that data into a
+;; database on the server, but for now I'm going to keep it in server
+;; memory. 
+
+;; Clojure's memory model is already quite database-like, so
+;; presumably it won't be that hard to move the data into a database
+;; eventually.
+
+;; Of course that means that it will get lost when the server
+;; restarts, but one thing at a time.
+
+;; I'm also going to simplify the problem somewhat. The flash messages
+;; are just a distracting detail, and the global counters are
+;; redundant if we have all the data in memory. The redirects to the
+;; home page make it harder to understand what's going on, so I'm
+;; going to remove them an replace them with links back. I'm going to
+;; put the session data in a cookie-backed session so that there's no
+;; session-related state in the server to worry about.
+
+;; So here's a simplified version of the already very simple character
+;; test, which I'll then try to convert to a more centralised design.
+
+(require 'ring.adapter.jetty
+         'ring.middleware.stacktrace
+         'ring.middleware.session.cookie
+         'ring.middleware.session
+         'ring.middleware.flash
+         'clojure.pprint)
+
+;; middleware for spying on request maps
+
+(defn html-escape [string]
+  (str "<pre>" (clojure.string/escape string {\< "&lt;", \> "&gt;"}) "</pre>"))
+
+(defn format-request [name request kill-keys kill-headers]
+  (let [r1 (reduce dissoc request kill-keys)
+        r (reduce (fn [h n] (update-in h [:headers] dissoc n)) r1 kill-headers)]
+  (with-out-str
+    (println "-------------------------------")
+    (println name)
+    (println "-------------------------------")
+    (clojure.pprint/pprint r)
+    (println "-------------------------------"))))
+
+(def kill-keys [:body :request-method :character-encoding :remote-addr :server-name :server-port :ssl-client-cert :scheme  :content-type  :content-length])
+(def kill-headers ["user-agent" "accept" "accept-encoding" "accept-language" "accept-charset" "connection" "host"])
+
+(defn wrap-spy [handler spyname]
+  (fn [request]
+    (let [incoming (format-request (str spyname ":\n Incoming Request:") request kill-keys kill-headers)]
+      (println incoming)
+      (let [response (handler request)]
+        (let [outgoing (format-request (str spyname ":\n Outgoing Response Map:") response kill-keys kill-headers)]
+          (println outgoing)
+          (if (= (type (response :body)) java.lang.String)
+                     (update-in response  [:body] (fn[x] (str (html-escape incoming) x  (html-escape outgoing))))
+                     response))))))
+
+;; plumbing
+
+(declare handler)
+
+(def app
+  (-> #'handler
+      (ring.middleware.stacktrace/wrap-stacktrace)
+      (wrap-spy "what the handler sees" )
+      (ring.middleware.session/wrap-session {:store (ring.middleware.session.cookie/cookie-store {:key "a 16-byte secret"})})
+      (wrap-spy "what the server sees" )
+      (ring.middleware.stacktrace/wrap-stacktrace)))
+
+(defonce server (ring.adapter.jetty/run-jetty #'app {:port 8080 :join? false}))
+
+
+;; response map makers
+
+(defn status-response [code body]
+  {:status code
+   :headers {"Content-Type" "text/html"}
+   :body body})
+
+(def response (partial status-response 200))
+
+;; Finally here is the app itself, reduced, I hope, to the absolute essentials
+
+(defn good [request]
+  (let [ good   (get-in request [:session :good] 0) ] 
+    (assoc (response "<h1>good</h1> <a href=\"/\">choose again</a>" )
+      :session (assoc (request :session) :good (inc good)))))
+
+(defn evil [request]
+  (let [ evil   (get-in request [:session :evil] 0) ]  
+    (assoc (response "<h1>evil</h1> <a href=\"/\">choose again</a>" )
+      :session (assoc (request :session) :evil (inc evil)))))
+
+(defn home [request]
+  (let
+      [good   (get-in request [:session :good] 0)
+       evil   (get-in request [:session :evil] 0)]
+    (response (str "<h1>The Moral Maze</h1>"
+                   "Good " good " : Evil " evil "<p>"
+                   "What do you choose: "
+                   "<a href=\"/good\">good</a> or <a href=\"/evil\">evil</a>?"))))
+
+(defn handler [request]
+  (case (request :uri)
+    "/" (home request)
+    "/good" (good request)
+    "/evil" (evil request)
+    (status-response 404 (str "<h1>404 Not Found: " (:uri request) "</h1>" ))))
+
+
+
+
+
+
+
+
+
