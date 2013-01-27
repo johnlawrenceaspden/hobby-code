f5c343b22972957694de0082ed748fdd5f61f34e
John Lawrence Aspden <github@aspden.com>
2013-01-27 23:45:48 +0000

    try cloverage library

--- ring-wiki/project.clj
+++ ring-wiki/project.clj
@@ -3,7 +3,8 @@
                  [ring "1.1.7"]
                  [compojure "1.1.4"]
                  [enlive "1.0.1"]]
-  :plugins [[lein-marginalia "0.7.1"]]
+  :plugins [[lein-marginalia "0.7.1"]
+            [lein-cloverage "1.0.2"]]
   :repl-options { 
                  :port 4001 
                  :init (println "hello from ring-wiki/project.clj")
