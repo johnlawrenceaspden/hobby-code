8cd6a8407d3986f8de6c48d578a4a1feaea4dd93
John Lawrence Aspden <github@aspden.com>
2013-01-31 19:26:10 +0000

    remove handler definitions, replace with macro part ii

--- ring-wiki/datainsessionatom.clj
+++ ring-wiki/datainsessionatom.clj
@@ -108,13 +108,6 @@
 ;; paste the handler all the time, so thank you to Nikita Beloglazov
 ;; who told me how to write this replacement:
 
-(defmacro def-handler [& addresses]
-  `(defn handler [~'request]
-     (case (~'request :uri)
-       ~@(mapcat (fn[x] [(str "/" x) (list x 'request)]) addresses)
-       "/" (home ~'request)
-       (status-response 404 (str "<h1>404 Not Found: " (:uri ~'request) "</h1>" )))))
-
 (defmacro routefn [& addresses]
   `(fn[~'request]
      (case (~'request :uri)
@@ -420,26 +413,45 @@
 (defn passwords [req]
   (response (hppp (for [[ k {n :name p :password}] @db] [n p]))))
 
-(def handler (routefn 
-  good evil 
-  database passwords highscores 
-  namechange change-my-name 
-  changeidentity change-my-identity))
+(defn handler [request]
+  (case (request :uri)
+    "/" (home request)
+    "/good" (good request)
+    "/evil" (evil request)
+    "/database" (database request)
+    "/passwords" (passwords request)
+    "/highscores" (highscoretable request)
+    "/namechange" (namechange request)
+    "/change-my-name" (change-my-name request)
+    "/changeidentity" (changeidentity request)
+    "/change-my-identity" (change-my-identity request)
+    (status-response 404 (str "<h1>404 Not Found: " (:uri request) "</h1>" ))))
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
 ;; Here's an example database for testing purposes
+(swap! db (fn[x] 
+{"4c1c2b12-3095-4136-abc1-e9778115cbd0" {:evil 3, :good 2, :name "atomic man"},
+ "7e0a7e86-b00c-4a78-8dd0-2a1ccf627c52" {:name "darkfluffy", :good 2},
+ "61252413-28be-4c47-a2f5-37893f19d4b1" {:name "type name here"},
+ "099dc04e-8b19-462e-8aff-519b6c5fa50f" {:evil 2, :good 3, :name "hello world"},
+ "0989d4d5-531d-4e25-bdf7-425a8c62663f" {:evil 1, :name "righteousman", :good 2},
+ "83939a50-0073-41b1-8fb0-a85274a67aad" {:good 1}}
+))
+
+(swap!  db  (fn[x]  (merge  x 
+  { "89be190a-4fb5-4562-aee4-1a65b0d6b415" {:password "type name here", :good 1, :evil 2, :name "freds"}})))
 
 (swap! db (fn[x] (merge x 
-{"83939a50-0073-41b1-8fb0-a85274a67aad" {:good 1},
- "0989d4d5-531d-4e25-bdf7-425a8c62663f" {:evil 1, :name "righteousman", :good 2},
- "099dc04e-8b19-462e-8aff-519b6c5fa50f" {:evil 2, :name "hello world", :good 3, :password "f@ilz0r!"},
- "61252413-28be-4c47-a2f5-37893f19d4b1" {:name "type name here"},
- "7e0a7e86-b00c-4a78-8dd0-2a1ccf627c52" {:name "darkfluffy", :good 2, :password "df"},
- "89be190a-4fb5-4562-aee4-1a65b0d6b415" {:evil 2, :name "fluffy", :good 1, :password "doom"},
+{"89be190a-4fb5-4562-aee4-1a65b0d6b415" {:evil 2, :name "fluffy", :good 1, :password "doom"},
  "4c1c2b12-3095-4136-abc1-e9778115cbd0" {:evil 3, :name "atomic man", :good 2},
- "8d2f51c3-bd88-41bf-a8b3-463c9ac96409" {:good 3, :password "pass", :name "mrdoom"},
- "09b5e860-6139-435f-a359-eab110b622c2" {:password "", :name "mrdoom", :evil 5, :good 2}}
+ "7e0a7e86-b00c-4a78-8dd0-2a1ccf627c52" {:password "df", :name "darkfluffy", :good 2},
+ "61252413-28be-4c47-a2f5-37893f19d4b1" {:name "type name here"},
+ "099dc04e-8b19-462e-8aff-519b6c5fa50f" {:password "f@ilz0r!", :evil 2, :name "hello world", :good 3},
+ "0989d4d5-531d-4e25-bdf7-425a8c62663f" {:evil 1, :name "righteousman", :good 2},
+ "83939a50-0073-41b1-8fb0-a85274a67aad" {:good 1}}
 )))
