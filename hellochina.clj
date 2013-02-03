c891ad4ccc38fa98fc627f6ca1aa047c21cdfefc
John Lawrence Aspden <github@aspden.com>
2013-02-03 12:48:41 +0000

    hello china blogpost

---
+++ hellochina.clj
@@ -0,0 +1,25 @@
+;; A warm welcome to readers from China!
+
+;; After many years when this blog's audience was strictly Empire and
+;; Colonies only, I've recently noticed a vast number of hits from
+;; China.
+
+;; It turns out I'm on the front page if you search for Clojure on
+;; baidu, which goes to show what a very fine search engine it is.
+
+;; This blog is about my continuous process of learning Clojure. In it
+;; I have conversations with my REPL about stuff I don't understand,
+;; hoping to understand it by poking it with a stick and seeing what
+;; happens.
+
+
+;; I'm told that this is how to say hello world in Mandarin:
+(defn 你好世界 []
+  (print "你好世界"))
+
+;; Clojure seems to deal well with this:
+user> (你好世界)
+你好世界
+nil
+
+;; Is this right? And how do I pronounce it?
