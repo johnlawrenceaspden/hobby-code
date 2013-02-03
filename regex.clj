5926d544b56210b7e0e4f389dab8391b8acc69a3
John Lawrence Aspden <github@aspden.com>
2013-02-03 21:22:44 +0000

    something about regexes

--- regex.clj
+++ regex.clj
@@ -30,6 +30,60 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Simple regexes
+
+;; Most characters will match themselves
+
+(def greek "alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi omicron pi rho sigma tau upsilon phi chi psi omega")
+
+;; There are lots of "a"s in greek
+(re-seq #"a" greek) ;-> ("a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a" "a")   
+
+;; But not so many "b"s
+(re-seq #"b" greek) ;-> ("b" "b")
+
+;; A . will match anything
+
+(take 20 (re-seq #"." greek)) ; -> ("a" "l" "p" "h" "a" " " "b" "e" "t" "a" " " "g" "a" "m" "m" "a" " " "d" "e" "l")
+
+;; * means 'any number of the previous thing'
+(re-seq #"ga*m*a" greek)
+
+;; So .* means any number of any thing
+(re-seq #".*" greek) ; -> ("alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi omicron pi rho sigma tau upsilon phi chi psi omega" "")
+
+;; \b is a word boundary, \w is non-whitespace, so:
+(re-seq #"\b\w*\b" greek) ; -> ("alpha" "" "beta" "" "gamma" "" "delta" "" "epsilon" "" "zeta" "" "eta" "" "theta" "" "iota" "" "kappa" "" "lambda" "" "mu" "" "nu" "" "xi" "" "omicron" "" "pi" "" "rho" "" "sigma" "" "tau" "" "upsilon" "" "phi" "" "chi" "" "psi" "" "omega" "")
+
+;; but we found too much there. Instead of * meaning any number, we can say:
+
+(re-seq #"\b\w{0}\b" greek) ; ("" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "")
+(re-seq #"\b\w{1}\b" greek) ; nil
+(re-seq #"\b\w{2}\b" greek) ; ("mu" "nu" "xi" "pi")
+(re-seq #"\b\w{3}\b" greek) ; ("eta" "rho" "tau" "phi" "chi" "psi")
+(re-seq #"\b\w{4}\b" greek) ; ("beta" "zeta" "iota")
+
+
+;; Ranges work too
+(re-seq #"\b\w{1,4}\b" greek) ; ("beta" "zeta" "eta" "iota" "mu" "nu" "xi" "pi" "rho" "tau" "phi" "chi" "psi")
+(re-seq #"\b\w{5}\b" greek) ; ("alpha" "gamma" "delta" "theta" "kappa" "sigma" "omega")
+(re-seq #"\b\w{6,}\b" greek) ; ("epsilon" "lambda" "omicron" "upsilon")
+
+;; + is a shorthand for {1,} in the same way as * is one for {0,}
+(re-seq #"\b\w+\b" greek) ; ("alpha" "beta" "gamma" "delta" "epsilon" "zeta" "eta" "theta" "iota" "kappa" "lambda" "mu" "nu" "xi" "omicron" "pi" "rho" "sigma" "tau" "upsilon" "phi" "chi" "psi" "omega")
+
+;; we can use character ranges:
+(re-seq #"\b[a-o]+\b" greek) ; ("gamma" "lambda" "chi" "omega")
+
+;; or exclude certain characters:
+(re-seq #"\b[^ ]+\b" greek) ; ("gamma" "lambda" "chi" "omega")
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;; Here are some classic example regexes (ripped off from
 ;; http://myregexp.com/examples.html), because I can never remember
