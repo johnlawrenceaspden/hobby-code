a82307f28e4779b8935d9c2e4d4e71101be54cc7
John Lawrence Aspden <github@aspden.com>
2013-02-14 22:39:41 +0000

    I always want trace on the classpath

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject hobby-code "0.0.1"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.trace "0.7.5"]
                  [com.cemerick/pomegranate "0.0.13"]]
 
 ;; The magic words are:
@@ -7,6 +8,8 @@
 ;; (cemerick.pomegranate/add-dependencies 
 ;;  :coordinates '[[ring "1.1.7"]]
 ;;  :repositories {"clojars" "http://clojars.org/repo" } )
+
+;; (cemerick.pomegranate/add-dependencies :coordinates '[[org.clojure/tools.trace "0.7.5"]])
 
   :repl-options { 
                  :port 4001 
