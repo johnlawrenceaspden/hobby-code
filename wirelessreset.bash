308d1fbe201da75c3a7ffadb3942f7370b279521
John Lawrence Aspden <github@aspden.com>
2013-04-04 15:38:16 +0100

    more crap

--- wirelessreset.bash
+++ wirelessreset.bash
@@ -1,9 +1,26 @@
 #!/bin/bash
 
 # this sequence restored comms in the Maypole when it was broken
-# when typed in, but when run as a script seem to break it
+# when typed in, but when run as a script broke it irrevocably
+
+# reboot and it still didn't work, but then try this sequence again
+# and all was suddenly well.
 
 # added short pauses to see if that helps
+
+# Sometimes you get Association Request to the driver failed
+# sometimes it's deauth request to the driver failed
+
+# This repeatedly in syslog
+#Apr  4 15:16:25 dell-mini wpa_supplicant[1002]: Deauth request to the driver failed
+#Apr  4 15:16:25 dell-mini NetworkManager[2939]: <info> (eth2): supplicant interface state: associating -> disconnected
+#Apr  4 15:16:25 dell-mini NetworkManager[2939]: <info> (eth2): supplicant interface state: disconnected -> scanning
+
+# repeated for ages then all sorts of guff about changing regulatory domain to TW
+# then makes connection quickly
+
+# It seems if it's in this state then these commands will cure it.
+
 
 sudo service network-manager stop
 sleep 4
