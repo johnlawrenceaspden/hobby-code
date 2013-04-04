fb5a4ce93c3b566a491018377f1f83b7c9c25a83
John Lawrence Aspden <github@aspden.com>
2013-04-04 14:31:48 +0100

    aargh, doesn't work as script but does work as commands

--- wirelessreset.bash
+++ wirelessreset.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # this sequence restored comms in the Maypole when it was broken
+# when typed in, but when run as a script seem to break it
+
+# added short pauses to see if that helps
 
 sudo service network-manager stop
+sleep 4
 sudo modprobe -r b43 ssb wl mac80211 cfg80211 bcma 
+sleep 4
 sudo modprobe wl
+sleep 4
 sudo service network-manager restart
