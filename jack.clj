4c2cf7f4bebb55c7587b5ee602a5d81f115cbf9d
John Lawrence Aspden <github@aspden.com>
2013-08-27 14:47:02 +0100

    square roots for jack

--- jack.clj
+++ jack.clj
@@ -1,9 +1,18 @@
-;; Program to find the square root of 11
+;; Program to find square roots
 
-(def abs (fn [x] (if (< (- x) 0) x (- x))))
+(def ave (fn [x y] (/ (+ x y) 2)))
 
-(def improve (fn [guess] (ave guess (/ 11 guess))))
+(def improve (fn [n guess] (ave guess (/ n guess))))
 
 (def sq (fn [x] (* x x)))
 
-(def ave (fn [x y] (/ (+ x y) 2)))
+(def abs (fn [x] (if (< (- x) 0) x (- x))))
+
+(def check (fn [n x tol] (< (abs (- (sq x) n)) tol)))
+
+(def root (fn [n x tol] (if (check n x tol) x (recur n (improve n x) tol))))
+
+
+;; examples
+
+(root 11 1. 0.01) ;; find sqrt 11 starting from 1 and make it good to 2 decimal places
