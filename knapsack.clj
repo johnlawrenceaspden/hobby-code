52e68d7e68d769cb82a23284b818bd0b773bb031
John Lawrence Aspden <github@aspden.com>
2013-09-18 21:23:11 +0100

    Knapsack Problem

--- knapsack.clj
+++ knapsack.clj
@@ -1,4 +1,4 @@
-;; Knapsack Problem : Dynamic Programming
+;; The Knapsack Problem
 
 ;; Suppose you've got twelve pounds
 (def budget 12)
@@ -19,22 +19,68 @@
 
 ;; What do you buy? 
 
-;; Well, if you're a cynic
-(evaluate
- (let [order   (sort-by :cost things)
-       baskets (reductions conj '() order)]
-   (last (take-while #(<= (price %) budget) baskets)))) ;-> 71
+(defn value [sorted-things]
+  (evaluate
+   (let [order   sorted-things
+         baskets (reductions conj '() order)]
+     (last (take-while #(<= (price %) budget) baskets)))))
 
+;; Well, if you're a cynic
+(value (sort-by :cost things)) ;-> 71
 ;; Then you come away with 71's worth
 
 ;; And if you're an idealist
-(evaluate
- (let [order   (reverse (sort-by :value things))
-       baskets (reductions conj '() order)]
-   (last (take-while #(<= (price %) budget) baskets)))) ;-> 91
+(value (reverse (sort-by :value things))) ;-> 91
+;; Then you do way better with 91
 
-;; Then you do slightly better
-(evaluate
- (let [order   (reverse (sort-by (fn [{:keys[value cost]}](/ value cost)) things))
-       baskets (reductions conj '() order)]
-   (last (take-while #(<= (price %) budget) baskets))))
+;; A more cunning approach is to take things in order of their price/value ratio
+(value (reverse (sort-by (fn [{:keys[value cost]}] (/ value cost)) things))) ;-> 71
+;; Sadly that does worse than the approach that only pays attention to the value.
+
+;; So it seems that out of the three natural-seeming 'greedy algorithms', the best solution is 91
+
+
+;; Yet another approach is to exhaustively search the space of possibilities:
+
+(defn subsets [things]
+  (if (empty? things) '(())
+      (let [srt (subsets (rest things))]
+        (concat (map #(cons (first things) %) srt) srt))))
+
+ 
+(reverse (sort-by second (for [i (subsets things)] [(price i) (evaluate i)]))) 
+;-> ([13 111] [12 91] [10 90] [10 81] [7 71] [9 70] [9 61] [7 60] [6 51] [4 50] [4 41] [6 40] [3 30] [3 21] [1 20] [0 0])
+
+;; Which tells us that the best combination is unaffordable, so we
+;; have to settle for the second best, which is paying 12 to get 91,
+;; which the idealist has been trying to tell us all along.
+
+;; But the idealistic approach is unlikely to work in the general case. 
+
+;; Consider a thing which is worth a lot, but horribly expensive, and
+;; lots of other things which are worth a fair bit and dirt cheap.
+
+;; Personally my money would have been on the 'buy things in order of
+;; price/value ratio' approach, but we above that that fails in at
+;; least one easy case.
+
+;; So it appears that if we are faced with a problem like this, ( and
+;; there are many such problems ), then we are doomed.
+
+;; Exhaustive search is not feasible once you've got more than a very
+;; few items, and yet the various greedy algorithms above get the
+;; wrong answers.
+
+;; And yet if you write down a knapsack problem like this, you will
+;; not find it appallingly difficult to pick the best arrangement.
+
+;; There is a certain tradition at this point of exclaiming 'The HUMAN
+;; BRAIN is performing a COMPUTATION INFEASIBLE for a CLASSICAL
+;; COMPUTER', and then going on to derive your favourite philosophical
+;; position on the nature of consciousness, which will miraculously
+;; turn out to be whatever it was you thought before you contemplated
+;; the problem in question.
+
+;; But wait ...
+
+
