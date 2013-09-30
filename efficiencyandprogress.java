56ace0e28b1dbd882062a476ccc3aaf82db35ff1
John Lawrence Aspden <github@aspden.com>
2013-09-30 02:16:56 +0100

    kill dead code

--- efficiencyandprogress.java
+++ efficiencyandprogress.java
@@ -3,33 +3,6 @@
 // java efficiencyandprogress
 
 
-/* Demonstration of dead-code elimination in the JVM.
- * 
- * This class demonstrates some of the difficulties of benchmarking
- * code which may be subject to dead-code elimination by the JVM.
- * There are two loops, both calling the same function. But only the
- * second loop actually uses the result of the function.
- *
- * When I run this code, I get results like this:
- *
- * Dead code:
- * Computed 10000.0 in 108 msecs
- * Computed 10000.0 in 22 msecs
- * Computed 10000.0 in 0 msecs      // dead-code elimination
- * Computed 10000.0 in 0 msecs
- * Computed 10000.0 in 0 msecs
- * 
- * Live code:
- * Computed 4.9995E11 in 92 msecs
- * Computed 4.9995E11 in 92 msecs
- * Computed 4.9995E11 in 96 msecs
- * Computed 4.9995E11 in 93 msecs
- * Computed 4.9995E11 in 92 msecs
- *
- * However, if I swap the order of the two loops, they both run at the
- * same speed! Maybe because by that point the JVM has already
- * optimized the 'sum' function so it doesn't bother to eliminate it.
- */
  
 public class efficiencyandprogress {
 
@@ -51,11 +24,14 @@ public class efficiencyandprogress {
             b[i]=i;
             
         }
-        for(count=0; count<100; count++){
+        for(count=0;count<100000; count++){
             for (i=0; i< N; i++) {
                 c[i]=a[i]+b[i];
             }
 
+            if(count/100*100==count) {
+                System.out.println("count" + count);
+            }
 
             for (i=0; i< N; i++) {
                 sum+=a[i];
@@ -67,41 +43,3 @@ public class efficiencyandprogress {
     }
 }
 
-/*
-    public static double sum(double[] array) {
-        double total = 0.0;
-        for (int i = 0; i < array.length; i++) {
-            total += array[i];
-        }
-        return total;
-    }
-        double[] array = new double[10000];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (double)i;
-        }
- 
-        System.out.println("Dead code:");
-        for (int i = 0; i < 5; i++) {
-            double grand_total = 0.0;
-            long start = System.currentTimeMillis();
-            for (int j = 0; j < 10000; j++) {
-                sum(array);
-                grand_total += 1.0;
-            }
-            long stop = System.currentTimeMillis();
-            System.out.println("Computed " + grand_total +
-                               " in " + (stop - start) + " msecs");
-        }
- 
-        System.out.println("\nLive code:");
-        for (int i = 0; i < 5; i++) {
-            double grand_total = 0.0;
-            long start = System.currentTimeMillis();
-            for (int j = 0; j < 10000; j++) {
-                grand_total += sum(array);
-            }
-            long stop = System.currentTimeMillis();
-            System.out.println("Computed " + grand_total +
-                               " in " + (stop - start) + " msecs");
-        }
-*/
