40e95a9a332bb92919184b16d4dea7a54e3b7479
John Lawrence Aspden <github@aspden.com>
2014-05-06 23:52:50 +0100

    slowly compressing

--- sdl/hello3.c
+++ sdl/hello3.c
@@ -1,16 +1,19 @@
 #include "SDL/SDL.h"
 
+// Install sdl with
+// sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+// although you probably actually need only:
+// sudo apt-get install libsdl1.2-dev
 // Compile with
 // gcc -std=gnu99 hello3.c -lSDL && ./a.out
 
 int main( int argc, char* args[] ) {
-  SDL_Surface* screen = NULL;
   //Start SDL
   SDL_Init( SDL_INIT_EVERYTHING );
 
   //Set up screen
-  screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
-  Uint32* pixels = (Uint32*)screen->pixels ;
+  SDL_Surface* screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+  Uint32* pixels = (Uint32*) screen->pixels ;
 
   // Draw here
   SDL_LockSurface(screen);
