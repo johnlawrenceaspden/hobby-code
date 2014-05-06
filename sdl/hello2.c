1c0d4261abedc3889eddc411f45dcf06d6f6a396
John Lawrence Aspden <github@aspden.com>
2014-05-06 23:38:52 +0100

    more sdl

--- sdl/hello2.c
+++ sdl/hello2.c
@@ -1,29 +1,17 @@
-// Stolen from http://www.facepunch.com/threads/919400
-
 #include "SDL/SDL.h"
 
-//#include <iostream>
-//#include <string>
+// Compile with
+// gcc -std=gnu99 -o hello hello2.c -lSDL && ./hello
 
-// Pixel functions (Contains color struct, nothing else yet)
-//#include "pixel.h"
-
-//using namespace std;
-
-// Make sure the Color struct imported from pixel.h
-//#ifndef COLOR_S
-//#define COLOR_S
 typedef struct  {
   Uint8 r, g, b;
 } Color;
-//#endif
 
-// Main
 int main( int argc, char* args[] ) {
 
   //Start SDL
   SDL_Init( SDL_INIT_EVERYTHING );
-  SDL_WM_SetCaption( "...", NULL );
+  SDL_WM_SetCaption( "SDL pixel plotting", NULL );
 
   // srand time
   srand( (unsigned)time(NULL) );
@@ -58,7 +46,6 @@ int main( int argc, char* args[] ) {
 
   // Pixels
   Uint32 *pixels = NULL;
-  int x=50, y=50;
 
   while (event.type != SDL_QUIT ) {
 
@@ -72,7 +59,9 @@ int main( int argc, char* args[] ) {
 
     // Show pixels
     SDL_LockSurface(screen);
-    pixels[x + y * screen->w] = SDL_MapRGB(screen->format, clr.r, clr.g, clr.b);
+    for(int x=50; x<300; x++)
+      for (int y=50; y<200; y++)
+        pixels[x + y * screen->w] = SDL_MapRGB(screen->format, clr.r, clr.g, clr.b);
     SDL_UnlockSurface(screen);
 
     // Poll event and update screen
@@ -80,8 +69,8 @@ int main( int argc, char* args[] ) {
     SDL_Flip( screen );
 
     // FPS Regulator
-    if (SDL_GetTicks() - tick < 1000 / 60)
-      SDL_Delay( ((1000/60) - (SDL_GetTicks() - tick) ) );
+    //    if (SDL_GetTicks() - tick < 1000 / 60)
+    //  SDL_Delay( ((1000/60) - (SDL_GetTicks() - tick) ) );
 
   }
 
