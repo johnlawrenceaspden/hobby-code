09a3f2d4e685f4dbd954c30ed9597ad1bbb4e58c
John Lawrence Aspden <github@aspden.com>
2014-05-06 23:04:57 +0100

    load a non-existent bitmap, but it still works

--- sdl/hello.c
+++ sdl/hello.c
@@ -4,9 +4,26 @@
 // g++ -o hello hello.c -lSDL
 
 int main( int argc, char* args[] ) {
-        //Start SDL
-        SDL_Init( SDL_INIT_EVERYTHING );
-        //Quit SDL
-        SDL_Quit();
-        return 0;
+  SDL_Surface* hello = NULL; 
+  SDL_Surface* screen = NULL; 
+  //Start SDL
+  SDL_Init( SDL_INIT_EVERYTHING );
+
+
+  //Set up screen 
+  screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE ); 
+  hello = SDL_LoadBMP( "hello.bmp" );
+
+  //Apply image to screen 
+  SDL_BlitSurface( hello, NULL, screen, NULL ); 
+  //Update Screen 
+  SDL_Flip( screen ); 
+  //Pause 
+  SDL_Delay( 2000 );
+  //Free the loaded image 
+  SDL_FreeSurface( hello ); 
+
+  //Quit SDL
+  SDL_Quit();
+  return 0;
 }
