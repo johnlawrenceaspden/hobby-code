da474f5d5d9285ba71b671ba5ade5dd9906d9de8
John Lawrence Aspden <github@aspden.com>
2014-05-07 22:38:49 +0100

    xscreensaver tutorial things

---
+++ simplesquares/simplesquares.c
@@ -0,0 +1,48 @@
+#include<stdlib.h>
+#include<X11/Xlib.h>
+
+#include "vroot.h"
+
+main ()
+{
+  Display *dpy;
+  Window root;
+  GC g;
+
+
+  /* open the display (connect to the X server) */
+  dpy = XOpenDisplay (getenv ("DISPLAY"));
+
+
+  /* get the root window */
+  root = DefaultRootWindow (dpy);
+
+
+  /* create a GC for drawing in the window */
+  g = XCreateGC (dpy, root, 0, NULL);
+
+
+  /* set foreground color */
+  XSetForeground(dpy, g, WhitePixelOfScreen(DefaultScreenOfDisplay(dpy)) );
+
+
+  /* draw something */
+  while (1)
+    {
+      /* draw a square */
+      XFillRectangle (dpy, root, g, random()%500, random()%500, 50, 40);
+
+
+      /* once in a while, clear all */
+      if( random()%500<1 )
+        XClearWindow(dpy, root);
+
+
+      /* flush changes and sleep */
+      XFlush(dpy);
+      usleep (10);
+    }
+
+
+  XCloseDisplay (dpy);
+}
