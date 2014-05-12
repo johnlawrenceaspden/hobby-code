49b6f4e4cc0999874263653f22b5c91ff8c735dc
John Lawrence Aspden <github@aspden.com>
2014-05-13 00:48:35 +0100

    aah, tutorial is not broken, ubuntu haskell is.

--- tutorial.hs
+++ tutorial.hs
@@ -6,9 +6,12 @@
 
 import Data.List
 import Data.Ord
+--- sudo apt-get install libghc-regex-posix-dev
+--- apparently on ubuntu 13.04 the haskell-platform is broken so:
+--- sudo apt-get install ghc alex cabal-install happy libghc-cgi-dev libghc-fgl-dev libghc-glut-dev libghc-haskell-src-dev libghc-html-dev libghc-http-dev libghc-hunit-dev libghc-mtl-dev libghc-network-dev libghc-opengl-dev libghc-parallel-dev libghc-parsec3-dev  libghc-quickcheck2-dev libghc-regex-base-dev libghc-regex-compat-dev  libghc-regex-posix-dev libghc-stm-dev libghc-syb-dev  libghc-text-dev  libghc-transformers-dev  libghc-xhtml-dev libghc-zlib-dev
 
---- import Text.Regex
---- import System.Random
+import Text.Regex
+import System.Random
 
 type Point     = (Float,Float)
 type Color     = (Int,Int,Int)
@@ -20,7 +23,7 @@ type Placement = [(Point,Person)]
 type EnergyFunction a              = a -> Int
 type TemperatureFunction           = Int -> Int -> Float
 type TransitionProbabilityFunction = Int -> Int -> Float -> Float
---- type MotionFunction a              = StdGen -> a -> (StdGen,a)
+type MotionFunction a              = StdGen -> a -> (StdGen,a)
 
 main = do
   putStr "Hello World! Let's have a picnic! \n"
@@ -48,3 +51,18 @@ main = do
   let rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
 
   writeFile "tut0.svg" $ writePolygons (blue [[(100,100),(200,100),(200,200),(100,200)],[(200,200),(300,200),(300,300),(200,300)]])
+
+  let readPoint :: String -> Point
+      readPoint s | Just [x,y] <- matchRegex (mkRegex "([0-9.]+),([0-9.]+)") s = (read x,read y)
+
+  let readPolygon :: String -> Polygon
+      readPolygon = (map readPoint).(splitRegex $ mkRegex " L ")
+
+  let readPolygons :: String -> [Polygon]
+      readPolygons = (map readPolygon).tail.(splitRegex $ mkRegex "<path")
+
+  park_data <- readFile "park.svg" 
+                      
+  let park = readPolygons park_data
+
+  writeFile "tut1.svg" $ writePolygons (green park)
