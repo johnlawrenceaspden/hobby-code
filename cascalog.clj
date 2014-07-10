543227033c04e37469256f73a83bc14df94d84a4
John Lawrence Aspden <github@aspden.com>
2014-07-10 01:28:30 +0100

    comment about how to get it to work

--- cascalog.clj
+++ cascalog.clj
@@ -1,3 +1,7 @@
+;; This fought with something in my user.clj 
+;; solution is to move the hidden ~/.lein directory to olddotlein
+;; so that the user.clj file doesn't get read in
+
 (use 'cascalog.api)
 (use 'cascalog.playground)
 (require '[cascalog.logic.def :as def])
