0479410760486111400fa4a4909534aa9e6b4d2d
John Lawrence Aspden <github@aspden.com>
2014-09-02 12:19:06 +0100

    open file in emacs from command line / thunar

---
+++ e.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FLUFFY="$@";
+SERVERSOCKET=/tmp/emacs${UID}/server;
+echo "trying to open: " $FLUFFY;
+echo " checking: " $SERVERSOCKET "for emacs server ";
+if [ -e $SERVERSOCKET ]; then
+    echo "using emacsclient";
+    emacsclient -n "$FLUFFY";
+else
+    echo "starting emacs: make tea...";
+    emacs --geometry 10x10 --fullscreen --no-splash "$FLUFFY" & disown;
+fi
