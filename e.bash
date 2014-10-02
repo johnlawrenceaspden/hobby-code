10a62a75d957f8ba3ad4fedcaca545763128c834
John Lawrence Aspden <github@aspden.com>
2014-10-02 12:32:20 +0100

    unquote argument means you can open file1 file2 rather than "file1 file2"

--- e.bash
+++ e.bash
@@ -6,8 +6,8 @@ echo "trying to open: " $FLUFFY;
 echo " checking: " $SERVERSOCKET "for emacs server ";
 if [ -e $SERVERSOCKET ]; then
     echo "using emacsclient";
-    emacsclient -n "$FLUFFY";
+    emacsclient -n $FLUFFY;
 else
     echo "starting emacs: make tea...";
-    emacs --geometry 10x10 --fullscreen --no-splash "$FLUFFY" & disown;
+    emacs --geometry 10x10 --fullscreen --no-splash $FLUFFY & disown;
 fi
