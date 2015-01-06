5795e2a6e9821aa49cc154f1dc49bb14e50e4d7d
John Lawrence Aspden <github@aspden.com>
2015-01-06 20:38:20 +0000

    correct image reading

--- firststeps.jl
+++ firststeps.jl
@@ -3,7 +3,7 @@
 # Our next task is to read in all the training images and make them into a big matrix
 
 using DataFrames
-using Images
+using Images # lots of warnings, but it's ok
 
 # Paths to our data files
 labelsfile="./trainLabels.csv"
@@ -11,33 +11,54 @@ imagedirectory="./trainResized/"
 
 @printf("Reading %s\n", labelsfile)
 labels=readtable(labelsfile)
-@printf("Read in %d labels from %s\n", size(labels)[2], labelsfile)
+no_of_images=size(labels)[1]
+@printf("Read in %d labels from %s\n", no_of_images, labelsfile)
 @printf("Image %i is of an %s\n",labels[1,1],labels[1,2])
 
-no_of_images=size(labels)[1]
+
 @printf("reading %s images\n", no_of_images)
 
 imageSize=20*20
 
+
+# The code on the kaggle website doesn't seem to work for me, but after a bit of hunting around, this is how to get the desired effect on the first image:
+image="$(imagedirectory)1.Bmp"
+img=imread(image)
+# turn our colour image into a greyscale image
+img_gs=convert(Image{Gray},img)
+# turn the specialized image format into an array of floats
+img_floats=reinterpret(Float32,float32(img_gs))
+# turn the 20x20 array into a 1x400 vector
+img_vec=reshape(img_floats,1,imageSize)
+
+# After all that, I feel the need to check I haven't buggered it up
+Pkg.add("ImageView")
+require("ImageView")
+# Should pop up a little grey 'n'
+ImageView.view(grayim(reshape(img_vec,20,20)))
+
+# So now we want to use that process to convert all the images into
+# one big array of image vectors
 #Create a gigantic array to put the images in
 x=zeros(no_of_images,imageSize)
 
-# We can iterate over a dataframe's columns by name
+# We can iterate over a dataframe's columns by name (it takes a while!)
 for (a,b) in enumerate(labels[:ID]);
     image="$(imagedirectory)$(b).Bmp"
     img=imread(image)
-    temp=float32(img)
-    assert(ndims(temp)==2)
-    assert(size(temp)==(20,20))
+    assert(size(img)==(20,20)) # paranoia
+    img_gs=convert(Image{Gray},img)
+    assert(size(img_gs)==(20,20))
+    img_floats=reinterpret(Float32,float32(img_gs))
+    assert(size(img_floats)==(20,20))
+    img_vec=reshape(img_floats,1,imageSize)
+    assert(size(img_vec)==(1,400))
+
     @printf("%s %s\n",a,image)
-    x[a,:]=reshape(temp,1,imageSize)
+    x[a,:]=img_vec
 end
 
 
-image="$(imagedirectory)$(1).Bmp"
-img=imread(image)
-img_gs=convert(Image{Gray},img)
-img_uint8=reinterpret(Uint8,data(img_gs))
 
 # The kaggle julia tutorial then tells us to do:
 ## RGB Image with:
