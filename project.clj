75189aebb821048350249c3f5f330f48432a47a9
John Lawrence Aspden <github@aspden.com>
2015-01-24 19:25:49 +0000

    add trace library

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license ""
   :url ""
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.trace "0.7.5"]
                  [simple-plotter "0.1.2"]
                  [nz.ac.waikato.cms.weka/weka-stable "3.6.12"]]
   :plugins [[cider/cider-nrepl "0.8.2"]]
