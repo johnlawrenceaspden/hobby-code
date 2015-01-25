28119916a27692792994a0128e6d80088707c826
John Lawrence Aspden <github@aspden.com>
2015-01-25 18:40:33 +0000

    assembler version of the gcdmachine

--- gcdmachine.asm
+++ gcdmachine.asm
@@ -1,16 +1,34 @@
-;;; hello world for assembler
+;;; assembly language version of:
+;; (define-machine gcd
+;;   (registers a b t)
+;;   (controller
+;;    loop
+;;    (branch (zero? (fetch b)) done)
+;;    (assign t (remainder (fetch a) (fetch b)))
+;;    (assign a (fetch b))
+;;    (assign b (fetch t))
+;;    (goto loop)
+;;    done))
+
 
 ;;; On 64 bit linux compile, link, run with these commands
 ;;; nasm -f elf64 hello.asm
-;;; ld -s -o hello hello.o
+;;; ld -o hello hello.o
 ;;; ./hello
+;;; or gdb hello
 
+
+        
+        
         sys_exit equ 0x01
         write    equ 0x04
         stdout   equ 0x01
 
         section .bss
         outputBuffer resb 4
+        a resb 4
+        b resb 4
+        t resb 4
         
         section .data
         msg db "GCD Machine",0x0a
@@ -30,10 +48,39 @@ _start:
         mov eax,write            ; system call number 4 (write)
         int 0x80                ; make system call
 
+        mov [a], dword 42      ; (2*3*7)
+        mov [b], dword 32      ; (2*2*2*2*2)
+
+
+loop:   
+        ;; if b is zero branch to done
+        cmp [b],dword 0
+        jz done
+
+        ;; divide a by b and put remainder in t
+        mov eax,[a]
+        mov edx,0
+        mov ebx,[b]
+        div ebx
+        mov [t],edx
+
+        ;; put b into a
+        mov eax,[b]
+        mov [a],eax
+
+        ;; put t into b
+        mov eax,[t]
+        mov [b],eax
+
+        ;; loop
+        jmp loop                
+        
+done:   
+printa:       
         ;; put value in outputBuffer
-        mov ecx,1
-        add ecx,0x30            ;ascii conversion
-        mov [outputBuffer],ecx
+        mov eax,[a]
+        add eax,0x30            ;ascii conversion
+        mov [outputBuffer],eax
 
         
         ;; write digit to stdout
@@ -43,6 +90,7 @@ _start:
         mov eax, write            ; system call number 4 (write)         
         int 0x80
 
+exit:   
         ;; exit with code 0
         mov ebx,0x00            ; return code 0
         mov eax,sys_exit            ; system call number 1 (exit)
--- makefile
+++ makefile
@@ -4,10 +4,10 @@ run: $(PROGRAM)
 	./$(PROGRAM)
 
 $(PROGRAM).o: $(PROGRAM).asm
-	nasm -f elf64 $(PROGRAM).asm
+	nasm -f elf64 -g $(PROGRAM).asm
 
 $(PROGRAM): $(PROGRAM).o
-	ld -s -o $(PROGRAM) $(PROGRAM).o
+	ld -o $(PROGRAM) $(PROGRAM).o
 
 # $(PROGRAM): $(PROGRAM).c
 # 	gcc --std=gnu99 $(PROGRAM).c -o $(PROGRAM)
