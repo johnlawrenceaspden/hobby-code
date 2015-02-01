c57e2611e05ffe41c1cb7c4cb27b73f53959fb8b
John Lawrence Aspden <github@aspden.com>
2015-02-01 17:24:27 +0000

    add stack, better test fails

--- registermachines.clj
+++ registermachines.clj
@@ -21,7 +21,7 @@
 ;; e.g. {:pc 2 :state {n 1} :controller [:begin (goto :begin)]}
 ;; do the right thing to create the successor state
 (do
-  (defn step [{:keys [pc state controller] :as machine}]
+  (defn step [{:keys [pc state controller stack] :as machine}]
     (print pc)
     ;; if the program counter is not a number, do nothing
     (cond (not (number? pc)) machine 
@@ -40,11 +40,14 @@
                                       (if (operation test state)
                                         (assoc machine :pc (.indexOf controller label))
                                         (assoc machine :pc npc)))
+                           restore      (if (empty? stack) (assoc machine :pc :underflow)
+                                                   (assoc machine :pc npc :stack (rest stack) :state (assoc state s1 (first stack))))
+                           save    (assoc machine :pc npc :stack (cons (state s1) stack))
                            (assoc machine :pc :error)))))))
   (mtest))
 
 
-(def basemachine {:pc 0  :state {} :controller '[] :dummy "hello"})
+(def basemachine {:pc 0  :state {} :controller '[] :dummy "hello" :stack '()})
 (def errmachine (assoc basemachine :controller '[(failzor)]))
 (def loopmachine (assoc basemachine :controller '[:begin (goto :begin)]))
 (def assignimmediatemachine  (assoc basemachine :controller '[(assign val 10)]))
@@ -55,27 +58,48 @@
 (def branchprogram '[(branch (> a b) :a>b) :a<=b :a>b])
 (def branchmachine    (assoc basemachine :state '{a 2 b 1} :controller branchprogram))
 (def nobranchmachine  (assoc basemachine :state '{a 1 b 2} :controller branchprogram))
+(def savemachine    (assoc basemachine :state '{n 1} :controller '[(save n)]))
+(def restoremachine (assoc basemachine :stack '(1) :controller '[(restore n)]))
+(def oopsrestoremachine (assoc basemachine :stack '() :controller '[(restore n)]))
+(def swapmachine (assoc basemachine :state '{a :keyword b "string"} :controller '[(save a)(save b)(restore a)(restore b)]))
+
+(require 'clojure.data)
+(defn check= [m1 m2]
+  (let [[a b c] (clojure.data/diff m1 m2)]
+    (if (and (nil? a)( nil? b))
+      true
+      [a b])))
+
+
+
+
 
 (defn mtest[]
   (list
    ;; halting
-   (= (step basemachine) (assoc basemachine :pc :halt))
-   (= (step (step basemachine)) (step basemachine))
+   (check= (step basemachine) (assoc basemachine :pc :halt))
+   (check= (step (step basemachine)) (step basemachine))
    ;; error state
-   (= (step errmachine) (assoc errmachine :pc :error))
-   (= (step (step errmachine)) (step errmachine))
+   (check= (step errmachine) (assoc errmachine :pc :error))
+   (check= (step (step errmachine)) (step errmachine))
    ;; labels and gotos
-   (= (step loopmachine) (assoc loopmachine :pc 1))
-   (= (step (step loopmachine)) loopmachine)
+   (check= (step loopmachine) (assoc loopmachine :pc 1))
+   (check= (step (step loopmachine)) loopmachine)
    ;; assignment
-   (= (step assignmachine) (assoc assignmachine :state '{val 10} :pc 1))
-   (= (step assignkeywordmachine) (assoc assignkeywordmachine :state '{val :keyword} :pc 1))
-   (= (step assignregistermachine1) (assoc assignregistermachine1 :state '{val nil} :pc 1))
-   (= (step assignregistermachine2) (assoc assignregistermachine2 :state '{val 1 doom 1} :pc 1))
-   (= (step assignoperationmachine) (assoc assignoperationmachine :state '{a 15 b 5} :pc 1))
+   (check= (step assignmachine) (assoc assignmachine :state '{val 10} :pc 1))
+   (check= (step assignkeywordmachine) (assoc assignkeywordmachine :state '{val :keyword} :pc 1))
+   (check= (step assignregistermachine1) (assoc assignregistermachine1 :state '{val nil} :pc 1))
+   (check= (step assignregistermachine2) (assoc assignregistermachine2 :state '{val 1 doom 1} :pc 1))
+   (check= (step assignoperationmachine) (assoc assignoperationmachine :state '{a 15 b 5} :pc 1))
    ;; branch
-   (= (step nobranchmachine)  (assoc nobranchmachine :pc 1))
-   (= (step branchmachine)    (assoc branchmachine :pc 2))))
+   (check= (step nobranchmachine)  (assoc nobranchmachine :pc 1))
+   (check= (step branchmachine)    (assoc branchmachine :pc 2))
+   ;; stack
+   (check= (step savemachine)    (assoc savemachine :stack '(1) :pc 1))
+   (check= (step restoremachine) (assoc restoremachine :state '{n 1} :stack '() :pc 1))
+   (check= (step oopsrestoremachine) (assoc oopsrestoremachine :pc :underflow))
+   (check= (step (step (step (step swapmachine)))) (assoc swapmachine :pc 4 :state '{b :keyword a "string"}))
+   ))
 
 (def iterative-factorial
   '[(assign product 1)                     ;0
