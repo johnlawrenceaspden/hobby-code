bbf88415e81d693ce17b4d8f68853002335e7d41
John Lawrence Aspden <github@aspden.com>
2015-02-15 15:33:12 +0000

    add ring session destructuring example

--- map-destructuring.clj
+++ map-destructuring.clj
@@ -34,3 +34,36 @@
 (let [{a :a {c :c d :d :as b} :b}
       {:a 1 :b {:c 2 :d 3}}]
   [a b c d]) ;-> [1 {:c 2, :d 3} 2 3]
+
+
+;; Finally a 'real' example, a ring request map containing parameters and a session, both of
+;; which have substructure
+
+(def ring-request
+  {:params {:action "a" :key "k" :spurious "sp"}
+   :session {:data "d" :state "s"}
+   :irrelevant "irr"})
+
+;; So the parameters we're interested in look like
+{:params {:action :key} :session {:data :state}}
+
+;; And we can extract all the pieces, naming each part, like so:
+
+(defn process-request [{{action :action key   :key   :as params } :params
+                        {data   :data   state :state :as session} :session :as request}]
+  (println action)
+  (println key)
+  (println data)
+  (println state)
+  (println params)
+  (println session)
+  (println request))
+
+(process-request ring-request)
+;; a
+;; k
+;; d
+;; s
+;; {:key k, :action a, :spurious sp}
+;; {:state s, :data d}
+;; {:irrelevant irr, :params {:key k, :action a, :spurious sp}, :session {:state s, :data d}}
