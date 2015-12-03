cf80e94de0c48e924678c5ff772fbfe732d390e2
John Lawrence Aspden <github@aspden.com>
2015-12-03 22:39:59 +0000

    various horrors working

--- gamblers/gamblers.clj
+++ gamblers/gamblers.clj
@@ -1,3 +1,9 @@
+
+;Manually starting an nrepl server
+;(require 'cider.nrepl)
+;(require 'clojure.tools.nrepl.server)
+;(clojure.tools.nrepl.server/start-server :port 7888 :handler cider.nrepl/cider-nrepl-handler)
+
 (defn coin [] (rand-nth [:H :T]))
 
 (coin)
---
+++ gamblers/project.clj
@@ -0,0 +1,11 @@
+(defproject hobby-code "0.0.0"
+  :description ""
+  :license ""
+  :url ""
+  :dependencies [[org.clojure/clojure "LATEST"]
+                 [org.clojure/tools.trace "LATEST"]
+                 [org.clojure/tools.nrepl "LATEST"]
+                 [simple-plotter "LATEST"]]
+  :plugins [[cider/cider-nrepl "0.10.0"]]
+  :global-vars {*print-length* 100})
+
