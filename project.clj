c7d2b5607ab709eb7ccd0efdae68fbdf1c5ee488
John Lawrence Aspden <github@aspden.com>
2016-06-15 15:21:27 +0100

    updated project.clj

--- project.clj
+++ project.clj
@@ -2,11 +2,23 @@
   :description ""
   :license ""
   :url ""
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :plugins [[cider/cider-nrepl "0.12.0"]]
+  :global-vars {*print-length* 100 *print-level* 100}
+  
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.trace "0.7.5"]
-                 [simple-plotter "0.1.2"]
-                 [nz.ac.waikato.cms.weka/weka-stable "3.6.12"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :plugins [[cider/cider-nrepl "0.8.2"]]
-  :global-vars {*print-length* 100})
+                 [com.cemerick/pomegranate "0.3.1"]
+;;                 [simple-plotter "0.1.2"]
+                 ])
+
+;; Examples of how to use pomegranate to load other libraries
+
+;;(require 'cemerick.pomegranate)
+
+;; (cemerick.pomegranate/add-dependencies
+;; :coordinates [[net.sourceforge.parallelcolt/parallelcolt "0.10.0"]])
+
+;; (cemerick.pomegranate/add-dependencies 
+;;  :coordinates '[[hiccup "1.0.5"]]
+;;  :repositories {"clojars" "http://clojars.org/repo" })
 
