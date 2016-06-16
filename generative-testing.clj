76bcb6bfb7ff0a6c16f04c0345235747a8567f80
John Lawrence Aspden <github@aspden.com>
2016-06-16 19:16:33 +0100

    generative test fun

--- generative-testing.clj
+++ generative-testing.clj
@@ -50,6 +50,84 @@ gen/vector ; #function[clojure.test.check.generators/vector]
   (gen/vector gen/boolean 3))) ; ([true \¾ \m [false false false]] [true \} \u [true true false]] [true \È \> [false false false]] [true \ú \6 [false true true]] [true \8 \" [false false false]] [true \ \~ [false true true]] [false \= \= [true false true]] [true \¤ \Z [false true false]] [false \ï \u [true false false]] [false \backspace \e [false true false]])
 
 
+;; OK, what can we do with them?
+
+;; Here's an example straight from the docs
+
+;; We say that sort should be idempotent on any vector of ints
+(def sort-idempotent-prop
+  (prop/for-all [v (gen/vector gen/int)]
+                (= (sort v) (sort (sort v)))))
+
+;; We can test this by generating 100 random vectors of ints and seeing if it always holds
+(tc/quick-check 100 sort-idempotent-prop)
+;; {:result true, :num-tests 100, :seed 1466100813479}
+
+;; Another property is that the list should be sorted
+(def prop-sorted
+   (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
+     (let [s (sort v)]
+       (apply < s))))
+
+;; Which is often true
+(tc/quick-check 1 prop-sorted)
+;; {:result true, :num-tests 1, :seed 1466100802862}
+
+;; But occasionally not true
+(tc/quick-check 10 prop-sorted)
+;; {:result false,
+;;  :seed 1466100766430,
+;;  :failing-size 5,
+;;  :num-tests 6,
+;;  :fail [[-3 -3 -1 -3]],
+;;  :shrunk
+;;  {:total-nodes-visited 9, :depth 2, :result false, :smallest [[-3 -3]]}}
+
+;; What's really nice here is that the original vector that failed was [-3 -3 -1 -3]
+(apply < (sort [-3 -3 -1 -3])) ; false
+
+;; But that quick check has managed to reduce that to a smaller test case
+(apply < (sort [-3 -3])) ; false
+
+;; Allowing us to see what's wrong
+(def prop-sorted
+   (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
+     (let [s (sort v)]
+       (apply <= s))))
+
+;; Our new property survives extensive trials
+(tc/quick-check 10000 prop-sorted) ; {:result true, :num-tests 10000, :seed 1466100962412}
+
+
+
+
+
+
+;; Alternatively, if we try to assert that the first element of a sorted vector is always smaller
+;; than the last element
+(def prop-sorted-first-less-than-last
+   (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
+     (let [s (sort v)]
+       (< (first s) (last s)))))
+
+(tc/quick-check 3 prop-sorted-first-less-than-last)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 (require '[com.gfredericks.test.chuck :as chuck])
@@ -88,18 +166,7 @@ gen/vector ; #function[clojure.test.check.generators/vector]
 ;; (require '[clojure.test.check.properties :as prop])
 
 
-;; (def sort-idempotent-prop
-;;   (prop/for-all [v (gen/vector gen/int)]
-;;                 (= (sort v) (sort (sort v)))))
 
-;; (tc/quick-check 100 sort-idempotent-prop)
-
-;; (def prop-sorted-first-less-than-last
-;;   (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
-;;     (let [s (sort v)]
-;;       (< (first s) (last s)))))
-
-;; (tc/quick-check 3 prop-sorted-first-less-than-last)
 
 
 ;; (def wason-prop
