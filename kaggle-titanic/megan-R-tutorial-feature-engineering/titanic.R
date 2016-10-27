15d2cc820a5ef472b047811bc746bba77d8e2fc1
John Lawrence Aspden <github@aspden.com>
2016-10-27 19:19:20 +0100

    megan's tutorial

--- kaggle-titanic/megan-R-tutorial-feature-engineering/titanic.R
+++ kaggle-titanic/megan-R-tutorial-feature-engineering/titanic.R
@@ -24,75 +24,38 @@ library('VIM')
 
 
 ## read in training and test data
-train <- read.csv('train.csv', stringsAsFactors = F)
-test  <- read.csv('test.csv', stringsAsFactors = F)
+train <- read.csv('../train.csv', stringsAsFactors = F)
+test  <- read.csv('../test.csv', stringsAsFactors = F)
 train_size <- nrow(train)
 test_size <- nrow(test)
 
 ## bind them and process them together, then split apart again
 full <- bind_rows(train, test)
-full$Sex <-factor(full$Sex)
-full$Pclass <-factor(full$Pclass)
-full$Survived <-factor(full$Survived)
-full$Embarked <-factor(full$Embarked)
-full$SibSp <-factor(full$SibSp)
-full$Parch <-factor(full$Parch)
-processed_train <- full[1:train_size,]
-processed_test  <- full[(train_size+1):(train_size+test_size),]
-
-## choose a random seed to make things repeatable
-set.seed(754)
-
-summary(processed_train)
-rf_model <- randomForest(Survived ~ Sex + Fare + Pclass + Embarked + SibSp + Parch,  data=processed_train)
-rf_model
-plot(rf_model)
-legend('topright', colnames(rf_model$err.rate), col=1:3, fill=1:3)
-
-importance    <- importance(rf_model)
-varImportance <- data.frame(Variables = row.names(importance), 
-                            Importance = round(importance[ ,'MeanDecreaseGini'],2))
-rankImportance <- varImportance %>%
-  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
-ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
-    y = Importance, fill = Importance)) +
-  geom_bar(stat='identity') + 
-  geom_text(aes(x = Variables, y = 0.5, label = Rank),
-    hjust=0, vjust=0.55, size = 4, colour = 'red') +
-  labs(x = 'Variables') +
-  coord_flip() + 
-  theme_few()
 
 
+# Grab title from passenger names
+full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
 
-
-
-
-
-
-
-rf_model <- randomForest(factor(Survived) ~ PassengerId + Pclass + Name + Sex + Age + SibSp + Parch + Ticket + Fare + Cabin + Embarked, data=train)
-
-processed_train_stripped <-subset(processed_train,select=-Age:-Ticket)
-md.pattern(processed_train_stripped)
-
-
-
-
-full$Title <- gsub('(.*, )|(\\..*)','',full$Name)
-
+# Show title counts by sex
 table(full$Sex, full$Title)
 
-rare_title <- c('Dona','Lady','the Countess','Capt','Col','Don','Dr','Major','Rev','Sir','Jonkheer')
+# Titles with very low cell counts to be combined to "rare" level
+rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
+                'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
 
-full$Title[full$Title == 'Mlle'] <- 'Miss'
-full$Title[full$Title == 'Ms'] <- 'Miss'
-full$Title[full$Title == 'Mme'] <- 'Mrs'
-full$Title[full$Title %in% rare_title] <- 'Rare Title'
+# Also reassign mlle, ms, and mme accordingly
+full$Title[full$Title == 'Mlle']        <- 'Miss' 
+full$Title[full$Title == 'Ms']          <- 'Miss'
+full$Title[full$Title == 'Mme']         <- 'Mrs' 
+full$Title[full$Title %in% rare_title]  <- 'Rare Title'
 
+# Show title counts by sex again
 table(full$Sex, full$Title)
 
-full$Surname <- sapply(full$Name,function(x) strsplit(x,split='[,.]')[[1]][1])
+# Finally, grab surname from passenger name
+full$Surname <- sapply(full$Name,  
+                      function(x) strsplit(x, split = '[,.]')[[1]][1])
+
 
 cat(paste('We have <b>', nlevels(factor(full$Surname)),'</b> unique surnames. I would be interested to infer ethnicity based on surname --- another time.'))
 
@@ -102,6 +65,7 @@ full$Fsize <- full$SibSp + full$Parch + 1
 ## Create a family variable
 full$Family <- paste(full$Surname, full$Fsize, sep='_')
 
+## Plot of family size vs survival
 ggplot(full[1:891,], aes(x = Fsize, fill = factor(Survived)))+
     geom_bar(stat='count', position='dodge')+
     scale_x_continuous(breaks=c(1:11))+
@@ -116,14 +80,23 @@ full$FsizeD[full$Fsize > 4] <- 'large'
 ## Show family size by survival using a mosaic plot
 mosaicplot(table(full$FsizeD,full$Survived), main='Family Size by Survival', shade=TRUE)
 
-full$Cabin
+# This variable appears to have a lot of missing values
+full$Cabin[1:28]
 
-strsplit(full$Cabin[2],NULL)[[1]]
+# The first character is the deck. For example:
+strsplit(full$Cabin[2], NULL)[[1]]
 
-full$Deck <-factor(sapply(full$Cabin, function(x) strsplit(x,NULL)[[1]][1]))
+# Create a Deck variable. Get passenger deck A - F:
+full$Deck<-factor(sapply(full$Cabin, function(x) strsplit(x, NULL)[[1]][1]))
 
-# Passengers 62 and 830 are missing embarkment data
-full[c(62,830),'Embarked']
+
+################################################################################
+## Missingness
+################################################################################
+
+# Passengers 62 and 830 are missing Embarkment
+full[c(62, 830), 'Embarked']
+
 
 cat(paste('We will infer their values for **embarkment** based on present data that we can imagine may be relevant: **passenger class** and **fare**. We see that they paid<b> $', full[c(62, 830), 'Fare'][[1]][1], '</b>and<b> $', full[c(62, 830), 'Fare'][[1]][2], '</b>respectively and their classes are<b>', full[c(62, 830), 'Pclass'][[1]][1], '</b>and<b>', full[c(62, 830), 'Pclass'][[1]][2], '</b>. So from where did they embark?'))
 
@@ -139,15 +112,143 @@ ggplot(embark_fare, aes(x=Embarked, y=Fare, fill=factor(Pclass))) +
 
 full$Embarked[c(62,830)] <- 'C'
 
+full[1044,]
+
+ggplot(full[full$Pclass == '3' & full$Embarked == 'S', ], 
+  aes(x = Fare)) +
+  geom_density(fill = '#99d6ff', alpha=0.4) + 
+  geom_vline(aes(xintercept=median(Fare, na.rm=T)),
+    colour='red', linetype='dashed', lwd=1) +
+  scale_x_continuous(labels=dollar_format()) +
+  theme_few()
+
+full$Fare[1044] <- median(full[full$Pclass == '3' & full$Embarked == 'S', ]$Fare, na.rm = TRUE)
+
+################################################################################
+## Predictive Imputation of Ages 
+################################################################################
+
+sum(is.na(full$Age))
+
+
+
+# Make variables factors into factors
+factor_vars <- c('PassengerId','Pclass','Sex','Embarked',
+                 'Title','Surname','Family','FsizeD')
+
+full[factor_vars] <- lapply(full[factor_vars], function(x) as.factor(x))
+
+# Set a random seed
+set.seed(129)
+
+# Perform mice imputation, excluding certain less-than-useful variables:
+mice_mod <- mice(full[, !names(full) %in% c('PassengerId', 'Name', 'Ticket', 'Cabin', 'Family', 'Surname', 'Survived')], method='rf')
+
+
+# Save the complete output 
+mice_output <- complete(mice_mod)
+
+
+# Plot age distributions
+par(mfrow=c(1,2))
+hist(full$Age, freq=F, main='Age: Original Data', 
+  col='darkgreen', ylim=c(0,0.04))
+hist(mice_output$Age, freq=F, main='Age: MICE Output', 
+  col='lightgreen', ylim=c(0,0.04))
+
+
+# Replace Age variable from the mice model.
+full$Age <- mice_output$Age
+
+# Show new number of missing Age values
+sum(is.na(full$Age))
+
+################################################################################
+## Feature Engineering 2
+################################################################################
+
+
+# First we'll look at the relationship between age & survival
+ggplot(full[1:891,], aes(Age, fill = factor(Survived))) + 
+  geom_histogram() + 
+  # I include Sex since we know (a priori) it's a significant predictor
+  facet_grid(.~Sex) + 
+  theme_few()
+
+# Create the column child, and indicate whether child or adult
+full$Child[full$Age < 18] <- 'Child'
+full$Child[full$Age >= 18] <- 'Adult'
+
+table(full$Child, full$Survived)
+
+# Adding Mother variable
+full$Mother <- 'Not Mother'
+full$Mother[full$Sex == 'female' & full$Parch > 0 & full$Age > 18 & full$Title != 'Miss'] <- 'Mother'
+
+#Show Counts
+table(full$Mother, full$Survived)
+
+# Finish by factorizing our two new factor variables
+full$Child  <- factor(full$Child)
+full$Mother <- factor(full$Mother)
+
+
+md.pattern(full)
+
+# Split the data back into a train set and a test set
+train <- full[1:891,]
+test <- full[892:1309,]
+
+# Set a random seed
+set.seed(754)
+
+# Build the model (note: not all possible variables are used)
+rf_model <- randomForest(factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + 
+                                            Fare + Embarked + Title + 
+                                            FsizeD + Child + Mother,
+                                            data = train)
+
+# Show model error
+plot(rf_model, ylim=c(0,0.36))
+legend('topright', colnames(rf_model$err.rate), col=1:3, fill=1:3)
+
+
+# Get importance
+importance    <- importance(rf_model)
+varImportance <- data.frame(Variables = row.names(importance), 
+                            Importance = round(importance[ ,'MeanDecreaseGini'],2))
+
+
+
+# Create a rank variable based on importance
+rankImportance <- varImportance %>%
+  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+
+
+# Use ggplot2 to visualize the relative importance of variables
+ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
+    y = Importance, fill = Importance)) +
+  geom_bar(stat='identity') + 
+  geom_text(aes(x = Variables, y = 0.5, label = Rank),
+    hjust=0, vjust=0.55, size = 4, colour = 'red') +
+  labs(x = 'Variables') +
+  coord_flip() + 
+  theme_few()
 
 
 
 
 
+# Predict using the test set
+prediction <- predict(rf_model, test)
 
-######################################################
-n = c(2, 3, NA) 
-s = c("aa", "bb", "cc") 
-b = c(TRUE, FALSE, TRUE) 
-df = data.frame(n, s, b)       # df is a data frame
+# Save the solution to a dataframe with two columns: PassengerId and Survived (prediction)
+solution <- data.frame(PassengerID = test$PassengerId, Survived = prediction)
+
+# Write the solution to file
+write.csv(solution, file = 'rf_mod_Solution.csv', row.names = F)
+
+
+
+
 
