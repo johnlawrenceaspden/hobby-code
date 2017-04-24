bd324a00c416a2be6bf619b32a34f97b7d4dc898
John Lawrence Aspden <github@aspden.com>
2017-04-24 21:29:07 +0100

    python debugging stuff

--- python-debuggers.py
+++ python-debuggers.py
@@ -1,31 +1,64 @@
 #!/usr/bin/env python
 
 # Run with
-# ./debug-test.py
-# python ./debug-test.py
+# ./python-debuggers.py
+# python ./python-debuggers.py
 
 # Command line debug 
-# python -m pdb debug-test.py
+# python -m pdb python-debuggers.py
 # type help for commands
 
 # Emacs debugger M-x pdb
-# Run pdb (like this): python -m pdb /home/jla/solar_capture/debug-test.py
+# Run pdb (like this): python -m pdb /home/jla/hobby-code/python-debuggers.py
 # goto *gud-pdb* window 
 
 # Or maybe better to use realgud
 # https://github.com/realgud/realgud/
 # M-x package-install RET realgud RET
 # M-x load-library RET realgud RET
+# and then as above.
 
 x=2
+
+
+class aclass(object):
+    def __init__(self, val):
+        self.data=val
+    def __repr__(self):
+        return "aclass({0.data})".format(self)
+
+
+class bclass(aclass):
+    def __init__(self, val, val2):
+        self.moredata=val2
+        super(bclass,self).__init__(val)
+    def __repr__(self):
+        return "bclass({0.data}, {0.moredata})".format(self)
+
+    
+
+
+def factorial(n):
+    if n<=1 :
+        return 1
+    else:
+        return n * factorial(n-1)
 
 #hello
 
 for i in range(10):
-    print x
+    print i
     print "hello"
+    print factorial(i)
     print "world"
 
+
+a=aclass(2)
+print a
+b=bclass(3,7)
+print b
+
+    
 print "bye"
 
 
