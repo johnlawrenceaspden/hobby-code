b8653e172da18e0e244dbf1e4891917acc97632f
John Lawrence Aspden <github@aspden.com>
2017-04-28 18:45:19 +0100

    python test frameworks

--- python-setup/hello.py
+++ python-setup/hello.py
@@ -10,29 +10,56 @@
 # nosetests hello.py
 # you can see which tests were run with:
 # nosetests -v hello.py
+# -vv tells you where nose is looking for tests
+# nosetests -vv hello.py
+# and you can include doctests
+# nosetests --with-doctest -vv hello.py
 
-import re
+import unittest
 
-EMAIL_REGEXP = r'[\S.]+@[\S.]+'
+# this is a nose test, it gets picked up by virtue of starting test_
+def test_b():     
+    assert 'b' == 'b'
 
-
-def test_email_regexp():
-    # a regular e-mail address should match
-    assert re.match(EMAIL_REGEXP, 'test@nowhere.com')
-
-    # no domain should fail
-    assert not re.match(EMAIL_REGEXP, 'test@')
-
-
+# this too
 class TestExampleTwo:
     def test_c(self):
         assert 'c' == 'c'
 
+# this is a classic unittest testcase, nose should pick it up too
+class ExampleTest(unittest.TestCase):
+    def test_b(self):
+        self.assert_(1 == 2)
 
-def test_b():
-    assert 'b' == 'b'
 
+# and this is a doctest, which nose will pick up if told to
+def multiply(a, b):
+    """
+    >>> multiply(3,4)
+    12
+    >>> multiply(-1, -1)
+    1
+    """
+    return a*b
+
+
+
+
+# here we run the various test frameworks programmatically
 if __name__ == '__main__':
     print "hello"
-    print re.match(r'[ab]*', "ababab")
-    print re.match(r'[ab]*', "abababa")
+
+    print "-------------nose"
+    import nose
+    nose.run(argv=['', '-vv', '--exe', '--with-doctest', './hello.py'])
+
+    print "-------------doctest"
+    import doctest
+    doctest.testmod()
+
+    print "------------unittest"
+    import unittest
+    try:
+        unittest.main()
+    except SystemExit, exc:
+        pass
