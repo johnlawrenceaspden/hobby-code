1ec0e0d8c3109688b16ef98269a9867c63c68b41
John Lawrence Aspden <github@aspden.com>
2017-05-12 14:16:44 +0100

    blah

--- rms-gdb-tutorial/hello.c
+++ rms-gdb-tutorial/hello.c
@@ -3,6 +3,25 @@
 
 /* cc hello.c -o hello
    ./hello
+   gdb hello
+
+   gdb complains about missing debug symbols
+   
+   gcc -g hello.c -o hello
+   gdb hello
+
+   gdb complains about missing debuginfos, and suggests (on fedora)
+
+   sudo dnf debuginfo-install glibc-2.23.1-11.fc24.x86_64
+   installs:
+   glibc-debuginfo.x86_64 2.23.1-11.fc24
+   glibc-debuginfo-common.x86_64 2.23.1-11.fc24
+   nss-softokn-debuginfo.x86_64 3.30.2-1.0.fc24
+
+   gdb hello 
+   now starts without complaints
+
+
  */
 
 int main(void){
