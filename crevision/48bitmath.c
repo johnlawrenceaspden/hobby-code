7bf280b3b5eb93e3b4a6d75cf80f01eccb13ca99
John Lawrence Aspden <github@aspden.com>
2017-05-24 23:03:30 +0100

    48 bit subtractions when you've only got uint64_t

---
+++ crevision/48bitmath.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<inttypes.h>
+#include<assert.h>
+
+
+
+/**
+
+https://stackoverflow.com/questions/44166714/in-c-how-do-i-calculate-the-signed-difference-between-two-48-bit-unsigned-integ 
+
+I've got two values from an unsigned 48bit nanosecond counter, which may wrap.
+
+I need the difference, in nanoseconds, of the two times.
+
+I think I can assume that the readings were taken at roughly the same time, so of the two possible answers I think I'm safe taking the smallest.
+
+They're both stored as uint64_t. Because I don't think I can have 48 bit types.
+
+I'd like to calculate the difference between them, as a signed integer (presumably int64_t), accounting for the wrapping.
+
+and so on.....
+
+*/
+
+/** 
+
+To examine the produced assembler 
+gcc -std=c99 -O3 -o 48bitmath 48bitmath.c
+objdump -d 48bitmath
+objdump -d 48bitmath | grep -C10 sub48 
+
+*/
+
+  
+/* Use noinline to stop gcc optimising the crap out of the various test functions */
+int64_t __attribute__((noinline)) sub48(uint64_t x, uint64_t y)
+{
+  uint64_t diff;
+  
+  diff=x-y;
+
+  int64_t shifteddiff;
+  shifteddiff=(diff << 16);
+  
+  int64_t sdiff;
+  sdiff= shifteddiff >> 16;
+  //sdiff= shifteddiff / 65536;
+
+  /* printf("x=%"PRIu64" y=%"PRIu64"\n",x,y); */
+
+  /* printf("x=%"PRIu64"(0x%"PRIx64")\n",x,x); */
+  /* printf("y=%"PRIu64"(0x%"PRIx64")\n",y,y); */
+  
+  /* printf("diff=%"PRIu64"(0x%"PRIx64")\n",diff,diff); */
+
+  /* printf("shifteddiff=%"PRId64"(0x%"PRIx64")\n",shifteddiff,shifteddiff); */
+
+  /* printf("sdiff=%"PRId64"(0x%"PRIx64")\n",sdiff,sdiff); */
+
+  /* printf("\n"); */
+
+  return sdiff;
+  
+}
+
+
+int main(int argc, char**argv){
+  uint64_t x;
+  uint64_t y;
+
+  int64_t diff;
+
+
+  x=5; y=3;
+  assert(sub48(x,y)==2);
+
+  x=3; y=5;
+  assert(sub48(x,y)==-2);
+
+  x=0xffffffffffff;
+  y=0x000000000000;
+  assert(sub48(x,y)==-1);
+  assert(sub48(y,x)==+1);
+
+  x=0xffffffffffff;
+  y=0x000000000002;
+  assert(sub48(x,y)==-3);
+  assert(sub48(y,x)==+3);
+
+  x=0xfffffffffff8;
+  y=0xfffffffffff9;
+  assert(sub48(x,y)==-1);
+  assert(sub48(y,x)==+1);
+
+  x=0x7fffffffffff;
+  y=0x800000000002;
+  assert(sub48(x,y)==-3);
+  assert(sub48(y,x)==+3);
+
+  x=0xfffffffffffff;
+  y=0xf000000000000;
+  assert(sub48(x,y)==-1);
+  assert(sub48(y,x)==+1);
+
+  /* this bit means that gcc can't precalculate all the results and actually has to produce a function */
+  srand(argc);
+  printf("%"PRIx64"\n",sub48((((uint64_t)rand()<<32)+rand()),((uint64_t)rand()<<32)+rand()));
+
+  
+  return 0;
+}
