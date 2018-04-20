7828d2af3f3f332524f987662a49242c95762182
John Lawrence Aspden <github@aspden.com>
2018-04-20 23:29:40 +0100

    minikanren

--- minikanren/icfp2017-minikanren/nada.rkt
+++ minikanren/icfp2017-minikanren/nada.rkt
@@ -1,5 +1,11 @@
 #lang icfp2017-minikanren/racket-scheme-compat
 
+;; Install the racket package icfp2017-minikanren
+;; and then copy its files around this file to get it to work
+;; cp -a ~/.racket/6.7/pkgs/icfp2017-minikanren/ ~/hobby-code/minikanren/
+
+;; There is likely a better way!
+
 ;; minikanren tutorial 
 ;; http://io.livecode.ch/learn/webyrd/webmk
 
@@ -37,46 +43,86 @@
          (== `(,a . ,res) ls)
          (appendo d s res))])))
 
-(print '(run 1 (q) (appendo '(a) '(b) '(a b)))) (print '->)
-(run 1 (q) (appendo '(a) '(b) '(a b)))
 
-;l = '(a)
-;s = '(b)
-;ls = '(a b)
+;; input l s ls
+;; can succeed if l is empty and s and ls are the same
+(run* (q) (appendo '() '(a b) '(a b)))
+;; if l is empty and either s or ls are variable then can succeed almost immediately
+(run* (q) (appendo '() q '(a b)))
+;; just becomes:
+(run* (q) (== q '(a b)))
+
+;; if l is not empty
+(run* (q) (appendo '(a) q '(a b)))
+
+;; then we're all
+(run* (q) (fresh (alpha beta res)
+                 (== `(,alpha . ,beta) '(a))
+                 (== `(,alpha . ,res) '(a b))
+                 (appendo beta q res)))
+
+;;; can we find alpha so that
+;alpha + beta is '(a)
+;alpha + res is '(a b)
+;beta + q is res
+
+
+
+
+
+
+
+;; otherwise
+
 ;
-;l=='() FAIL
 ;
-;are there:  a d res
-;s.t.
 ;
-;a d == '(a)
-;a res == '(a b)
-;and append d '(b) is res
+;(printf "(run 1 (q) (appendo '(a) '(b) '(a b)))) -> ")
 ;
-;a = '(a)
-;d = '()
-;res = b FAIL
+;(run 1 (q) (appendo '(a) '(b) '(a b)))
 ;
-;a = ()
-;d = '(a)
-;=> 
-;res = '(a b)
-;SUCCESS
+;
+;
+;;l = '(a)
+;;s = '(b)
+;;ls = '(a b)
+;;
+;;l=='() FAIL
+;;
+;;are there:  a d res
+;;s.t.
+;;
+;;a d == '(a)
+;;a res == '(a b)
+;;and append d '(b) is res
+;;
+;;a = '(a)
+;;d = '()
+;;res = b FAIL
+;;
+;;a = ()
+;;d = '(a)
+;;=> 
+;;res = '(a b)
+;;SUCCESS
+;
+;
+;
+;
+;
+;(run 1 (q) (appendo '(a) '(b) q))
+;(run 1 (q) (appendo q '(b) '(a b)))
+;(run 1 (q) (appendo '(a) q '(a b)))
+;
+;
+;
+;
+;
+;
+;(run 1 (q) (fresh (x y z) (== x z) (== 3 y)))
+;
+;
+;
 
 
-
-
-
-(run 1 (q) (appendo '(a) '(b) q))
-(run 1 (q) (appendo q '(b) '(a b)))
-(run 1 (q) (appendo '(a) q '(a b)))
-
-
-
-
-
-
-(run 1 (q) (fresh (x y z) (== x z) (== 3 y)))
-
-
-
+(run* (x y) (appendo x y '(a b c d e)))
--- nada.scm
+++ nada.scm
@@ -1,6 +1,19 @@
 #lang racket
 
+(require minikanren)
+
+;; #lang icfp2017-minikanren
+;; #lang icfp2017-minikanren/racket-scheme-compat
+
 ;; http://io.livecode.ch/learn/gregr/icfp2017-artifact-auas7pp
+
+;; https://pkgs.racket-lang.org/package/icfp2017-minikanren
+
+;; Install minikanren package File->Install Package in Dr Racket
+
+;;(require icfp2017-minikanren)
+
+
 
 (cdr '(Hark! I love you))
 
@@ -8,6 +21,8 @@
 
 (match #t [#t '(I love you)][#f "boo"])
 
+
+;; append as normally written
 (define append 
   (lambda (l s)
     (cond 
@@ -16,6 +31,7 @@
 
 (append '(a b c) '(d e f))
 
+;; append slightly restructured
 (define append1 
   (lambda (l s)
     (cond 
@@ -26,3 +42,21 @@
               (cons a res))])))
 
 (append1 '(a b c) '(d e f))
+
+;; minikanren version
+(define appendo
+  (lambda (l s ls)
+    (conde 
+     [ (== '() l) (== s ls)]
+     [ (fresh (a d res)
+              (== `(,a . ,d) l)
+              (== `(,a . ,res) ls)
+              (appendo d s res))])))
+
+
+(append '(a b c) '(d e))
+(run* (q) (appendo '(a b c) '(d e) q))
+
+(run* (x) (appendo x x '(a b c a b c)))
+
+
