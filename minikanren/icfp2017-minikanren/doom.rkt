4542dd9840b589d836968da495afb55357c37630
John Lawrence Aspden <github@aspden.com>
2018-04-20 15:11:45 +0100

    more minikanren

--- minikanren/icfp2017-minikanren/doom.rkt
+++ minikanren/icfp2017-minikanren/doom.rkt
@@ -20,5 +20,38 @@
   (fresh (x y)
     (== 4 x)
     (== x y))
-  (== 3 y))
+  (== 7 y))
 
+(run 1 (x) (== 4 3))
+
+(run 1 (x) (== 5 x) (== 6 x))
+
+(run 2 (q)
+  (fresh (w x y)
+    (conde
+      ((== `(,x ,w ,x) q)
+       (== y w))
+      ((== `(,w ,x ,w) q)
+       (== y w)))))
+
+(run 5 (q)
+     (let loop ()
+     (conde ((== #f q))
+            ((== #t q))
+            ((loop)))))
+
+
+
+(run 5 (q)
+     (conde ((== #f q))
+            ((== #t q))
+            ((== #f q))))
+
+
+(run 10 (q)
+     (conde ((== #f q))
+            ((== #t q))
+            ((conde ((== #f q))
+                    ((== 'yo q))
+                    ((conde ((== #f q) (== #t q))
+                            ((== 'oy q))))))))
