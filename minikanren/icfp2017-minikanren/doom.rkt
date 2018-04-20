f676d78437aae5ab58a21916c942f60fdb00e3ff
John Lawrence Aspden <github@aspden.com>
2018-04-20 14:38:47 +0100

    minikanren examples

--- minikanren/icfp2017-minikanren/doom.rkt
+++ minikanren/icfp2017-minikanren/doom.rkt
@@ -2,11 +2,6 @@
 
 (require "mk/mk.rkt" "mk/test-check.rkt")
 
-(load "mk/test-check.scm")
-(load "mk/mk.scm")
-
-
-
 (run 1 (q) (fresh (x y z) (== x z) (== 3 y)))
 
 (run 1 (y)
@@ -14,6 +9,16 @@
     (== x z)
     (== 3 y)))
 
+(run 1 (q)
+     (fresh (x y)        
+            (== y x)
+            (== y q)
+            (== x 3)))
 
-;;(include "intro-examples.scm")
-;;(declare-racket-wrapper-for "intro-examples.scm")
+
+(run 1 (y)
+  (fresh (x y)
+    (== 4 x)
+    (== x y))
+  (== 3 y))
+
