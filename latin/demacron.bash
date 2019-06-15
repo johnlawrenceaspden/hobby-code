28a14dc16d1637c99fbeb2e6e83ae18ec652bdec
John Lawrence Aspden <github@aspden.com>
2019-06-15 21:21:14 +0100

    pesky capitals

--- latin/demacron.bash
+++ latin/demacron.bash
@@ -1,1 +1,1 @@
-sed -e 's/ō/o/g' -e 's/ā/a/g' -e 's/ī/i/g' -e 's/ē/e/g' -e 's/ū/u/g'  < aeneidI.txt >aeneidI-demacronned.txt
+sed -e 's/Ā/A/g' -e 's/Ē/E/g' -e 's/Ī/I/g' -e 's/Ū/U/g' -e 's/Ō/O/g' -e 's/ō/o/g' -e 's/ā/a/g' -e 's/ī/i/g' -e 's/ē/e/g' -e 's/ū/u/g'  < aeneidI.txt >aeneidI-demacronned.txt
