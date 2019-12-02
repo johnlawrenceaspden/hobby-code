13ab2ec066dde82e50400c89a9c8beb6094163b9
John Lawrence Aspden <github@aspden.com>
2019-12-02 18:12:13 +0000

    plot

--- python-plot/hello.py
+++ python-plot/hello.py
@@ -1,7 +1,7 @@
-from pyparsing import Word, alphas
+import matplotlib.pyplot as plt
+import numpy as np
 
-greet = Word(alphas) + "," + Word(alphas) + "!"
-hello = "Hello, World!"
-
-print(hello, "->", greet.parseString(hello))
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
 
