34dd13462118294f5f030af81105a39c9359929e
John Lawrence Aspden <github@aspden.com>
2019-12-04 18:12:57 +0000

    tutorial example of running average

---
+++ python-sandbox/generators.py
@@ -0,0 +1,34 @@
+import random
+
+def get_data():
+    """Return 3 random integers between 0 and 9"""
+    return random.sample(range(10), 3)
+
+def consume():
+    """Displays a running average across lists of integers sent to it"""
+    running_sum = 0
+    data_items_seen = 0
+
+    while True:
+        data = yield
+        data_items_seen += len(data)
+        running_sum += sum(data)
+        print('The running average is {}'.format(running_sum / float(data_items_seen)))
+
+def produce(consumer):
+    """Produces a set of values and forwards them to the pre-defined consumer
+    function"""
+    while True:
+        data = get_data()
+        print('Produced {}'.format(data))
+        consumer.send(data)
+        yield
+
+if __name__ == '__main__':
+    consumer = consume()
+    consumer.send(None)
+    producer = produce(consumer)
+
+    for _ in range(10):
+        print('Producing...')
+        next(producer)
