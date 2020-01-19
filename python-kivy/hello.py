947e383e0a64b8ad31bbe8c93bd4d5047a96d53f
John Lawrence Aspden <github@aspden.com>
2020-01-19 17:26:36 +0000

    kivy hello world

--- python-kivy/hello.py
+++ python-kivy/hello.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python3
-print("Remember to set\nKIVY_VIDEO=ffpyplayer\n before running things")
-
-import os
-print("KIVY_VIDEO=", os.environ.get('KIVY_VIDEO'))
-
+#!/usr/bin/env python 
 from kivy.app import App #We need to import the bits of kivy we need as we need them as importing everything would slow the app down unnecessarily
 from kivy.uix.widget import Widget #this is a thing that you want the App to display
+from kivy.uix.label import Label #this will import the code for the label in which we want to display Hello World! 
 
 
-class Lesson0(Widget): #this defines the instance of the widget. 
-    pass # pass is used to keep the class valid but allow it not to contain anything - At the moment our widget is not defined.
-
-
-class MyApp(App):
+class HelloWorldApp(App):
     def build(self):
-        return Lesson0()
+        lbl=Label(text='Hello World!') #lbl is a variable name being assigned the Label definition
+        return lbl #This  must match the name of the Widget you want to appear on screen
 
 if __name__ == '__main__': #Documentation suggests that each program file should be called main.py but I think that only matters if you're creating the final App to go onto a phone or tablet we're a long way off from that yet
 
-    MyApp().run() #This must match the name of your App
+    HelloWorldApp().run() #This must match the name of your App
