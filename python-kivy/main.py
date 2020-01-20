fc4204a9fa33aa24b271b4f8b450c621c5f48620
John Lawrence Aspden <github@aspden.com>
2020-01-20 13:17:23 +0000

    hello world suffers requirements creep

--- python-kivy/main.py
+++ python-kivy/main.py
@@ -8,22 +8,27 @@ import kivy
 
 # See:
 # https://kivy.org/doc/stable/guide/basic.html
+# https://nmilosev.svbtle.com/installing-kivy-on-fedora-29
 
 from kivy.app import App #We need to import the bits of kivy we need as we need them as importing everything would slow the app down unnecessarily
 from kivy.uix.widget import Widget #this is a thing that you want the App to display
-from kivy.uix.label import Label #this will import the code for the label in which we want to display Hello World! 
+from kivy.uix.label import Label #this will import the code for the label in which we want to display Hello World!
+import kivy.uix.boxlayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+import kivy.uix.button
+
 
 class LoginScreen(GridLayout):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-
-        
         self.username = TextInput(multiline=False)
         self.password = TextInput(password=True, multiline=False)
+        self.button = kivy.uix.button.Button(text="Click Me.")
+        self.button.bind(on_press=self.displayMessage)
+        self.label = kivy.uix.label.Label(text="Not logged in.")
 
         self.cols = 2
         #order of addition is significant for widget placement
@@ -31,7 +36,11 @@ class LoginScreen(GridLayout):
         self.add_widget(self.username)
         self.add_widget(Label(text='password'))
         self.add_widget(self.password)
+        self.add_widget(self.button)
+        self.add_widget(self.label)
 
+    def displayMessage(self, btn):
+        self.label.text = "that's close enough for government work" + "\n"+self.username.text+"\n"+self.password.text
 
 
 
@@ -42,5 +51,33 @@ class HelloWorldApp(App):
 if __name__ == '__main__': #Documentation suggests that each program file should be called main.py but I think that only matters if you're creating the final App to go onto a phone or tablet we're a long way off from that yet
 
     HelloWorldApp().run() #This must match the name of your App
+
+
+## just dropping off the end will do, but want to keep scratch code below
+exit()
+## dead code with any luck
+assert(False)
+
+    
+    
+class SimpleApp(kivy.app.App):
+    def build(self):
+        self.textInput = kivy.uix.textinput.TextInput()
+        self.label = kivy.uix.label.Label(text="Your Message.")
+        self.button = kivy.uix.button.Button(text="Click Me.")
+        self.button.bind(on_press=self.displayMessage)
+        self.boxLayout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
+        self.boxLayout.add_widget(self.textInput)
+        self.boxLayout.add_widget(self.label)
+        self.boxLayout.add_widget(self.button)
+        return self.boxLayout
+
+    def displayMessage(self, btn):
+        self.label.text = self.textInput.text
+
+if __name__ == "__main__":
+    simpleApp = SimpleApp()
+    simpleApp.run()
+
 
 
