c59eb5fe507a5089c0b605b43e732a9bb9682cf9
John Lawrence Aspden <github@aspden.com>
2020-01-21 17:03:43 +0000

    remove stuff at end doesn't help

--- python-kivy/main.py
+++ python-kivy/main.py
@@ -84,31 +84,3 @@ if __name__ == '__main__': #Documentation suggests that each program file should
     HelloWorldApp().run() #This must match the name of your App
 
 
-## just dropping off the end will do, but want to keep scratch code below
-exit()
-## dead code with any luck
-assert(False)
-
-    
-    
-class SimpleApp(kivy.app.App):
-    def build(self):
-        self.textInput = kivy.uix.textinput.TextInput()
-        self.label = kivy.uix.label.Label(text="Your Message.")
-        self.button = kivy.uix.button.Button(text="Click Me.")
-        self.button.bind(on_press=self.displayMessage)
-        self.boxLayout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
-        self.boxLayout.add_widget(self.textInput)
-        self.boxLayout.add_widget(self.label)
-        self.boxLayout.add_widget(self.button)
-        return self.boxLayout
-
-    def displayMessage(self, btn):
-        self.label.text = self.textInput.text
-
-if __name__ == "__main__":
-    simpleApp = SimpleApp()
-    simpleApp.run()
-
-
-
