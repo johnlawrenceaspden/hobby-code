8d7a7bd31a62977cc448a9a0c859ade8ba79f2b7
John Lawrence Aspden <github@aspden.com>
2020-01-22 15:30:24 +0000

    kivy itself no longer supports python2, giving up

--- python2-kivy/main.py
+++ python2-kivy/main.py
@@ -1,4 +1,35 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == '__main__':
+    PongApp().run()
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/usr/bin/env python
+
+
+
 
 
 # See:
