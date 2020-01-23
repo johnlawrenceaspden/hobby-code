ebf57ed866bf65c56c7c769136be4ebae5d5fbee
John Lawrence Aspden <github@aspden.com>
2020-01-23 12:30:44 +0000

    back to basics

--- python-kivy/main.py
+++ python-kivy/main.py
@@ -6,20 +6,11 @@
 # https://kivy.org/doc/stable/guide/basic.html
 # https://nmilosev.svbtle.com/installing-kivy-on-fedora-29
 
-
 from kivy.app import App
 
-from kivy.uix.label import Label
+class YourApp(App):
 
-class HelloWorldApp(App):
     def build(self):
-        root_widget = Label()
-        root_widget.text = 'Hello world!'
-        root_widget.font_size='100sp'
-        root_widget.italic=True
-        root_widget.markup=True
-        root_widget.text = '[color=#ff0000]Hello[/color] [color=#00ff00]world![/color]'
+        return None
 
-        return root_widget
-
-HelloWorldApp().run()
+YourApp().run()
