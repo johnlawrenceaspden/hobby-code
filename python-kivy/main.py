c2c6c9ca852c53ae7e83436a398a3c93f6b12790
John Lawrence Aspden <github@aspden.com>
2020-01-23 12:37:41 +0000

    calculator app

--- python-kivy/main.py
+++ python-kivy/main.py
@@ -5,12 +5,36 @@
 # See:
 # https://kivy.org/doc/stable/guide/basic.html
 # https://nmilosev.svbtle.com/installing-kivy-on-fedora-29
-
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+
 
 class YourApp(App):
-
     def build(self):
-        return None
+        root_widget = BoxLayout(orientation='vertical')
+
+        output_label = Label(size_hint_y=1)
+
+        button_symbols = ('1', '2', '3', '+',
+                          '4', '5', '6', '-',
+                          '7', '8', '9', '.',
+                          '0', '*', '/', '=')
+
+        button_grid = GridLayout(cols=4, size_hint_y=2)
+        for symbol in button_symbols:
+            button_grid.add_widget(Button(text=symbol))
+
+        clear_button = Button(text='clear', size_hint_y=None,
+                              height=100)
+
+        root_widget.add_widget(output_label)
+        root_widget.add_widget(button_grid)
+        root_widget.add_widget(clear_button)
+
+        return root_widget
+
 
 YourApp().run()
