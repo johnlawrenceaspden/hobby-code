c104c1c03ca0fc09ba22eed64c087092afff9004
John Lawrence Aspden <github@aspden.com>
2020-04-22 11:22:02 +0100

    twat script modified for dell-3521 since that's now carry laptop

--- twat.bash
+++ twat.bash
@@ -1,9 +1,9 @@
-reset 
-sync
+#reset 
+#sync
 sudo pkill -9 supplicant
-sudo rmmod wl 
-sudo modprobe wl 
-sudo iwconfig wlp2s0 power off 
+sudo rmmod ath9k
+sudo modprobe ath9k
+#sudo iwconfig wlp2s0 power off 
 sudo /sbin/wpa_supplicant -u -s -O /run/wpa_supplicant &
 disown
-sudo tail -f /var/log/syslog
+#sudo tail -f /var/log/syslog
