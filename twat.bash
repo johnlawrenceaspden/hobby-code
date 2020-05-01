f7992b86ac0de7ece8fbf1ca41477cc9df9b4b9c
John Lawrence Aspden <github@aspden.com>
2020-05-01 16:28:51 +0100

    faster works too

--- twat.bash
+++ twat.bash
@@ -2,13 +2,17 @@
 #sync
 
 nmcli radio wifi off
+#sleep 1
 nmcli networking off
+#sleep 1
 sudo pkill -9 supplicant
 sudo rmmod ath9k
 sudo modprobe ath9k
 #sudo iwconfig wlp2s0 power off 
 sudo /sbin/wpa_supplicant -u -s -O /run/wpa_supplicant &
+#sleep 1
 nmcli networking on
+#sleep 1
 nmcli radio wifi on
 #disown
 #sudo tail -f /var/log/syslog
