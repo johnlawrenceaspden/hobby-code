b24eb21853dd93c955e17007022475d2d82f57b6
John Lawrence Aspden <github@aspden.com>
2020-05-01 16:06:51 +0100

    twat script

--- twat.bash
+++ twat.bash
@@ -1,9 +1,14 @@
 #reset 
 #sync
+
+nmcli radio wifi off
+nmcli networking off
 sudo pkill -9 supplicant
 sudo rmmod ath9k
 sudo modprobe ath9k
 #sudo iwconfig wlp2s0 power off 
 sudo /sbin/wpa_supplicant -u -s -O /run/wpa_supplicant &
+nmcli networking on
+nmcli radio wifi on
 #disown
 #sudo tail -f /var/log/syslog
