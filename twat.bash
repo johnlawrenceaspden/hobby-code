7969470f53cb0707fdd00a3d2c4792e1c80003f3
John Lawrence Aspden <github@aspden.com>
2020-05-09 18:28:04 +0100

    yanking the driver doesn't seem to help

--- twat.bash
+++ twat.bash
@@ -6,8 +6,8 @@ nmcli radio wifi off
 nmcli networking off
 #sleep 1
 sudo pkill -9 supplicant
-sudo rmmod ath9k
-sudo modprobe ath9k
+#sudo rmmod ath9k
+#sudo modprobe ath9k
 #sudo iwconfig wlp2s0 power off 
 sudo /sbin/wpa_supplicant -u -s -O /run/wpa_supplicant &
 #sleep 1
