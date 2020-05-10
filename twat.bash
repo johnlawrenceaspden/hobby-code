a817ec54e42c7cfbeca36b3384420a4835f17211
John Lawrence Aspden <github@aspden.com>
2020-05-10 23:40:58 +0100

    remove excessive debugging

--- twat.bash
+++ twat.bash
@@ -9,7 +9,7 @@ sudo pkill -9 supplicant
 #sudo rmmod ath9k
 #sudo modprobe ath9k
 #sudo iwconfig wlp2s0 power off 
-sudo /sbin/wpa_supplicant -dd -u -s -c/etc/wpa_supplicant.conf -O /run/wpa_supplicant &
+sudo /sbin/wpa_supplicant -u -s -c/etc/wpa_supplicant.conf -O /run/wpa_supplicant &
 #sudo /sbin/wpa_supplicant -dd -u -s -O /run/wpa_supplicant &
 #sleep 1
 nmcli networking on
