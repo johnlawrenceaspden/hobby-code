8bf1af5111804099e1893216df3455144cf9e113
John Lawrence Aspden <github@aspden.com>
2020-05-18 15:10:34 +0100

    ok this is your last warning motherfucker

--- twattingloop.bash
+++ twattingloop.bash
@@ -14,7 +14,7 @@ while true;
 		       sleep 1 ; 
 	           else 
 		       play -q -n synth 0.1 sin 880 vol 0.99 ; 
-		       echo sudo /home/john/hobby-code/twat.bash; 
+		       sudo /home/john/hobby-code/twat.bash; 
 		       sleep 10 ; 
 	       fi ;
 	       fi ;
