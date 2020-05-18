6e2fa486d6c1f911d2c7deaeaf5201d7130f5d3b
John Lawrence Aspden <github@aspden.com>
2020-05-18 15:28:46 +0100

    make comments executable, do not repeat yourself twice

--- twattingloop.bash
+++ twattingloop.bash
@@ -1,23 +1,27 @@
 while true; 
 	do if ping -c1 -W1 -n -v 8.8.8.8; 
 	   then
-	       sleep 1 ; # all is well, back to sleep 
+	       echo ping successful
 	   else
-               # a packet went missing!
+               echo a packet went missing!
                play -q -n synth 0.1 sin 880 vol 0.009 ; #very quiet beep, we're just doing some tests
                if ping -c1 -W1 -n -v 8.8.8.8; 
 	       then 
-		   sleep 1 ; # all is well, back to sleep
-	       else 
-                   play -q -n synth 0.1 sin 880 vol 0.09 ; # coincidence?
+		   echo re-ping successful
+	       else
+                   echo coincidence?
+                   play -q -n synth 0.1 sin 880 vol 0.09 ; 
                    if ping -c1 -W1 -n -v 8.8.8.8; 
-	           then 
-		       sleep 1 ; # all is well, back to sleep
-	           else 
-		       play -q -n synth 0.1 sin 880 vol 0.99 ; # enemy action! battle stations!
-		       sudo /home/john/hobby-code/twat.bash; 
-		       sleep 10 ; # give it a while to recover before going back on watch
-	       fi ;
+	           then
+                       echo re-re-ping successful
+	           else
+                       echo enemy action! battle stations!
+		       play -q -n synth 0.1 sin 880 vol 0.99 ; 
+		       sudo /home/john/hobby-code/twat.bash;
+                       echo give it a while to recover before going back on watch
+		       sleep 10 ; 
+	           fi ;
 	       fi ;
 	   fi ;
+        sleep 1 ; # all is well, back to sleep
        	done
