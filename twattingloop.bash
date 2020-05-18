d9f46f7b86cf0d4fc48d1d13a215e31c0e169e45
John Lawrence Aspden <github@aspden.com>
2020-05-18 15:10:08 +0100

    if it's very very very bad

--- twattingloop.bash
+++ twattingloop.bash
@@ -8,9 +8,15 @@ while true;
 	       then 
 		   sleep 1 ; 
 	       else 
-		   play -q -n synth 0.1 sin 880 vol 0.099 ; 
-		   echo sudo /home/john/hobby-code/twat.bash; 
-		   sleep 10 ; 
+                   play -q -n synth 0.1 sin 880 vol 0.09 ; 
+                   if ping -c1 -W1 -n -v 8.8.8.8; 
+	           then 
+		       sleep 1 ; 
+	           else 
+		       play -q -n synth 0.1 sin 880 vol 0.99 ; 
+		       echo sudo /home/john/hobby-code/twat.bash; 
+		       sleep 10 ; 
+	       fi ;
 	       fi ;
 	   fi ;
        	done
