07818378dc2ca6d5baf6d99ec7c8f95625fc5d84
John Lawrence Aspden <github@aspden.com>
2020-05-19 10:15:34 +0100

    tidy twat

--- twat.bash
+++ twat.bash
@@ -1,23 +1,31 @@
-set +x
-#reset 
 
-#sync
-
-#nmcli connection down eduroam
 nmcli radio wifi off
-#nmcli networking off
+
 sleep 1
 sudo pkill -9 supplicant
 sudo rmmod ath9k
 sudo modprobe ath9k
-#sudo iwconfig wlp2s0 power off 
+
 sudo /sbin/wpa_supplicant -B -u -s -c/etc/wpa_supplicant.conf -O /run/wpa_supplicant &
+
+nmcli radio wifi on
+
+
+
+
+
+#sudo iwconfig wlp2s0 power off 
+
+#nmcli connection down eduroam
+#nmcli connection up eduroam
+
+#nmcli networking off
+#nmcli networking on
+
 #sudo /sbin/wpa_supplicant -dd -u -s -O /run/wpa_supplicant &
 #disown
-#nmcli networking on
-nmcli radio wifi on
-#nmcli connection up eduroam
 #sudo tail -f /var/log/syslog
+
 
 # mighty loop
 # while true; do if ping -q -c1 8.8.8.8; then sleep 1 ; else play -q -n synth 0.1 sin 880; sudo /home/john/hobby-code/twat.bash; sleep 15 ; fi ; done
