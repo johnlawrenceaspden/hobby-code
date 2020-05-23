c342bfcac123f2ca67edf5791ec93f2aa0ff3b48
John Lawrence Aspden <github@aspden.com>
2020-05-23 16:29:20 +0100

    grrr. this fucking heap...

--- twattingloop.bash
+++ twattingloop.bash
@@ -1,8 +1,10 @@
+FIX=false
+
 while true;
-           nmcli connection modify eduroam\ roaming ipv4.route-metric 500
-           nmcli connection modify eduroam\ strongest\ boat ipv4.route-metric 500
-           nmcli connection modify XT1032\ Network ipv4.route-metric 1000
-           nmcli connection modify Nokia\ 2\ Network ipv4.route-metric 1000
+           #nmcli connection modify eduroam\ roaming ipv4.route-metric 500
+           #nmcli connection modify eduroam\ strongest\ boat ipv4.route-metric 500
+           #nmcli connection modify XT1032\ Network ipv4.route-metric 1000
+           #nmcli connection modify Nokia\ 2\ Network ipv4.route-metric 1000
       
            echo =================================================================
            echo checking what network manager thinks
@@ -12,18 +14,21 @@ while true;
                echo XT1032 Network down
                play -q -n synth 0.1 sin 880 vol 0.009 ;
                #nmcli con down XT1032\ Network
-               nmcli con up   XT1032\ Network
+               if $FIX; then
+                   nmcli con up   XT1032\ Network
+               fi
            else
                echo XT1032 Network up
-
            fi
 
            if nmcli d s | grep A0:28:ED:82:15:B8 | grep disconnected;
            then
                echo Nokia 2 Network down
-               play -q -n synth 0.1 sin 220 vol 0.009 ;
+               play -q -n synth 0.1 sin 660 vol 0.009 ;
                #nmcli con down Nokia\ 2\ Network
-               nmcli con up   Nokia\ 2\ Network
+               if $FIX ; then
+                   nmcli con up   Nokia\ 2\ Network
+               fi
            else
                echo Nokia 2 Network up
            fi
@@ -34,8 +39,9 @@ while true;
                echo Wifi Network down
                play -q -n synth 0.1 sin 1320 vol 0.009 ;
                #nmcli con down eduroam\ roaming
-               nmcli con up   eduroam\ roaming
-
+               if $FIX; then
+                   nmcli con up   eduroam\ roaming
+               fi
            else
                echo Wifi Network up
                
@@ -61,27 +67,28 @@ while true;
                        echo re-re-ping successful
 	           else
                        echo enemy action! battle stations!
-		       play -q -n synth 0.1 sin 440 vol 0.99 ; 
-		       sudo /home/john/hobby-code/twat.bash;
-                       echo give it a while to recover before going back on watch
-		       sleep 10 ; 
+		       play -q -n synth 0.1 sin 440 vol 0.99 ;
+                       if $FIX ; then
+		           sudo /home/john/hobby-code/twat.bash;
+                           echo give it a while to recover before going back on watch
+		           sleep 10 ; 
+                       fi
 	           fi ;
 	       fi ;
 	   fi ;
 
-           if ip route | grep wlp2s0 | grep default;
-           then
-               echo wireless route present
-           else
-               echo -------------------- wireless route NOT present----------------------
-               play -q -n synth 0.1 sin 1320 vol 0.0099 ; 
-	       sudo /home/john/hobby-code/twat.bash;
-               echo give it a while to recover before going back on watch
-	       sleep 10 ; 
+           # if ip route | grep wlp2s0 | grep default;
+           # then
+           #     echo wireless route present
+           # else
+           #     echo -------------------- wireless route NOT present----------------------
+           #     play -q -n synth 0.1 sin 1320 vol 0.0099 ;
+           #     if $FIX; then
+	   #         sudo /home/john/hobby-code/twat.bash;
+           #         echo give it a while to recover before going back on watch
+	   #         sleep 10 ; 
+           #     fi
+           # fi
 
-           fi
-
-           
-           
         sleep 1 ; # all is well, back to sleep
        	done
