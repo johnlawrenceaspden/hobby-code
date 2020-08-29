3584bb1dfe3305d731ba77bdc7d17b7f7ce1856e
John Lawrence Aspden <github@aspden.com>
2020-08-29 15:35:46 +0100

    comment out some libraries

--- project.clj
+++ project.clj
@@ -10,9 +10,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.trace "0.7.9"]
                  [com.cemerick/pomegranate "0.3.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  
-                 [com.stuartsierra/component "0.3.1"]
-                 [simple-plotter "0.1.2"]
+                 ;[com.stuartsierra/component "0.3.1"]
+                 ;[simple-plotter "0.1.2"]
                  ])
 
 ;; Examples of how to use pomegranate to load other libraries
