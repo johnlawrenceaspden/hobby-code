48e2a89d28557994c72299962cd8e3ace6a75b2d
John Lawrence Aspden <github@aspden.com>
2021-01-07 05:33:16 +0000

    comby

--- comby/test.c
+++ comby/test.c
@@ -1,6 +1,20 @@
 int main(void) {
   printf("hello world!");
-  if(du->fc == 2) printf(du->fc) else printf("%s",du.fc);
+  printf("hello", "world!");
+  if(du->fc == 2) printf(du->fc) else printf("%s", du.fc);
+  if(du->fc == 2) {
+    printf(du->fc);
+  } else {
+
+    printf("%s", du.fc);
+
+    printf("%s\n", du->fc, du.fc);
+  }
   return a->b->c.d->fc ;
   return a->b->c.d.fc ;
+  return (a->b->c.d).fc ;
+  return (a->b->c.d)->fc ;
+
 }
+
+
