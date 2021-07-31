acfe1cbb287a65257c818a31cb90233075782b44
John Lawrence Aspden <github@aspden.com>
2021-07-31 16:50:34 +0100

    non-ergodic bet

---
+++ non-ergodic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void main(void){
+  double a=1000;
+  int wins=0;
+  int losses=0;
+  printf("hello\n");
+  srand((unsigned int)time(NULL));
+  for(int i=0; i<100; i++){
+    if(rand()%2 == 0){
+      printf("win");
+      wins++;
+      a*=1.7;
+    } else {
+      losses++;
+      printf("lose");
+      a*=0.6;
+    }
+    printf("amount %f (%d,%d)\n",a,wins,losses);
+  }
+}
