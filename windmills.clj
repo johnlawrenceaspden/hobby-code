0013df410fdfe00f3927f76900e113d3b4f4bbc6
John Lawrence Aspden <github@aspden.com>
2021-12-19 20:42:19 +0000

    waypoint

--- windmills.clj
+++ windmills.clj
@@ -48,61 +48,6 @@
 (defn svg-file [filename objects]
   (spit (str filename ".svg") (with-out-str (clojure.xml/emit (make-svg objects)))))
 
-
-
-
-(svg-file "windmill" (list [0 0 "red"]))
-
-(svg-file "windmill" 
-          (list [0 0 "red"]
-                [1 0 "red"]
-                [2 0 "red"]))
-
-(svg-file "windmill"
-          (list [0 0 "green"]
-                [0 1 "green"]
-                [0 2 "green"]))
-
-
-
-(def cross (list [0 0 "red"]
-                [1 0 "white"]
-                [2 0 "white"]
-                [0 1 "green"]
-                [0 2 "green"]
-                [-1 0 "white"]
-                [-2 0 "white"]
-                [0 -1 "green"]
-                [0 -2 "green"]))
-
-
-
-
-
-
-
-
-
-
-
-
-        
-(adjust-list cross)
-
-(svg-file "windmill" 
-           (list [0 0 "red"]
-                 [1 0 "white"]
-                 [2 0 "white"]
-                 [0 1 "green"]
-                 [0 2 "green"]
-                 [-1 0 "white"]
-                 [-2 0 "white"]
-                 [0 -1 "green"]
-                 [0 -2 "green"]))
-
-
-
-
 (defn orange [n]
   (if (< n 0)
     (range 0 n -1)
@@ -111,16 +56,83 @@
 (defn make-composite-rectangle [h v hsquares vsquares colour]
   (for [i (orange hsquares) j (orange vsquares)] [(+ i h) (+ j v) colour]))
 
-(make-composite-rectangle 0 0 3 1 "red")
-(make-composite-rectangle 0 0 -3 -1 "red")
 
+;; With this drawing code in hand we can diagram 29 = 1 * 1 + 4 * (1 * 7)
+
+;; As this windmill shape:
+(svg-file "windmill" 
+          (concat (make-composite-rectangle  0  0   1   1 "red")
+                  (make-composite-rectangle  1  0   7   1 "white")
+                  (make-composite-rectangle -1  0  -7  -1 "white")
+                  (make-composite-rectangle  0  1  -1   7 "green")
+                  (make-composite-rectangle  0 -1   1  -7 "green")))
+
+
+
+;; Or alternatively we could show it as 29 = 1*1 + 4 * (1 * 7) 
 
 (svg-file "windmill" 
-          (concat (list [0 0 "red"])
-                  (make-composite-rectangle  1  0   7  1 "white")
-                  (make-composite-rectangle -1  0  -7  1 "white")
-                  (make-composite-rectangle  0  1   1  7 "green")
-                  (make-composite-rectangle  0 -1  -1 -7 "green")))
+          (concat (make-composite-rectangle  0  0   1   1 "red")
+                  (make-composite-rectangle  1  0   1   7 "white")
+                  (make-composite-rectangle -1  0  -1  -7 "white")
+                  (make-composite-rectangle  0  1  -7   1 "green")
+                  (make-composite-rectangle  0 -1   7  -1 "green")))
+
+
+;; Now we notice that there's a 3x3 square in the middle, so what about:
+
+(svg-file "windmill" 
+          (concat (make-composite-rectangle  -1  -1     3   3 "red")
+                  (make-composite-rectangle   1   2     1   5 "white")
+                  (make-composite-rectangle  -1  -2    -1  -5 "white")
+                  (make-composite-rectangle  -2   1    -5   1 "green")
+                  (make-composite-rectangle   2  -1     5  -1 "green")))
+
+;; Which is equivalent to 29 = 3*3 + 4* (1 * 5)
+
+;; And then of course we can flatten the arms again:
+
+;; 29 = 3*3 + 4 * ( 5 * 1 )
+
+(svg-file "windmill" 
+          (concat (make-composite-rectangle  -1  -1   3   3 "red")
+                  (make-composite-rectangle   1   2  -5   1 "white")
+                  (make-composite-rectangle  -1  -2   5  -1 "white")
+                  (make-composite-rectangle  -2   1   1  -5 "green")
+                  (make-composite-rectangle   2  -1   1   5 "green")))
+
+;; And we see a 5*5 square now, so
+
+;; 29 = 5*5 + 4 * ( 1 * 1)
+
+(svg-file "windmill" 
+          (concat (make-composite-rectangle  -2  -2   5   5 "red")
+                  (make-composite-rectangle  -3   2  -1   1 "white")
+                  (make-composite-rectangle   3  -2   1   1 "white")
+                  (make-composite-rectangle  -2  -3   1  -1 "green")
+                  (make-composite-rectangle   2   3   1   1 "green")))
+
+;; Now notice that 4*1*1 is also equal to 2*2*1*1, =  (2*1)*(2*1)
+
+;; so 29 = 5*5+2*2
+
+;; Which is to say that 29, a prime number of form 4n + 1, is equal to the sum of an odd and an even square.
+
+;; 29 = 25 + 4 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 (svg-file "windmill" 
@@ -158,56 +170,3 @@
                        (make-composite-rectangle  -1 1   2 2 "green")
                        (make-composite-rectangle  1 -1  -2 -2 "green"))))
 
-
-
-
-
-
-
-(svg-file "windmill" (list (make-composite-rectangle 0 0 3 1 "red")))
-
-
-
-(spit "windmill.svg" (with-out-str (clojure.xml/emit (make-svg (list (make-composite-rectangle 20 30 4 5 "red"))))))
-
-
-
-
-
-
-;; The other day, I wanted to make some graph paper.
-
-;; While experimenting with inkscape, I noticed that svg is actually an xml file format.
-
-;; Since clojure is good with xml, that means that it's actually easier to make such a drawing with a program:
-
-;; Each element of the drawing is represented as a map
-
-
-;; The whole file is represented as a map containing those maps
-
-
-
-
-(defn make-svg [gridsize squaresize]
-  {:tag :svg
-   :attrs {:width "100%"
-           :height "100%"
-           :version "1.1"
-           :xmlns "http://www.w3.org/2000/svg"}
-   :content (concat #_(make-composite-rectangle 20 30 3 3 "green")
-                    (make-composite-rectangle 20 30 4 5 "red") )})
-
-;; The library clojure.contrib.lazy-xml will turn the nested map into xml:
-
-(require 'clojure.xml)
-
-;; We can use with-out-str to capture the output, which is unaccountably printed
-;; rather than given back as a string, and spit to write it to a file.
-
-(spit "windmill.svg" (with-out-str (clojure.xml/emit (make-svg 10 80))))
-
-;; The nice thing about this is that you can then use inkscape to modify the
-;; file, and then diff to work out how to take the modifications back into the
-;; program. Does anybody know how to make the emit function format the xml so
-;; that the output file is nicely readable?
