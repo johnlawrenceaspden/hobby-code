9e9c969d8da7ee5ee6018695781a3d66aa6752b7
John Lawrence Aspden <github@aspden.com>
2021-12-24 15:33:20 +0000

    start of next post

---
+++ windmills/windmills4.clj
@@ -0,0 +1,103 @@
+#!/usr/bin/env clojure
+
+;; Fermats' Christmas Theorem: Automatic Windmills
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Here's a bunch of code to make svg files of arrangements of coloured squares.
+;; I'm using this to draw the windmills.
+;; It's safe to ignore this if you're not interested in how to create such svg files.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'clojure.xml)
+
+(def squaresize 10)
+
+(defn make-svg-rect [i j colour]
+  {:tag :rect
+   :attrs {:x (str (* i squaresize)) :y (str (* j squaresize)) :width  (str squaresize) :height (str squaresize)
+           :style (str "fill:", colour, ";stroke:black;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1")}})
+
+(defn adjust-list [rectlist]
+  (if (empty? rectlist) rectlist
+      (let [hmin (apply min (map first  rectlist))
+            vmax (apply max (map second rectlist))]
+        (for [[a b c] rectlist] [(- a hmin) (- vmax b) c]))))
+
+(defn make-svg [objects]
+  {:tag :svg :attrs { :version "1.1"  :xmlns "http://www.w3.org/2000/svg"}
+   :content (for [[i j c] (adjust-list objects)] (make-svg-rect i j c))})
+
+(defn svg-file-from-rectlist [filename objects]
+  (spit (str filename ".svg") (with-out-str (clojure.xml/emit (make-svg objects)))))
+
+(defn hjoin
+  ([sql1 sql2] (hjoin sql1 sql2 1))
+  ([sql1 sql2 sep]
+   (cond (empty? sql1) sql2
+         (empty? sql2) sql1
+         :else (let [xmax1 (apply max (map first sql1))
+                     xmin2 (apply min (map first sql2))
+                     shift  (+ 1 sep (- xmax1 xmin2))]
+                 (concat sql1 (for [[h v c] sql2] [(+ shift h) v c]))))))
+
+(defn hcombine [& sqllist] (reduce hjoin '() sqllist))
+
+(defn svg-file [filename & objects]
+  (svg-file-from-rectlist filename (apply hcombine objects)))
+
+(defn orange [n] (if (< n 0) (range 0 n -1) (range 0 n 1)))
+
+(defn make-composite-rectangle [h v hsquares vsquares colour]
+  (for [i (orange hsquares) j (orange vsquares)] [(+ i h) (+ j v) colour]))
+
+(defn make-windmill [[s p n]]
+            (let [ s2 (quot s 2)
+                  is2 (inc s2)
+                  ds2 (- is2)]
+              (concat (make-composite-rectangle  (- s2)  (- s2) s      s     "red")
+                      (make-composite-rectangle  (- s2)  is2    p      n     "white")
+                      (make-composite-rectangle  s2      ds2    (- p)  (- n) "white")
+                      (make-composite-rectangle  ds2     (- s2) (- n)  p     "green")
+                      (make-composite-rectangle  is2     s2     n      (- p) "green"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; end of drawing code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; and here's some code from the previous posts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; code to tell us what number a triple represents
+(defn total [[s p n]] (+ (* s s) (* 4 p n)))
+
+;; the easy green transform
+(defn green [[s p n]] [s n p])
+
+;; and the complicated red transform, which doesn't look so bad once you've got it.
+(defn red [[s p n]]
+  (cond (< p (/ s 2))   [(- s (* 2 p))       (+ n (- s p)) p]
+        (< (/ s 2) p s) [(- s (* 2 (- s p))) p (+ n (- s p))]
+        (< s p (+ n s)) [(+ s (* 2 (- p s))) p (- n (- p s))]
+        (< (+ n s) p)   [(+ s (* 2 n))       n (- p (+ n s))]
+        :else [s p n]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; end of code from previous posts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(svg-file "windmills3-1" (make-windmill [1 1 9]) (make-windmill [1 9 1]) (make-windmill [3 1 7])(make-windmill [3 7 1])(make-windmill [5 1 3])(make-windmill [5 3 1])(make-windmill [1 3 3]))
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; end of blog post
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Note, to convert the svg files to png for posting on blogger, can do:
+;; for i in windmills3*svg; do rsvg-convert -z 2 "$i" -o "${i%svg}png"; done
+
+;; -z 2 (zoom factor 2 prevents the fuzzyness that affected the last post)
+
