84bd4bc1ce157aad3ed39d88c681e95502aecbf0
John Lawrence Aspden <github@aspden.com>
2021-12-24 13:36:36 +0000

    ok, this is working now

--- windmills3.clj
+++ windmills3.clj
@@ -79,47 +79,201 @@
 
 ;; So, automating the red transform:
 
+;; By eye it's always obvious what to do, but it's not so obvious to a computer,
+
+;; There are actually six different cases to consider, depending on the relative sizes of s, n and p!
+
+;; Let's fix s to be seven, and n to be three, and let p vary, that should be enough to capture all the different cases
+
+;; Start off with p as one, the smallest it can be
+
+;; The picture for the transformation we want to do is:
+(svg-file "windmill" (make-windmill [7 1 3]) (make-windmill [5 9 1]))
+
+;; We see that the arms of the windmill are all extending into the square, taking squares until they
+;; collide with the other arms.
+
+;; Because the arms stay n wide throughout, the collision happens when the arms have all extended by s-n squares.
+
+;; So the normal length 3 gets 6 (7-1) added to it, to become 9, the parallel length 1 stays the same
+
+;; And the red square loses the parallel length taken off it on both sides, so s 7 becomes s 7 - 2*1
+
+;; We might think that the transformation would take [7 1 3] to [5 1 9]
+
+;; Or in general terms, s -> s-2p n->n+(s-p), p->p
+
+(svg-file "windmill" (make-windmill [7 1 3]) (make-windmill [5 1 9]))
+
+;; But no! During the transformation, the arm that started at the north-west corner extended down so that it's now the arm that
+;; starts at the south-west corner, so we need to make our triple with 1 as the normal length and 9 as the parallel length
+
+;; That's what causes the red-green colour flip in our drawings
+
+;; So in fact what we want is s -> s-2p p->n+(s-p), n->p
+
+(svg-file "windmill" (make-windmill [7 1 3]) (make-windmill [5 9 1]))
+
+;; In order for this procedure to work, we need s-2p to be positive, or equivalently p < s/2
+
+;; In code:
+(defn red [[s p n]]
+  (cond (< p (/ s 2)) [(- s (* 2 p)) (+ n (- s p)) p]
+        :else [s p n]))
+
+;; If the condition's not true, we'll just hand back the original triple (for now...)
+
+(red [7 1 3]) ; [5 9 1]
+
+(let [[s p n][7 1 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; So now let's increase p by one
+(let [[s p n][7 2 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; Looks good, and again
+(let [[s p n][7 3 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; but increasing p again
+(let [[s p n][7 4 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; we see that our idea has failed, that transformation would take up more squares than we have (p >  s/2)
+;; So we've just given up and returned the original triple
+
+;; Notice that we haven't seen the case where p=s/2, because s is odd, so s/2 isn't an integer
+
+;; If p> s/2, we can do roughly the same thing, but we have to stop earlier.
+
+;; If we imagine our arms extending in, then we'll see that they have to stop after three moves, when they collide with each other
+;; that three comes from s-p, so the normal length needs to extend by s-p, and the square needs to lose s-p from both sides
+;; And in this case, we don't need to swap n and p, the normal length remains the normal length.
+
+;; The condition for being able to do this is that s-p is positive, or s>p
+
+;; And the transformation is s->s-2(s-p), n->n+(s-p), p->p
+
+;; Adding this case to our transform
+(defn red [[s p n]]
+  (cond (< p (/ s 2))   [(- s (* 2 p))       (+ n (- s p)) p]
+        (< (/ s 2) p s) [(- s (* 2 (- s p))) p (+ n (- s p))]
+        :else [s p n]))
+
+(red [7 4 3]) ; [1 4 6]
+
+(let [[s p n][7 4 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; In this case, our drawing appears to flip, because the corners of the red square have moved!
+;; But we can see that the shape is the same, and the number of squares is the same.
+
+;; keep increasing p
+(let [[s p n][7 5 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; again it works
+
+;; keep increasing p
+(let [[s p n][7 6 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; and again
+(let [[s p n][7 7 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; this time p=s, and so our procedure does nothing.
+
+;; In fact, there's nothing we can do here, we can neither shrink nor expand the red square while keeping the shape the same.
+
+;; I'm going to call this special, more symmetric type of windmill a 'cross'. It's a fixed point of our red
+;; transform, we just let it return the same triple.
+
+;; What if we make p even bigger?
+(let [[s p n][7 8 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; Neither of our previous ideas will work, but there's a new option here, we can make the square bigger at the expense of the arms
+(svg-file "windmill" (make-windmill [7 8 3]) (make-windmill [9 8 2]))
+
+;; Again, the shape appears to flip, but it's just a mirror image of the shape we would get if we extended the red square
+
+;; so how do we code this case?
+
+;; Well, the red square can get bigger (on both sides), by the amount by which the parallel length exceeds the square
+;; i.e. p-s
+;; The parallel length stays the same, and the normal length goes down by p-s
+
+;; so s-> s+2*(p-s), p->p, n-> n-(p-s)
+;; And this should work as long as p-s<n, or p<n+s
+
+
+
+(defn red [[s p n]]
+  (cond (< p (/ s 2))   [(- s (* 2 p))       (+ n (- s p)) p]
+        (< (/ s 2) p s) [(- s (* 2 (- s p))) p (+ n (- s p))]
+        (< s p (+ n s)) [(+ s (* 2 (- p s))) p (- n (- p s))]
+        :else [s p n]))
+
+(red [7 8 3]) ; [9 8 2]
+
+(let [[s p n][7 8 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; increase p
+(let [[s p n][7 9 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; looks good
+
+;; increase p
+(let [[s p n][7 10 3]] (svg-file "windmill" (make-windmill [s p n]) (make-windmill (red [s p n]))))
+
+;; Now p=s+n, and we can't make any of our previous ideas work.
+
+;; We could increase the red square to fill the whole shape now, but the result wouldn't be a windmill, and there's nothing
+;; else we can do, so we'll leave this case as a fixed point
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ;; from my drawing
-case 1 : (p < s/2)     decrease, delta is -p, negative, add to n, swap : s->s-2p, n->n+s-p , swap p and n
-case 2 : (s/2 < p < s) decrease, delta is (p-s) negative, add to n , don't swap
-case 4 : (s < p < s+n) increase, delta is (p-s) positive, take p-s from n , don't swap
-case 3 : (p > s+n), increase, delta is n, take it from p, swap
+;; case 1 : (p < s/2)     decrease, delta is -p, negative, add to n, swap : s->s-2p, n->n+s-p , swap p and n
+;; case 2 : (s/2 < p < s) decrease, delta is (p-s) negative, add to n , don't swap
+;; case 4 : (s < p < s+n) increase, delta is (p-s) positive, take p-s from n , don't swap
+;; case 3 : (p > s+n), increase, delta is n, take it from p, swap
 
 
 
 
 
-;; Notice that the red transform can actually work in four different ways
-
-;; Sometimes to keep the same shape, we need to add the spare squares to the parallel length of the blades, (or remove them)
-(defn red-parallel-swap [[s p n] delta]
-  (let [news  (+ s (* 2 delta))
-        spare (- ( * s s ) (* news news))
-        sparesperarm (/ spare 4)
-        lengthchange (/ sparesperarm n)]
-    [news n ( + p lengthchange)]))
-
-(defn red-parallel [[s p n] delta]
-  (let [news  (+ s (* 2 delta))
-        spare (- ( * s s ) (* news news))
-        sparesperarm (/ spare 4)
-        lengthchange (/ sparesperarm n)]
-    [news ( + p lengthchange) n]))
 
 
-(defn red-normal [[s p n] delta]
-  (let [news  (+ s (* 2 delta))
-        spare (- ( * s s ) (* news news))
-        sparesperarm (/ spare 4)
-        lengthchange (/ sparesperarm p)]
-    [news p (+ n lengthchange) ]))
 
-(defn red-normal-swap [[s p n] delta]
-  (let [news  (+ s (* 2 delta))
-        spare (- ( * s s ) (* news news))
-        sparesperarm (/ spare 4)
-        lengthchange (/ sparesperarm p)]
-    [news (+ n lengthchange) p ]))
+
+
+
+
+
+
 
 
 
@@ -129,8 +283,8 @@ case 3 : (p > s+n), increase, delta is n, take it from p, swap
 
 ;; And how do we choose delta??
 
-;; By eye it's always obvious what to do, but it's not so obvious to a computer,
-;; There are actually six different cases to consider, depending on the relative sizes of s, n and p!
+
+
 
 ;; So let's go through them one by one
 
@@ -1023,3 +1177,38 @@ case 3 : (p > s+n), increase, delta is n, take it from p, swap
 (red-normal [5 3 1] -2) ; [1 3 3]
 
 (svg-file "windmill" (make-windmill [5 3 1])(make-windmill [1 3 3]))
+
+
+;; Notice that the red transform can actually work in four different ways
+
+;; Sometimes to keep the same shape, we need to add the spare squares to the parallel length of the blades, (or remove them)
+(defn red-parallel-swap [[s p n] delta]
+  (let [news  (+ s (* 2 delta))
+        spare (- ( * s s ) (* news news))
+        sparesperarm (/ spare 4)
+        lengthchange (/ sparesperarm n)]
+    [news n ( + p lengthchange)]))
+
+(defn red-parallel [[s p n] delta]
+  (let [news  (+ s (* 2 delta))
+        spare (- ( * s s ) (* news news))
+        sparesperarm (/ spare 4)
+        lengthchange (/ sparesperarm n)]
+    [news ( + p lengthchange) n]))
+
+
+(defn red-normal [[s p n] delta]
+  (let [news  (+ s (* 2 delta))
+        spare (- ( * s s ) (* news news))
+        sparesperarm (/ spare 4)
+        lengthchange (/ sparesperarm p)]
+    [news p (+ n lengthchange) ]))
+
+(defn red-normal-swap [[s p n] delta]
+  (let [news  (+ s (* 2 delta))
+        spare (- ( * s s ) (* news news))
+        sparesperarm (/ spare 4)
+        lengthchange (/ sparesperarm p)]
+    [news (+ n lengthchange) p ]))
+
+
