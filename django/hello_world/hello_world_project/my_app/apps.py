5c32cb2c017d556cd246256b1812dfb56c92d26c
John Lawrence Aspden <github@aspden.com>
2022-01-03 15:35:43 +0000

    allow black to reformat every damned thing

--- django/hello_world/hello_world_project/my_app/apps.py
+++ django/hello_world/hello_world_project/my_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MyAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "my_app"
