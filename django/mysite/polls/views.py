dc022e70ccd548e6a9ce26c1689921455de81132
John Lawrence Aspden <github@aspden.com>
2022-01-05 20:28:23 +0000

    looking up an object in the database, and raising a 404 if it doesn't exist

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import Http404
 from django.http import HttpResponse
-from django.template import loader
+
+# from django.template import loader
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")
-    output = ", </br>".join([q.question_text for q in latest_question_list])
-    template = loader.get_template("polls/index.html")
+    #   output = ", </br>".join([q.question_text for q in latest_question_list])
+    #   template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
     # return HttpResponse(output + template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist as e:
+        raise Http404("Question does not exist") from e
+
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
