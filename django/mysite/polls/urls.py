727f8dcdc23a53b05d76d9fbd636307331aaa5fd
John Lawrence Aspden <github@aspden.com>
2022-01-05 20:50:11 +0000

    namespaces for the url names

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("specifics/<int:question_id>/", views.detail, name="detail"),
