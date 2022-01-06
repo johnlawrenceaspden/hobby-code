cd4e42ee2b627b68153a268b18e1d31a9e608577
John Lawrence Aspden <github@aspden.com>
2022-01-06 19:54:23 +0000

    yay tests

--- django/mysite/polls/tests.py
+++ django/mysite/polls/tests.py
@@ -9,8 +9,18 @@ from .models import Question
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), True)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=59)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
 
 
 # Create your tests here.
