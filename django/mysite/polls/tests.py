4fe7ea4705cba955d72ee3a8909253036834cb0e
John Lawrence Aspden <github@aspden.com>
2022-01-06 19:49:28 +0000

    make a test for an existing bug

--- django/mysite/polls/tests.py
+++ django/mysite/polls/tests.py
@@ -1,3 +1,16 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
 
 # Create your tests here.
