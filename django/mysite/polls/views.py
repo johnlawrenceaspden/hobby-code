2abaa660c862150a57f65140ee338098b32096f3
John Lawrence Aspden <github@aspden.com>
2022-01-06 20:28:06 +0000

    404 if the question is from the future

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -26,6 +26,9 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
