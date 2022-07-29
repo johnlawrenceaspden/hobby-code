a59af8eba09f471ba04a2d0408188c42a7f94f42
John Lawrence Aspden <github@aspden.com>
2022-07-29 19:43:48 +0100

    clojure hello

--- hello.clj
+++ hello.clj
@@ -1,2 +1,12 @@
 #!/usr/bin/env clojure
+
 (println "hello")
+
+;; using java builtins
+(let [a (Math/sqrt 7)] (println a))
+
+;; clojure standard library
+(require '[clojure.string])
+(println (clojure.string/upper-case "hello"))
+
+
