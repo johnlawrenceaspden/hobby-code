a7bb838f44d21ddab5fbe4f268363ba81867c2cd
John Lawrence Aspden <github@aspden.com>
2024-02-27 16:29:24 +0000

    sort array

--- leetcode/leetcode.c
+++ leetcode/leetcode.c
@@ -1,27 +1,12 @@
 #include<stdio.h>
 #include<malloc.h>
 #include<assert.h>
+#include<stdlib.h>
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-  int* i = nums;
-  int* j = nums + numsSize-1;
-  //printf("%d + %d = %d\n", *i, *j, *i + *j);
-  while (*i+*j != target && j>=i) {
-    if (*i + *j > target){ j--;}
-    else {i++;}
-    //printf("%d + %d = %d\n", *i, *j, *i + *j);
-
-  }
-  int* retval=(int*) malloc(2*sizeof(int));
-  *returnSize=2;
-  retval[0]=i-nums;
-  retval[1]=j-nums;
-  return retval;
-}
-
+int comparator(const void *p, const void *q) 
+{ 
+  return (*(int*)p-*(int*)q);
+} 
 
 void aiprint(int *array, int arraysize)
 {
@@ -33,13 +18,57 @@ void aiprint(int *array, int arraysize)
 }
 
 
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+  //printf("---\n");
+
+  int *newnums=malloc(sizeof(int)*numsSize);
+  for(int i=0; i<numsSize; i++) newnums[i]=nums[i];
+  qsort((void*)newnums, numsSize, sizeof(int), comparator);
+
+  //aiprint(nums, numsSize); printf("\n");
+  //aiprint(newnums, numsSize); printf("\n");
+  
+  int* i = newnums ; 
+  int* j = newnums + numsSize-1; 
+  //printf("%d + %d = %d\n", *i, *j, *i + *j);
+  while (*i+*j != target && j>=i) {
+    if (*i + *j > target){ j--;}
+    else {i++;}
+    //printf("%d + %d = %d\n", *i, *j, *i + *j);
+
+  }
+  int a=*i;
+  int b=*j;
+  free(newnums);
+
+  int* p1=nums;
+  int* p2=nums;
+  for( ;*p1!=a; p1++);
+  for( ;*p2!=b; p2++);
+  
+  int* retval=(int*) malloc(2*sizeof(int));
+  *returnSize=2;
+  retval[0]=p1-nums;
+  retval[1]=p2-nums;
+
+  //printf("---\n");
+  return retval;
+}
+
+
+
+
 void twoSumTest(int*nums, int numsize, int target)
 {
   int* retval;
   int returnSize;
-
+  printf("---\n");
   aiprint(nums, numsize);
-  printf(" =%d\n", target);
+  printf(" => %d\n", target);
   retval = twoSum(nums, numsize, target, &returnSize);
   aiprint(retval, returnSize); printf("\n");
 
@@ -87,6 +116,13 @@ int main(void)
     int target=6;
     twoSumTest(nums, sizeof(nums)/sizeof(int), target);
   }
+
+  {
+    int nums[]={-1,-2,-3,-4,-5};
+    int target=-8;
+    twoSumTest(nums, sizeof(nums)/sizeof(int), target);
+  }
+
 
 
   
