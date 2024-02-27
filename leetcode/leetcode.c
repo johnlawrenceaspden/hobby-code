3c6a41940745a61d9d18a89c84595a68c825e13a
John Lawrence Aspden <github@aspden.com>
2024-02-27 15:48:04 +0000

    autoindent

--- leetcode/leetcode.c
+++ leetcode/leetcode.c
@@ -6,20 +6,20 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* i = nums;
-    int* j = nums + numsSize-1;
+  int* i = nums;
+  int* j = nums + numsSize-1;
+  //printf("%d + %d = %d\n", *i, *j, *i + *j);
+  while (*i+*j != target && j>=i) {
+    if (*i + *j > target){ j--;}
+    else {i++;}
     //printf("%d + %d = %d\n", *i, *j, *i + *j);
-    while (*i+*j != target && j>=i) {
-        if (*i + *j > target){ j--;}
-        else {i++;}
-        //printf("%d + %d = %d\n", *i, *j, *i + *j);
 
-    }
-    int* retval=(int*) malloc(2*sizeof(int));
-    *returnSize=2;
-    retval[0]=i-nums;
-    retval[1]=j-nums;
-    return retval;
+  }
+  int* retval=(int*) malloc(2*sizeof(int));
+  *returnSize=2;
+  retval[0]=i-nums;
+  retval[1]=j-nums;
+  return retval;
 }
 
 
@@ -35,20 +35,20 @@ void aiprint(int *array, int arraysize)
 
 void twoSumTest(int*nums, int numsize, int target)
 {
-    int* retval;
-    int returnSize;
+  int* retval;
+  int returnSize;
 
-    aiprint(nums, numsize);
-    printf(" =%d\n", target);
-    retval = twoSum(nums, numsize, target, &returnSize);
-    aiprint(retval, returnSize); printf("\n");
+  aiprint(nums, numsize);
+  printf(" =%d\n", target);
+  retval = twoSum(nums, numsize, target, &returnSize);
+  aiprint(retval, returnSize); printf("\n");
 
-    int a=*(nums+retval[0]);
-    int b=*(nums+retval[1]);
-    int c = a+b;
-    printf("%d + %d = %d\n",a,b,c);
-    free(retval);
-    if(target==a+b) printf("yay\n"); else printf("BOO!\n");
+  int a=*(nums+retval[0]);
+  int b=*(nums+retval[1]);
+  int c = a+b;
+  printf("%d + %d = %d\n",a,b,c);
+  free(retval);
+  if(target==a+b) printf("yay\n"); else printf("BOO!\n");
 }
 
 
@@ -59,14 +59,15 @@ int main(void)
 
   {
     int nums[]={2,7,11,15};
-  int target=12;
-  twoSumTest(nums, sizeof(nums)/sizeof(int), target);
+    int target=12;
+    twoSumTest(nums, sizeof(nums)/sizeof(int), target);
   }
 
   {
     int nums[]={2,7,11,15};
-  int target=14;
-  twoSumTest(nums, sizeof(nums)/sizeof(int), target);}
+    int target=14;
+    twoSumTest(nums, sizeof(nums)/sizeof(int), target);
+  }
 
   
   return 0;
