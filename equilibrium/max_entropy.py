248028cf1a0966d31e9cd3f9729952d2549a3ae5
John Lawrence Aspden <github@aspden.com>
2024-10-15 00:43:22 +0100

    entropy

--- equilibrium/max_entropy.py
+++ equilibrium/max_entropy.py
@@ -7,8 +7,6 @@ from collections import Counter
 
 print("hello")
 
-dice = [10] * 20000
-
 
 def change(n):
     n = n + random.choice([-1, 1])
@@ -63,19 +61,38 @@ def exchange_floor_no_topology(dice):
     return dice
 
 
+def exchange_floor_ceiling_no_topology(dice):
+    random.shuffle(dice)
+    ld = len(dice)
+    for i in range(ld - 1):
+        # print(dice)
+        i1 = i
+        i2 = i + 1
+        if i1 != i2:
+            a = random.choice([-1, 1])
+            b = dice[i1] + a
+            c = dice[i2] - a
+            if b >= 1 and c >= 1 and b <= 6 and c <= 6:
+                dice[i1] = b
+                dice[i2] = c
+    return dice
+
+
 def info(p):
     return -p * math.log2(p)
 
 
+dice = [3] * 20000
 avg = []
 entropy = []
-for i in range(400):
+for i in range(100):
     avg.append(sum(dice) / len(dice))
     c = Counter(dice)
     e = sum([info(x / c.total()) for x in c.values()])
     entropy.append(e)
-    print(i, sum(dice) / len(dice), e)
-    dice = exchange_floor_no_topology(dice)
+    if i % 10 == 0:
+        print(i, sum(dice) / len(dice), e)
+    dice = exchange_floor_ceiling_no_topology(dice)
 
 
 print("max entropy of six", math.log2(6), "bits")
@@ -83,6 +100,54 @@ c = Counter(dice)
 print(sum([info(x / c.total()) for x in c.values()]))
 p = [b for a, b in sorted(c.items())]
 rats = [(a / b) for (a, b) in list(zip(p, p[1:]))]
+print(p)
+print(rats)
+
+print("Boltzmann distribution over six with average 3")
+l = 1.1908
+s = sum([1 / (l**i) for i in range(6)])
+p = [1 / (l**i) / s for i in range(6)]
+e = sum([info(px) for px in p])
+a = sum([(p * i) for p, i in zip(p, range(1, 7))])
+
+print(l, s, p, e, a)
+
+print("Boltzmann distribution over six with average 3.5")
+l = 1
+s = sum([1 / (l**i) for i in range(6)])
+p = [1 / (l**i) / s for i in range(6)]
+e = sum([info(px) for px in p])
+a = sum([(p * i) for p, i in zip(p, range(1, 7))])
+
+print(l, s, p, e, a)
+
+print("Boltzmann distribution over six with average 4")
+l = 1 / 1.1908
+s = sum([1 / (l**i) for i in range(6)])
+p = [1 / (l**i) / s for i in range(6)]
+e = sum([info(px) for px in p])
+a = sum([(p * i) for p, i in zip(p, range(1, 7))])
+
+print(l, s, p, e, a)
+
+print("Boltzmann distributions over six")
+for l in [1 / 1.1908, 1, 1.1908]:
+    s = sum([1 / (l**i) for i in range(6)])
+    p = [1 / (l**i) / s for i in range(6)]
+    e = sum([info(px) for px in p])
+    a = sum([(p * i) for p, i in zip(p, range(1, 7))])
+    print(l, e, a)
+
+
+def boltzmann(l, n):
+    s = sum([1 / (l**i) for i in range(n)])
+    p = [1 / (l**i) / s for i in range(n)]
+    e = sum([info(px) for px in p])
+    a = sum([(p * i) for p, i in zip(p, range(1, n + 1))])
+    return a, e
+
+
+print(boltzmann(4 / 3, 100))
 
 plt.subplot(1, 4, 1)
 plt.bar(c.keys(), c.values())
@@ -92,4 +157,5 @@ plt.subplot(1, 4, 3)
 plt.plot(avg)
 plt.subplot(1, 4, 4)
 plt.plot(entropy)
+
 plt.show()
