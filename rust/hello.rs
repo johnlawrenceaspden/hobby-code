f3f70905030acfb90340d6cbe61f2b63845d17aa
John Lawrence Aspden <github@aspden.com>
2024-10-26 15:23:37 +0100

    yay factorial

--- rust/hello.rs
+++ rust/hello.rs
@@ -3,6 +3,12 @@
 /* Playing with the tutorial at
    https:doc.rust-lang.org/rust-by-example/hello/comment.html */
 
+fn factorial(n:i32) ->i32{
+    if n==0 {return 1;}
+    else {return n*factorial(n-1);}
+}
+
 fn main(){
-    println!("yo {}",2);
+    println!("yo {}",factorial(12));
+    println!("yo {}",factorial(13));
 }
