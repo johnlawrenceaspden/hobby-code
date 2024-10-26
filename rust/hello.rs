1e9d6e9a4236fa2dd04df2f8d8371506fde80ef8
John Lawrence Aspden <github@aspden.com>
2024-10-26 14:35:27 +0100

    I think I quite like this language now

--- rust/hello.rs
+++ rust/hello.rs
@@ -1,4 +1,9 @@
 // you can run me with rustc hello.rs && ./hello
+
+/*Block Comment*/
+
+
+///weird-ass comment, wonder what this does?
 fn main(){
     println!("Hello World from Rust!");
     println!("I am instructed to add a second line here but am a bit too crusty...");
