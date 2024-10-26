64d89f19af0713a67351901f535b8f617819e24a
John Lawrence Aspden <github@aspden.com>
2024-10-26 15:26:02 +0100

    no biginteger type apparently, but at least it blows

--- rust/hello.rs
+++ rust/hello.rs
@@ -3,7 +3,7 @@
 /* Playing with the tutorial at
    https:doc.rust-lang.org/rust-by-example/hello/comment.html */
 
-fn factorial(n:i32) ->i32{
+fn factorial(n:i128) ->i128{
     if n==0 {return 1;}
     else {return n*factorial(n-1);}
 }
@@ -11,4 +11,6 @@ fn factorial(n:i32) ->i32{
 fn main(){
     println!("yo {}",factorial(12));
     println!("yo {}",factorial(13));
+    println!("yo {}",factorial(33));
+    println!("yo {}",factorial(34));
 }
