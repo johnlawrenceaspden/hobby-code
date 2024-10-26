ab4f1098dca7f3699a15fb73b14d4de5d8b1bf43
John Lawrence Aspden <github@aspden.com>
2024-10-26 13:58:54 +0100

    crab thing

--- rust/hello-rust/src/main.rs
+++ rust/hello-rust/src/main.rs
@@ -1,5 +1,12 @@
 use ferris_says::say;
+use std::io::BufWriter;
+use std::io::stdout;
 
 fn main() {
     println!("Hello, world!");
+    let stdout=stdout();
+    let message=String::from("Yo bitches");
+    let width=message.chars().count();
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
