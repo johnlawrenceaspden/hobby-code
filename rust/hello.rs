c7deafd6d7a07f380243af5d2a3d64e8d1abcbda
John Lawrence Aspden <github@aspden.com>
2024-10-26 14:27:37 +0100

    turns out you don't actually have to create a pile of vomit to say hello

---
+++ rust/hello.rs
@@ -0,0 +1,5 @@
+// you can run me with rustc hello.rs && ./hello
+fn main(){
+    println!("Hello World from Rust!");
+    println!("I am instructed to add a second line here but am a bit too crusty...");
+}
