332bcc39169deb9a19f56b2a7891a30afb157fad
John Lawrence Aspden <github@aspden.com>
2024-10-28 21:15:01 +0000

    iterator and targets

--- rust/hello.rs
+++ rust/hello.rs
@@ -1,15 +1,10 @@
 // you can run me with rustc hello.rs && ./hello
 
-/* Playing with the tutorial at
-   https:doc.rust-lang.org/rust-by-example/hello/comment.html */
-
-fn factorial(n:i128) ->i128{
-    if n==0 {return 1;}
-    else {return n*factorial(n-1);}
-}
-
 fn main(){
-    for n in 1..{
-        println!("{} yo {}",n,factorial(n));
+    for n in 1..10{
+        println!("{0} yo {0}",n);
     }
+    let a=["milton","chesterton"].map(|s|{format!("inputs/{s}_rgb.tiff")});
+    println!("{:?}",a);
+    a.into_iter().for_each(|h| println!("{h}"))
 }
