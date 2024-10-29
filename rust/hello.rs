a2fef9dc9380ab2582c83881116ce80e2bfbfc45
John Lawrence Aspden <github@aspden.com>
2024-10-29 11:57:02 +0000

    rust

--- rust/hello.rs
+++ rust/hello.rs
@@ -5,6 +5,7 @@
 
 use std::fmt::{self, Formatter, Display};
 
+#[derive(Debug)]
 struct City {
     name: &'static str,
     // Latitude
@@ -36,26 +37,33 @@ impl Display for City {
 
 
 #[derive(Debug)]
-struct Color {
+struct Colour {
     red: u8,
     green: u8,
     blue: u8,
 }
 
-const COLOURS:[Color;3]=[
-        Color { red: 128, green: 255, blue: 90 },
-        Color { red: 0, green: 3, blue: 254 },
-        Color { red: 0, green: 0, blue: 0 },
+impl Display for Colour{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result{
+        write!(f,"0x{:0>2X}{:0>2X}{:0>2X}",self.red, self.green,self.blue)}
+}
+
+const COLOURS:[Colour;3]=[
+        Colour { red: 128, green: 255, blue: 90 },
+        Colour { red: 0, green: 3, blue: 254 },
+        Colour { red: 0, green: 0, blue: 0 },
     ];
 
 fn main() {
     for city in CITIES {
+        println!("{:?}", city);
         println!("{}", city);
     }
-    for color in COLOURS {
+    for colour in COLOURS {
         // Switch this to use {} once you've added an implementation
         // for fmt::Display.
-        println!("{:?}", color);
+        println!("{:?}", colour);
+        println!("{}", colour);
     }
 }
 
