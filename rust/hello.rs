a40a7bf11ed137af8a126dc645620a416ea9ae8f
John Lawrence Aspden <github@aspden.com>
2024-10-30 17:04:33 +0000

    types, & symbol, type names

--- rust/hello.rs
+++ rust/hello.rs
@@ -3,4 +3,47 @@
 /* Playing with the tutorial at
    https:doc.rust-lang.org/rust-by-example/hello/comment.html */
 
-fn main(){println!("hello")}
+// fn print_type_of<T: std::fmt::Debug>(v: &T) {
+//     println!("{:?}{}", v, std::any::type_name::<T>());
+// }
+
+use std::any::type_name_of_val;
+use std::any::type_name;
+// fn main() {
+//     let mut my_number = 32.90;
+//     println!("{}", type_name_of_val(&my_number));
+// }
+
+
+
+fn p<T: std::fmt::Debug>(v: &T) {
+    println!("{} {:?}", std::any::type_name::<T>(), v);
+}
+
+fn s<T: std::fmt::Debug>(v: T) {
+    println!("{} {} {:?}", std::any::type_name_of_val(&v), std::any::type_name::<T>(), v);
+}
+
+
+// I have no idea what is going on here
+fn main(){
+    println!("hello");
+
+    let mut m = 32.90;
+    m=m+0.1;
+    println!("{} {:?}", type_name_of_val(&m), m);
+    s(m);
+    p(&m);
+
+    let m = [1,1,1];
+    println!("{} {:?}", type_name_of_val(&m), m);
+    s(m);
+    p(&m);
+
+    
+    let a = "hello!\n".as_bytes();
+    println!("{} {:?}",type_name_of_val(a),a);
+    println!("{} {:?}",type_name_of_val(&a),a);
+    s(a);
+    p(&a);
+}
