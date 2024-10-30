bfa0652d8a639a7db176f37994ef398f9e18dce0
John Lawrence Aspden <github@aspden.com>
2024-10-30 18:03:20 +0000

    new skeleton

--- rust/hello.rs
+++ rust/hello.rs
@@ -3,40 +3,24 @@
 /* Playing with the tutorial at
    https:doc.rust-lang.org/rust-by-example/hello/comment.html */
 
-// fn print_type_of<T: std::fmt::Debug>(v: &T) {
-//     println!("{:?}{}", v, std::any::type_name::<T>());
-// }
 
+// to print the type of a thing
+// println!("{} {:?}",type_name_of_val(a),a);
+
+#[allow(unused_imports)]
 use std::any::type_name_of_val;
 
-fn p<T: std::fmt::Debug>(v: &T) {
-    println!("{} {:?}", std::any::type_name::<T>(), v);
+//call this as p(a)
+#[allow(dead_code)]
+fn p<T: std::fmt::Debug>(v: T) {
+    println!("{} {:?}", std::any::type_name_of_val(&v), v);
 }
 
-fn s<T: std::fmt::Debug>(v: T) {
-    println!("{} {} {:?}", std::any::type_name_of_val(&v), std::any::type_name::<T>(), v);
-}
-
-
-// I have no idea what is going on here
 fn main(){
     println!("hello");
 
     let mut m = 32.90;
     m=m+0.1;
-    println!("{} {:?}", type_name_of_val(&m), m);
-    s(m);
-    p(&m);
 
-    let m = [1,1,1];
-    println!("{} {:?}", type_name_of_val(&m), m);
-    s(m);
-    p(&m);
-
-    
-    let a = "hello!\n".as_bytes();
-    println!("{} {:?}",type_name_of_val(a),a);
-    println!("{} {:?}",type_name_of_val(&a),a);
-    s(a);
-    p(&a);
+    p(m);
 }
