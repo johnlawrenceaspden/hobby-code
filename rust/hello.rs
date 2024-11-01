f0e12b831829914871b5d33d3615fe9536a5b24f
John Lawrence Aspden <github@aspden.com>
2024-11-01 18:34:35 +0000

    tidy up

--- rust/hello.rs
+++ rust/hello.rs
@@ -14,10 +14,11 @@ use std::any::type_name_of_val;
 // call this as p(&a, "name", "label")
 #[allow(dead_code)]
 fn p<T: std::fmt::Debug>(v: &T, name:&str, label:&str) {
+    let s = format!("({}) type {} value {:?}" ,name, std::any::type_name_of_val(&*v), v);
     if label.is_empty(){
-        println!("({}) type {} value {:?}" ,name, std::any::type_name_of_val(&*v), v);
+        println!("{s}");
     } else {
-        println!("{} ({}) type {} value {:?}",label ,name, std::any::type_name_of_val(&*v), v);
+        println!("{label} {s}");
     }
 }
 
@@ -29,6 +30,7 @@ macro_rules! p {
     ($a: expr) => {
         p(&$a, stringify!($a),"")
     };
+    () =>{println!("");}
 }
 
 
@@ -51,7 +53,7 @@ macro_rules! add {
 
 
 fn addopt(a: Option<i32>, b: Option<i32>) -> i32 {
-    a.unwrap_or(1) + b.unwrap_or(2)
+    a.unwrap_or(0) + b.unwrap_or(0)
 }
 
 
@@ -61,26 +63,30 @@ fn addopt(a: Option<i32>, b: Option<i32>) -> i32 {
 fn main(){
     println!("hello");
 
-    println!("{}",addopt(Some(3), Some(4)));
-    println!("{}",addopt(None, Some(4)));
-    println!("{}",addopt(Some(4),None));
-    println!("{}",addopt(None, None));
-    
-    println!("{}",add!());
-    println!("{}",add!(4));
-    println!("{}",add!(4,3));
 
-    let s=String::from("doom");
+    
+    p!(addopt(Some(3), Some(4)),"add two real things");
+    p!(addopt(None, Some(4)), "dummy first argument");
+    p!(addopt(Some(4),None), "dummy second argument");
+    p!(addopt(None, None), "no arguments at all");
+
+    p!();
+    
+    p!(add!());
+    p!(add!(4));
+    p!(add!(4,3));
+
+    p!();
+
     p(&7, "7", "");
     p(&7, "7", "seven");
-    p(&s, "s", "");
-
-    
     p!(7, "seven");
     p!(7);
+
+    p!();
+    
+    let s=String::from("doom");
+    p(&s, "s", "");
     p!(s, "the string s is");
     p!(s);
-
-    p!(add!(4,3));
-//    println!("{}",add!(4,3));
 }
