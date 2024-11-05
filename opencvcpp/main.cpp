19e93748c9c070a79c59f5c3565e01bb5ccfea6b
John Lawrence Aspden <github@aspden.com>
2024-11-05 17:07:52 +0000

    opencv hello

--- opencvcpp/main.cpp
+++ opencvcpp/main.cpp
@@ -11,6 +11,17 @@ int main()
  
     cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
     cv::imshow("Display Image", image);
-    cv::waitKey(0);
+
+    cv::Mat resized;
+    int new_width = 300;
+    int new_height = 200;
+    cv::resize(image, resized, cv::Size(new_width, new_height));
+    cv::imshow("Resized Image", resized);
+
+    int c;
+    while(c = cv::waitKey(0), c!=27){
+      cout << "yo" << c <<  "\n";
+    }
+    cv::destroyAllWindows(); 
     return 0;
 }
