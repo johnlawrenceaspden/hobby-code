522387288eba8687f47cf46694ef00e57bc4a63e
John Lawrence Aspden <github@aspden.com>
2026-06-22 19:22:49 +0100

    add explicit program path

--- reddit_gallery_download_test.bash
+++ reddit_gallery_download_test.bash
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+prog="/home/john/hobby-code/reddit_gallery_download.py"
+
 # run doctests first
-python -m doctest reddit_gallery_download.py -v
+python -m doctest $prog -v
 
 
 # Two different links to the same image 
-python reddit_gallery_download.py https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fo9fu9uw82rvf1.png
-python reddit_gallery_download.py https://i.redd.it/o9fu9uw82rvf1.png
+python $prog https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fo9fu9uw82rvf1.png
+python $prog https://i.redd.it/o9fu9uw82rvf1.png
 # Two different links to the same gallery
-python reddit_gallery_download.py "https://www.reddit.com/gallery/1occk95"
-python reddit_gallery_download.py "https://www.reddit.com/r/dalle2/comments/1occk95/youre_exploring_a_lonely_asteroid_in_the_middle/"
+python $prog "https://www.reddit.com/gallery/1occk95"
+python $prog "https://www.reddit.com/r/dalle2/comments/1occk95/youre_exploring_a_lonely_asteroid_in_the_middle/"
 # specify download directory manually
-python reddit_gallery_download.py "https://www.reddit.com/r/dalle2/comments/1occk95/youre_exploring_a_lonely_asteroid_in_the_middle/" dalle2/asteroid_images
+python $prog "https://www.reddit.com/r/dalle2/comments/1occk95/youre_exploring_a_lonely_asteroid_in_the_middle/" dalle2/asteroid_images
 # another gallery, just for paranoia
-python reddit_gallery_download.py https://www.reddit.com/r/dalle2/comments/1mqp46m/gloomy_winter_scenes_from_berlin_and_iceland/
+python $prog https://www.reddit.com/r/dalle2/comments/1mqp46m/gloomy_winter_scenes_from_berlin_and_iceland/
 
 # expected results if you started with an empty or non-existent dalle2 directory
 tree -s dalle2 
